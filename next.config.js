3b2a7e7c9d95980147d996a084755e286f7a4b6a
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-06-03 22:14:52 -0700

    Implement CSS Modules in app

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  webpack: (config) => {
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[name]_[local]__[hash:base64:5]',
+    modules: true,
+  },
+  webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
+      fs: 'empty',
+    };
 
-    return config
-  }
-}
+    return config;
+  },
+});
