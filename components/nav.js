9eae4965a9483d92372f794b0ff9dee21ee61c6a
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-06-04 00:01:04 -0700

    Simplify nav for quick scaffolding

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,5 @@
-import Head from './head'
-import Link from 'next/link'
-
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+import Head from './head';
+import Link from 'next/link';
 
 const Nav = () => (
   <nav>
@@ -16,45 +9,13 @@ const Nav = () => (
           <a>Home</a>
         </Link>
       </li>
-      <ul>
-        {links.map(
-          ({ key, href, label }) => (
-            <li key={key}>
-              <Link href={href}>
-                <a>{label}</a>
-              </Link>
-            </li>
-          )
-        )}
-      </ul>
+      <li>
+        <Link prefetch href="/404">
+          <a>404</a>
+        </Link>
+      </li>
     </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system,BlinkMacSystemFont,Avenir Next,Avenir,Helvetica,sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
