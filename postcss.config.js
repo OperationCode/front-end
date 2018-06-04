b3c22c672061d1c1e449412703cf6a7c29ae0dcf
Kyle Holmberg <kyle.holmberg@autogravity.com>
2018-06-03 22:13:59 -0700

    Add postcss config for autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      browsers: [
+        '>1%',
+        'last 4 versions',
+        'Firefox ESR',
+        'not ie < 9', // React doesn't support IE8 anyway
+      ],
+      flexbox: 'no-2009',
+    },
+  },
+};
