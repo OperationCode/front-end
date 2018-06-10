b520943abc9c543552729aaa438c555edc60fae1
Kyle Holmberg <kylemh.email@gmail.com>
2018-06-10 00:53:55 -0700

    Modularize style variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,25 @@
-module.exports = {
+module.exports = ({
+  file, options, env,
+}) => ({
   plugins: {
-    autoprefixer: {
-      browsers: [
-        '>1%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9', // React doesn't support IE8 anyway
-      ],
-      flexbox: 'no-2009',
+    autoprefixer:
+      env === 'production'
+        ? {
+          ...options.autoprefixer,
+          browsers: [
+            '>1%',
+            'last 4 versions',
+            'Firefox ESR',
+            'not ie < 9', // React doesn't support IE8 anyway
+          ],
+          flexbox: 'no-2009',
+        }
+        : false,
+    'postcss-import': {
+      root: file.dirname,
+    },
+    'postcss-export-custom-variables': {
+      destination: 'common/styles/themeMap.js',
     },
   },
-};
+});
