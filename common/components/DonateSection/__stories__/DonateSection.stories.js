84ec7c00cd740be63937b643bb278669c0416a30
Kyle Holmberg <kylemh.email@gmail.com>
2018-06-13 23:01:31 -0700

    Finish story for DonateSection

--- common/components/DonateSection/__stories__/DonateSection.stories.js
+++ common/components/DonateSection/__stories__/DonateSection.stories.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, number, select, text } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
 
 import DonateSection from '../DonateSection';
 
-storiesOf('DonateSection', module)
-  .addDecorator(withKnobs)
-  .add('default', () => (
-    <DonateSection
-      theme={select('theme', ['primary', 'secondary', 'gray'])}
-      fullWidth={boolean('fullWidth', false)}
-      onClick={action('DonateSection Clicked!')}
-      tabIndex={number('tabIndex', 0)}
-    >
-      {text('children', 'Click Me!')}
-    </DonateSection>
-  ));
+storiesOf('DonateSection', module).add('default', () => <DonateSection />);
