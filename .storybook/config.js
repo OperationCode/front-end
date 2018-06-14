a4e1f791ed0a88f407d22f094ba23d5db1ce6f4e
Kyle Holmberg <kylemh.email@gmail.com>
2018-06-13 23:01:20 -0700

    Mock next router for Link usage in Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Router from 'next/router';
 import { addDecorator, configure } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { setOptions } from '@storybook/addon-options';
 import brandingBackgrounds from './backgrounds';
 
@@ -16,6 +18,22 @@ function loadStories() {
   requireCommonComponents.keys().forEach(requireCommonComponents);
   // Add any new component folders with stories here, using the patterns defined above
 }
+
+/* Necessary to mock Next router */
+const actionWithPromise = () => {
+  action('clicked link')();
+  // we need to return promise because it is needed by Link.linkClicked
+  return new Promise((resolve, reject) => reject());
+};
+
+const mockedRouter = {
+  push: actionWithPromise,
+  replace: actionWithPromise,
+  prefetch: () => {},
+};
+
+Router.router = mockedRouter;
+/* ***************************** */
 
 const centerContainer = {
   alignItems: 'center',
