2b74632546c4a5f61fbae81f31cf40e90a8bdb97
Kyle Holmberg <inbox@kylemh.com>
2018-07-14 19:28:44 -0700

    Create <ImageCard> and add story

---
+++ components/Single-Purpose/Cards/ImageCard/ImageCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'common/components/Button/Button';
+import styles from './ImageCard.css';
+
+ImageCard.propTypes = {
+  buttonText: PropTypes.string,
+  cardText: PropTypes.string.isRequired,
+  imageSource: PropTypes.string.isRequired,
+  link: PropTypes.string,
+  title: PropTypes.string.isRequired,
+};
+
+ImageCard.defaultProps = {
+  buttonText: '',
+  link: '',
+};
+
+function ImageCard({
+  buttonText, cardText, imageSource, link, title,
+}) {
+  return (
+    <div className={styles.ImageCard}>
+      <img
+        className={styles.cardImage}
+        src={imageSource}
+        alt={title}
+      />
+
+      <div className={styles.cardText}>
+        <h6>{title}</h6>
+        <p>{cardText}</p>
+        {link && (
+          <Button
+            href={link}
+            theme="primary"
+          >
+            {buttonText}
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ImageCard;
---
+++ components/Single-Purpose/Cards/ImageCard/__stories__/ImageCard.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+import ImageCard from '../ImageCard';
+
+storiesOf('Single-Purpose/Cards/ImageCard', module)
+  .addDecorator(withKnobs)
+  .add('default', () => (
+    <ImageCard
+      buttonText={text('buttonText', 'Click Me!')}
+      cardText={text('cardText', 'buttonText is only shown/necessary when `link` exists')}
+      imageSource={text(
+        'imageSource',
+        'https://operationcode.org/static/media/ThinkstockPhotos-489787502.812e.jpg',
+      )}
+      link={text('buttonText', 'https://google.com')}
+      title={text('title', 'Image Card')}
+    />
+  ));
