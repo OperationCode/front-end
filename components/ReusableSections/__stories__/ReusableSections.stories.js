9bd7180b7884ea88212447e702ab185b54f0a5ad
Kyle Holmberg <inbox@kylemh.com>
2018-07-14 22:25:47 -0700

    Migrate Login component and subcomponents

--- components/ReusableSections/__stories__/ReusableSections.stories.js
+++ components/ReusableSections/__stories__/ReusableSections.stories.js
@@ -3,7 +3,9 @@ import { storiesOf } from '@storybook/react';
 
 import DonateSection from '../DonateSection/DonateSection';
 import JoinSection from '../JoinSection/JoinSection';
+import SignUpSection from '../SignUpSection/SignUpSection';
 
 storiesOf('ReusableSections', module)
   .add('DonateSection', () => <DonateSection />)
-  .add('JoinSection', () => <JoinSection />);
+  .add('JoinSection', () => <JoinSection />)
+  .add('SignUpSection', () => <SignUpSection />);
