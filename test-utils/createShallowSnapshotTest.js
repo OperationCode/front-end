ffb0c12c40e8e39b6f175d52fffb6090022237c2
Kyle Holmberg <inbox@kylemh.com>
2018-07-28 01:44:50 -0700

    Split out test utils

--- test-utils/createShallowSnapshotTest.js
+++ test-utils/createShallowSnapshotTest.js
@@ -1,18 +1,5 @@
 /* eslint-env jest */
-import renderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
-
-/**
- * Used to create fully-rendered snapshot test.
- * Useful for reusable component smoke tests.
- *
- * @export
- * @param {*} Component
- */
-export function createSnapshotTest(Component) {
-  const tree = renderer.create(Component).toJSON();
-  expect(tree).toMatchSnapshot();
-}
 
 /**
  * Used to create a snapshot test off of surface leaves of the tree.
@@ -21,7 +8,7 @@ export function createSnapshotTest(Component) {
  * @export
  * @param {*} Component
  */
-export function createShallowSnapshotTest(Component) {
+export default (Component) => {
   const shallowRenderer = new ShallowRenderer();
   const tree = shallowRenderer.render(Component);
 
---
+++ test-utils/setupTests.js
@@ -0,0 +1,3 @@
+import 'jest-dom/extend-expect';
+
+import 'react-testing-library/cleanup-after-each';
