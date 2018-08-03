8bbc909c9dfee388928222d98a30d4c46954a6f1
Kyle Holmberg <inbox@kylemh.com>
2018-08-03 01:53:10 -0700

    Consume `.browserslistrc`

--- postcss.config.js
+++ postcss.config.js
@@ -14,7 +14,6 @@ module.exports = ({ file, options, env }) => {
       env === 'production'
         ? {
             ...options.autoprefixer,
-            browsers: ['> 1%', 'last 2 Chrome versions', 'not ie < 11'],
             flexbox: 'no-2009',
           }
         : false,
