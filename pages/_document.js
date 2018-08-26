01c284e0f884cbac4d5a73da733303a222b56c67
Kyle Holmberg <inbox@kylemh.com>
2018-08-26 16:33:19 -0700

    Set lang to english

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ import Document, { Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
