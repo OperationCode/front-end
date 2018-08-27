e86b51c5990c726a6771a708c7f15b544035a6a4
Kyle Holmberg <inbox@kylemh.com>
2018-08-26 17:21:41 -0700

    Avoid unnecessary DOM content in Nav

--- components/nav.js
+++ components/nav.js
@@ -5,17 +5,12 @@ function Nav() {
     <nav>
       <ul>
         <li>
-          <Link prefetch href="/">
+          <Link href="/" prefetch>
             Home
           </Link>
         </li>
         <li>
-          <Link prefetch href="/404">
-            404
-          </Link>
-        </li>
-        <li>
-          <Link prefetch href="/test">
+          <Link href="/test" prefetch>
             Test
           </Link>
         </li>
