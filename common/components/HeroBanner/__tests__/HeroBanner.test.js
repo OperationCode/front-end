dd1f0694b9048207604d3401d2586fb4f2ef9115
Kyle Holmberg <inbox@kylemh.com>
2018-08-28 12:14:43 -0700

    Prevent prettier formatting issue

--- common/components/HeroBanner/__tests__/HeroBanner.test.js
+++ common/components/HeroBanner/__tests__/HeroBanner.test.js
@@ -5,9 +5,13 @@ import createSnapshotTest from 'test-utils/createSnapshotTest';
 import { s3 } from 'common/constants/urls';
 import HeroBanner from '../HeroBanner';
 
+// TODO: Remove this
+/* prettier-disable */
 describe('HeroBanner', () => {
   test('should render with just required props passed', () => {
-    createSnapshotTest(<HeroBanner title="Test" imageSource={`${s3}heroBanner/stock_family-2.jpg`} />);
+    createSnapshotTest(
+      <HeroBanner title="Test" imageSource={`${s3}heroBanner/stock_family-2.jpg`} />,
+    );
   });
 
   test('should render properly with all props assigned', () => {
