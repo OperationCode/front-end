d9ff138430ee05100076b143cfb34720ddad36f6
Kyle Holmberg <inbox@kylemh.com>
2018-08-28 11:54:15 -0700

    Update story

--- common/components/HeroBanner/__stories__/HeroBanner.stories.js
+++ common/components/HeroBanner/__stories__/HeroBanner.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
+import { s3 } from 'common/constants/urls';
 import HeroBanner from '../HeroBanner';
 
 storiesOf('Common/HeroBanner', module)
@@ -13,10 +14,9 @@ storiesOf('Common/HeroBanner', module)
       <HeroBanner
         imageSrc={text(
           'imageSrc',
-          'https://s3.amazonaws.com/operationcode-assets/heroBanners/churchill.jpg',
+          `${s3}heroBanners/stock_family-2.jpg`,
         )}
         title={text('title', 'Hero Banner')}
-        subtitle={text('subtitle', '')}
       >
         {text('children', '')}
       </HeroBanner>
