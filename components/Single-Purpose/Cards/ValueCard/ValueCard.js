dda251fddb8a4f379d80ad4e774922d9ec562017
Kyle Holmberg <inbox@kylemh.com>
2018-08-28 21:10:31 -0700

    Convert ES6 function to vanilla

--- components/Single-Purpose/Cards/ValueCard/ValueCard.js
+++ components/Single-Purpose/Cards/ValueCard/ValueCard.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import Card from 'common/components/Card/Card';
 import styles from './ValueCard.css';
 
-const ValueCard = ({ name, description }) => (
-  <Card className={styles.valueCard} hasAnimationOnHover={false}>
-    <h3>{name}</h3>
-    <p>{description}</p>
-  </Card>
-);
-
 ValueCard.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
+
+function ValueCard({ description, name }) {
+  return (
+    <Card className={styles.ValueCard} hasAnimationOnHover={false}>
+      <h3 className={styles.name}>{name}</h3>
+      <p>{description}</p>
+    </Card>
+  );
+}
 
 export default ValueCard;
