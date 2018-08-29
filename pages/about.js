bbea6bf2dfb99649b28bc967d61182ce7c4aedc0
Kyle Holmberg <inbox@kylemh.com>
2018-08-28 21:23:26 -0700

    Encapuslate all cards into a flex grid

--- pages/about.js
+++ pages/about.js
@@ -18,9 +18,10 @@ export default () => (
     />
 
     <Section theme="white">
-      <h6>Operation Code is a non-profit charity helping the military
-        community learn software development, enter the
-        tech industry, and code the future.</h6>
+      <h6>
+        Operation Code is a non-profit charity helping the military community learn software
+        development, enter the tech industry, and code the future.
+      </h6>
       <p>
         We at Operation Code strive to provide an efficient way into a tech career for veterans and
         their families. We work directly with Senators, Congressmen, and Congresswomen to allow
@@ -45,36 +46,38 @@ export default () => (
     </Section>
 
     <Section theme="mist" title="What We Do">
-      <ImageCard
-        title="Mentorship Program"
-        imageSource={`${s3}stock_teamwork-1.jpg`}
-        cardText="Our mentorship program connects members with seasoned software developers
-            to help you progress and achieve your goals."
-      />
-      <ImageCard
-        title="Code School Scholarships"
-        imageSource={`${s3}photo_red-hat-summit-2.jpg`}
-        cardText="Our scholarships to code schools provide opportunities for the
-            military community to kickstart their careers in software development."
-      />
-      <ImageCard
-        title="Career Services"
-        imageSource={`${s3}stock_paired-programming.jpg`}
-        cardText="Our career services team provides job opportunities, career guidance and
-            resume reviews, and prep for technical interviews."
-      />
-      <ImageCard
-        title="Conference Scholarships"
-        imageSource={`${s3}stock_laptop-working.jpg`}
-        cardText="We partner with tech conferences around the country and offers
-            scholarship tickets to events throughout the year."
-      />
-      <ImageCard
-        title="Community Events"
-        imageSource={`${s3}photo_oc-seattle-meetup.jpg`}
-        cardText="Join one of our chapters throughout the country at our local meetups,
-            and learn with fellow members of the military community."
-      />
+      <div className={styles.flexGrid}>
+        <ImageCard
+          title="Mentorship Program"
+          imageSource={`${s3}stock_teamwork-1.jpg`}
+          cardText="Our mentorship program connects members with seasoned software developers
+              to help you progress and achieve your goals."
+        />
+        <ImageCard
+          title="Code School Scholarships"
+          imageSource={`${s3}photo_red-hat-summit-2.jpg`}
+          cardText="Our scholarships to code schools provide opportunities for the
+              military community to kickstart their careers in software development."
+        />
+        <ImageCard
+          title="Career Services"
+          imageSource={`${s3}stock_paired-programming.jpg`}
+          cardText="Our career services team provides job opportunities, career guidance and
+              resume reviews, and prep for technical interviews."
+        />
+        <ImageCard
+          title="Conference Scholarships"
+          imageSource={`${s3}stock_laptop-working.jpg`}
+          cardText="We partner with tech conferences around the country and offers
+              scholarship tickets to events throughout the year."
+        />
+        <ImageCard
+          title="Community Events"
+          imageSource={`${s3}photo_oc-seattle-meetup.jpg`}
+          cardText="Join one of our chapters throughout the country at our local meetups,
+              and learn with fellow members of the military community."
+        />
+      </div>
     </Section>
 
     <Section theme="white" title="Mission">
@@ -89,56 +92,58 @@ export default () => (
       </p>
     </Section>
     <Section theme="mist" title="Core Values">
-      <ValueCard
-        name="Integrity"
-        description="We hold ourselves accountable for obtaining results
+      <div className={styles.flexGrid}>
+        <ValueCard
+          name="Integrity"
+          description="We hold ourselves accountable for obtaining results
             that fulfill our Mission and work towards our Vision.
             As leaders, we are responsible for what we do, or fail to do.
             We act consistently with Operation Code's mission, being honest
             in what we do and say, and accept responsibility for our collective
             and individual actions."
-      />
-      <ValueCard
-        name="Transparency"
-        description="By being transparent in everything Operation Code does,
+        />
+        <ValueCard
+          name="Transparency"
+          description="By being transparent in everything Operation Code does,
             from our open source code base to our financial operations, our
             community can trust that we will do our best with the resources we're given."
-      />
-      <ValueCard
-        name="Agility"
-        description="We run fast and we run lean. When our operating environment changes,
+        />
+        <ValueCard
+          name="Agility"
+          description="We run fast and we run lean. When our operating environment changes,
             and new challenges emerge, we're ready to respond. We communicate as early and as
             often as possible, and default to overcommunication in all of our interactions.
             We always look for a better, more effective, and more efficient way to run our
             operations."
-      />
-      <ValueCard
-        name="Curiosity"
-        description="Innovation is central to our mindset. We maintain an attitude of
+        />
+        <ValueCard
+          name="Curiosity"
+          description="Innovation is central to our mindset. We maintain an attitude of
             continuous improvement, and we constantly look for new and better ways to
             serve our community - we owe them nothing less."
-      />
-      <ValueCard
-        name="Responsibility"
-        description="We exist to meet the needs of the military community.
+        />
+        <ValueCard
+          name="Responsibility"
+          description="We exist to meet the needs of the military community.
             We will humbly remember that their service was of the highest order and
             that Operation Code works for them. When it comes to the welfare and
             well-being of our community, the buck stops here."
-      />
-      <ValueCard
-        name="Clarity"
-        description="In the words of the US Army Signal Corps, we get the
+        />
+        <ValueCard
+          name="Clarity"
+          description="In the words of the US Army Signal Corps, we get the
             message through. We ensure that we are clear and concise in how we
             communicate, in support of our operations. We are considerate of the
             sender and the receiver, and communicate in the manner that best suits
             the needs of the mission and the vision."
-      />
-      <ValueCard
-        name="Community"
-        description="We look out for the people to our left and right,
+        />
+        <ValueCard
+          name="Community"
+          description="We look out for the people to our left and right,
             nd always keep a hand free to help. We listen to our community
             and our people to ensure we are working towards the mission and the vision."
-      />
+        />
+      </div>
     </Section>
   </>
 );
