44067e5557c9ad8d69b876439fbf0f342479044a
Peter Eck <jpeckiii@gmail.com>
2018-08-29 18:26:19 -0500

    Update ErrorDisplay test to include proper statusCode

--- components/ErrorDisplay/__tests__/ErrorDisplay.test.js
+++ components/ErrorDisplay/__tests__/ErrorDisplay.test.js
@@ -1,25 +1,14 @@
 /* eslint-env jest */
 import React from 'react';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
-import renderer from "react-test-renderer";
-
 import ErrorDisplay from "../ErrorDisplay";
 
 describe('ErrorDisplay', () => {
     test("should render with just required props", () => {
         createSnapshotTest(
-            <ErrorDisplay statusCode="Test Code">
-                Test
-            </ErrorDisplay>,
-        );
-    });
-
-    test("should render properly with all props assigned", () => {
-        createSnapshotTest(
-            <ErrorDisplay
-                statusCode="Test Code">
-                Test
+            <ErrorDisplay statusCode={404}>
+                Err! 
             </ErrorDisplay>
         );
     });
-})
+});
