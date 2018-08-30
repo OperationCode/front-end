2840a5c19c7cd8b5c9f7c3d8d270f2fa029ca638
Peter Eck <jpeckiii@gmail.com>
2018-08-30 18:46:41 -0500

    Fix format issue with SignUpSection.test.js

--- components/ReusableSections/SignUpSection/__tests__/SignUpSection.test.js
+++ components/ReusableSections/SignUpSection/__tests__/SignUpSection.test.js
@@ -4,7 +4,7 @@ import createShallowSnapshotTest from 'test-utils/createShallowSnapshotTest';
 import SignUpSection from "../SignUpSection";
 
 describe('SignUpSection', () => {
-  test("it should render properly with no props", () => {
+  test('it should render properly with no props', () => {
     createShallowSnapshotTest(<SignUpSection>Test</SignUpSection>);
   });
 });
