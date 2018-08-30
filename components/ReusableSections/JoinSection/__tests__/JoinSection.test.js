6c5ea6c11622561c91971140e48f26fede9c974a
Peter Eck <jpeckiii@gmail.com>
2018-08-30 18:17:34 -0500

    Add JoinSection snapshot test

---
+++ components/ReusableSections/JoinSection/__tests__/JoinSection.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import createShallowSnapshotTest from 'test-utils/createShallowSnapshotTest';
+import JoinSection from '../JoinSection';
+
+describe('JoinSection', () => {
+  test('it should render properly with no props', () => {
+    createShallowSnapshotTest(<JoinSection>Test</JoinSection>);
+  });
+});
