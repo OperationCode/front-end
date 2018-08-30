00d6b5f1725c3cb028ac6059f4d2ba7de13b9efb
Peter Eck <jpeckiii@gmail.com>
2018-08-30 18:41:15 -0500

    Add ImageCard snapshot test

---
+++ components/Single-Purpose/Cards/ImageCard/__tests__/ImageCard.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import createShallowSnapshotTest from "test-utils/createShallowSnapshotTest";
+
+import ImageCard from "../ImageCard";
+
+describe('ImageCard', () => {
+  test('it should render properly with required props', () => {
+    createShallowSnapshotTest(
+        <ImageCard
+            cardTest='Test text'
+            imageSource='image.jpg'
+            title='Test title'
+        >
+            Test
+        </ImageCard>
+    );
+  });
+
+  test('should render properly with some props assigned', () => {
+      createShallowSnapshotTest(
+          <ImageCard
+            buttonText="Test me!"
+            cardText="This is the card"
+            imageSource="image.jpg"
+            link="www.thisIsTheLink.com"
+            title="Title Tester" >
+            Test
+          </ImageCard>
+      );
+  });
+});
