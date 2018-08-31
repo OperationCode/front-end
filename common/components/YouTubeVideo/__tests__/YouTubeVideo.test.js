423454d3b3bbeb0f5fc5fa8b2c2d85a1aa06e0de
Peter Eck <jpeckiii@gmail.com>
2018-08-30 19:34:02 -0500

    Add YouTubeVideo snapshot test

---
+++ common/components/YouTubeVideo/__tests__/YouTubeVideo.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import createShallowSnapshotTest from "test-utils/createShallowSnapshotTest";
+
+import YouTubeVideo from "../YouTubeVideo";
+
+describe('YouTubeVideo', () => {
+  test('it should render properly with required props assigned', () => {
+    createShallowSnapshotTest(
+        <YouTubeVideo
+            videoId='12345'
+        >
+            test
+        </YouTubeVideo>
+    );
+  });
+
+  test('it should render properly with some props assigned', () => {
+    createShallowSnapshotTest(
+        <YouTubeVideo
+            className='YouTube-video'
+            height='450px'
+            width='875px'
+            videoId='12395'
+        >
+            test
+        </YouTubeVideo>
+    );
+  });
+});
