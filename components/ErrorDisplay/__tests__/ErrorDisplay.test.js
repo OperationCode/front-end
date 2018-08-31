81600682ca3f024d0ee3d10b3690d35dddf762bd
Kyle Holmberg <inbox@kylemh.com>
2018-08-31 11:27:32 -0700

    Add new line between imports

--- components/ErrorDisplay/__tests__/ErrorDisplay.test.js
+++ components/ErrorDisplay/__tests__/ErrorDisplay.test.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 import React from 'react';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
+
 import ErrorDisplay from '../ErrorDisplay';
 
 describe('ErrorDisplay', () => {
