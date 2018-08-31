25e0284d468f5fd7953cba3f1b22383d1c29da70
Peter Eck <jpeckiii@gmail.com>
2018-08-30 19:04:08 -0500

    Add SuccessStory snapshot test

---
+++ components/Single-Purpose/SuccessStory/__tests__/SuccessStory.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import createShallowSnapshotTest from "test-utils/createShallowSnapshotTest";
+
+import SuccessStory from "../SuccessStory";
+
+describe('SuccessStory', () => {
+  test('it should render properly with all required props', () => {
+    createShallowSnapshotTest(
+        <SuccessStory
+            imageSource='image.png'
+            quote='Best test of my life!'
+            title='Great testing culture' >
+            Test
+        </SuccessStory>
+    );
+  });
+});
