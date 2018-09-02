c532cf4f4b14422c126a49487c68e51029f8c463
Kyle Holmberg <inbox@kylemh.com>
2018-09-02 12:01:25 -0700

    Adjust implementation of <SocialMediaItem> and change styles

--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import fbImage from 'images/icons/Facebook-Icon.svg';
-import ghImage from 'images/icons/GitHub-Icon.svg';
-import twtImage from 'images/icons/Twitter-Icon.svg';
-import liImage from 'images/icons/hrefedIn-Icon.svg';
+import FacebookLogo from 'static/images/icons/facebook_logo.svg';
+import GitHubLogo from 'static/images/icons/github_logo.svg';
+import TwitterLogo from 'static/images/icons/twitter_logo.svg';
+import LinkedInLogo from 'static/images/icons/linkedin_logo.svg';
+import PinterestLogo from 'static/images/icons/pinterest_logo.svg';
 import SocialMediaContainer from './SocialMediaContainer/SocialMediaContainer';
 import SocialMediaItem from './SocialMediaItem/SocialMediaItem';
+import styles from './SocialMedia.css';
 
-// TODO: Refactor this whole folder
 function SocialMedia() {
   return (
     <SocialMediaContainer>
       <SocialMediaItem
-        imageSource={fbImage}
+        svg={<FacebookLogo className={styles.logo} />}
         alt="Facebook"
         href="https://facebook.com/operationcode.org"
       />
-      <SocialMediaItem imageSource={ghImage} alt="Github" href="https://github.com/operationcode" />
       <SocialMediaItem
-        imageSource={twtImage}
+        svg={<GitHubLogo className={styles.logo} />}
+        alt="GitHub"
+        href="https://github.com/operationcode"
+      />
+      <SocialMediaItem
+        svg={<TwitterLogo className={styles.logo} />}
         alt="Twitter"
         href="https://twitter.com/operation_code"
       />
       <SocialMediaItem
-        imageSource={liImage}
+        svg={<LinkedInLogo className={styles.logo} />}
         alt="LinkedIn"
         href="https://www.linkedin.com/groups/13400924"
+      />
+      <SocialMediaItem
+        svg={<PinterestLogo className={styles.logo} />}
+        alt="Pinterest"
+        href="https://www.pinterest.com/operationcode/"
       />
     </SocialMediaContainer>
   );
