ec14341608a6a01b480cf38a094bf5ce615e9788
Kyle Holmberg <inbox@kylemh.com>
2018-09-02 12:00:26 -0700

    Change <SocialMediaItem> to render SVG as render prop and update test

--- components/SocialMedia/SocialMediaItem/SocialMediaItem.js
+++ components/SocialMedia/SocialMediaItem/SocialMediaItem.js
@@ -6,14 +6,14 @@ import styles from './SocialMediaItem.css';
 SocialMediaItem.propTypes = {
   alt: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
-  imageSource: PropTypes.string.isRequired,
+  svg: PropTypes.node.isRequired,
 };
 
-function SocialMediaItem({ alt, href, imageSource }) {
+function SocialMediaItem({ alt, href, svg }) {
   return (
     <div className={styles.SocialMediaItem}>
       <OutboundLink analyticsEventLabel={`Social Media Item: ${alt}`} hasIcon={false} href={href}>
-        <img src={imageSource} alt={alt} />
+        {svg}
       </OutboundLink>
     </div>
   );
--- components/SocialMedia/SocialMediaItem/__tests__/SocialMediaItem.test.js
+++ components/SocialMedia/SocialMediaItem/__tests__/SocialMediaItem.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
+import FacebookLogo from 'static/images/icons/facebook_logo.svg';
 import SocialMediaItem from '../SocialMediaItem';
 
 describe('SocialMediaItem', () => {
   test('it should render properly with required (all) props', () => {
     createSnapshotTest(
       <SocialMediaItem
-        href="https://facebook.com"
-        imageSource="/static/images/icons/facebook_logo.svg"
         alt="Facebook"
+        href="https://facebook.com"
+        svg={<FacebookLogo fill="blue" />}
       />,
     );
   });
