e0d6fb7266f307eb31e36727e6bc929c82a399f8
Peter Eck <jpeckiii@gmail.com>
2018-09-02 14:31:02 -0500

    Add Timeline and TimelineEvent snapshot tests

---
+++ components/Timeline/TimelineEvent/__tests__/TimelineEvent.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import createShallowSnapshotTest from 'test-utils/createShallowSnapshotTest';
+
+import TimelineEvent from "../TimelineEvent";
+
+describe('TimelineEvent', () => {
+  test('should render properly with all required props', () => {
+    createShallowSnapshotTest(
+        <TimelineEvent
+            title='test title'
+            content='here is some test content' />
+    );
+  });
+});
