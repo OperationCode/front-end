6ec1c43b07c5c3e6e9be0538f9134cf9f9185713
Kyle Holmberg <inbox@kylemh.com>
2018-09-02 18:04:43 -0700

    Mock Next Router

--- jest.setup.js
+++ jest.setup.js
@@ -4,7 +4,19 @@ import 'jest-enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+/* MOCKS */
+// Storybook Info Addon
 jest.mock('@storybook/addon-info', () => ({
   withInfo: () => storyFn => storyFn,
   setDefaults: () => {},
+}));
+
+
+// Next Router
+jest.mock('next/router', () => ({
+  push: () => {},
+  prefetch: () => {},
+  replace: () => {},
+  route: '/mock-route',
+  withRouter: (Component) => Component,
 }));
