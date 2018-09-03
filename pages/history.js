bfe29eb5ff39ed036c651171e5cf58726e4a4da2
Kyle Holmberg <inbox@kylemh.com>
2018-09-03 00:12:18 -0700

    Add styles to history page

--- pages/history.js
+++ pages/history.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { s3 } from 'common/constants/urls';
-import Section from '../common/components/Section/Section';
-import HeroBanner from '../common/components/HeroBanner/HeroBanner';
-import Timeline from '../components/Timeline/Timeline';
+import Section from 'common/components/Section/Section';
+import HeroBanner from 'common/components/HeroBanner/HeroBanner';
+import Timeline from 'components/Timeline/Timeline';
+import styles from './styles/history.css';
 
 export default function() {
   return (
     <>
       <HeroBanner
+        className={styles.hero}
         imageSource={`${s3}heroBanners/colin-powell.jpg`}
-        title="&quot;There are no secrets to success. It is the result of preparation, 
-        hard work, learning from failure.&quot;"
+        title="History"
       >
-        <h3>-Colin Powell</h3>
+        <>
+          <blockquote className={styles.quote}>
+            &ldquo;There are no secrets to success. It is the result of preparation, hard work,
+            learning from failure.&rdquo;
+          </blockquote>
+          <span className={styles.author}>- Colin Powell</span>
+        </>
       </HeroBanner>
 
-      <Section theme="white" title="History">
+      <Section theme="white" hasHeadingLines={false}>
         <Timeline />
       </Section>
     </>
