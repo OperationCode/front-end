1a440713cc675e116d75da78928baf55aac5cdba
Kyle Holmberg <inbox@kylemh.com>
2018-09-02 23:49:49 -0700

    Create history data object and reduce repeat code - update tests

--- components/Timeline/TimelineEvent/TimelineEvent.js
+++ components/Timeline/TimelineEvent/TimelineEvent.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TimelineEvent.css';
 
-const TimelineEvent = ({ title, content }) => (
-  <div className={styles.eventContainer}>
-    <h4 className={styles.eventTitle}>{title}</h4>
-
-    <div className={styles.eventContent}>{content}</div>
-  </div>
-);
-
 TimelineEvent.propTypes = {
-  title: PropTypes.string.isRequired,
   content: PropTypes.oneOfType([PropTypes.string, PropTypes.array, PropTypes.node]).isRequired,
+  title: PropTypes.string.isRequired,
 };
+
+function TimelineEvent({ content, title }) {
+  return (
+    <div className={styles.eventContainer}>
+      <h4 className={styles.eventTitle}>{title}</h4>
+
+      <div className={styles.eventContent}>{content}</div>
+    </div>
+  );
+}
 
 export default TimelineEvent;
--- components/Timeline/TimelineEvent/__tests__/TimelineEvent.test.js
+++ components/Timeline/TimelineEvent/__tests__/TimelineEvent.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import createShallowSnapshotTest from 'test-utils/createShallowSnapshotTest';
+import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import TimelineEvent from '../TimelineEvent';
 
 describe('TimelineEvent', () => {
   test('should render properly with all required props', () => {
-    createShallowSnapshotTest(
-      <TimelineEvent title="test title" content="here is some test content" />,
-    );
+    createSnapshotTest(<TimelineEvent title="test title" content="here is some test content" />);
   });
 });
