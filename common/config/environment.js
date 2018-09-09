15b8c850a3121ff86bb1a999ecfe333d1ab35e6a
Kyle Holmberg <inbox@kylemh.com>
2018-09-08 21:31:10 -0700

    Export config as variables

--- common/config/environment.js
+++ common/config/environment.js
@@ -1,3 +1,3 @@
-export default {
-  apiUrl: process.env.OC_API_URL || 'https://staging.api.operationcode.org/api/v1',
-};
+export const apiUrl = process.env.OC_API_URL || 'https://api.operationcode.org/api/v1';
+export const facebookKey = process.env.OC_FACEBOOK_KEY;
+export const googleKey = process.env.OC_GOOGLE_KEY;
