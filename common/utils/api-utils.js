2b46e30d951d9b0038b6e50f82314fb9f1bd0771
Kyle Holmberg <inbox@kylemh.com>
2018-09-08 20:30:55 -0700

    Simplify API utilities and map multiple requests

--- common/utils/api-utils.js
+++ common/utils/api-utils.js
@@ -1,68 +1,19 @@
 import axios from 'axios';
-import config from 'config/environment';
+import { apiUrl } from 'common/config/environment';
 import Cookies from 'universal-cookie';
 
-// TODO: Map config from environment
-export const setAuthorizationHeader = () => {
+const setAuthorizationHeader = () => {
   const cookies = new Cookies();
   return { Authorization: `bearer ${cookies.get('token')}` };
 };
 
-function makeGenericGet(endpoint) {
-  const authHeader = setAuthorizationHeader();
-  return axios
-    .get(`${config.backendUrl}/${endpoint}`, { headers: authHeader })
+export const get = endpoint =>
+  axios
+    .get(`${apiUrl}/${endpoint}`, { headers: setAuthorizationHeader() })
     .then(({ data }) => data);
-}
 
-export function postBackend(path, body) {
-  const authHeader = setAuthorizationHeader();
-  return axios.post(`${config.backendUrl}/${path}`, body, { headers: authHeader });
-}
+export const post = (path, body) =>
+  axios.post(`${apiUrl}/${path}`, body, { headers: setAuthorizationHeader() });
 
-export function patchBackend(path, body) {
-  const authHeader = setAuthorizationHeader();
-  return axios.patch(`${config.backendUrl}/${path}`, body, { headers: authHeader });
-}
-
-export const getServices = () => makeGenericGet('services');
-
-export const getMentors = () => makeGenericGet('mentors');
-export const getMentor = id => makeGenericGet(`mentors/${id}`);
-
-export const getRequests = () => makeGenericGet('requests');
-
-export const getScholarships = () => makeGenericGet('scholarships');
-export const getScholarship = id => makeGenericGet(`scholarships/${id}`);
-
-export function postRequest({ language, additionalDetails, mentor, service }) {
-  const authHeader = setAuthorizationHeader();
-
-  return axios.post(
-    `${config.backendUrl}/requests`,
-    {
-      request: {
-        details: additionalDetails,
-        requested_mentor_id: mentor,
-        service_id: service,
-        language,
-      },
-    },
-    { headers: authHeader },
-  );
-}
-
-export function updateRequest({ request, status, mentor }) {
-  const authHeader = setAuthorizationHeader();
-
-  return axios.patch(
-    `${config.backendUrl}/requests/${request}`,
-    {
-      request: {
-        status,
-        mentor,
-      },
-    },
-    { headers: authHeader },
-  );
-}
+export const patch = (path, body) =>
+  axios.patch(`${apiUrl}/${path}`, body, { headers: setAuthorizationHeader() });
