7f6010b9e694ef1cde7b79a2d30b247523e6e2f1
JohnGoure <johnsgithub@gmail.com>
2018-09-14 16:14:14 -0500

    Moved the content so that everything is rendered inside the return.

--- pages/faq.js
+++ pages/faq.js
@@ -4,32 +4,32 @@ import Question from 'components/faq/question';
 import QuestionAnswerData from 'components/faq/questions';
 import styles from './styles/faq.css';
 
-const FAQ = () => {
-  const generalQuestions = QuestionAnswerData().general.map(faq => (
-    <Question question={faq.question} answer={faq.answer} key={faq.question} />
-  ));
-  const donationQuestions = QuestionAnswerData().donation.map(faq => (
-    <Question question={faq.question} answer={faq.answer} key={faq.question} />
-  ));
-  const volunteerQuestions = QuestionAnswerData().volunteer.map(faq => (
-    <Question question={faq.question} answer={faq.answer} key={faq.question} />
-  ));
-
-  return (
+const FAQ = () => (
     <Section title="General Questions" theme="white">
       <Section theme="white" headingLines={false}>
-        <div className={styles.container}>{generalQuestions}</div>
+        <div className={styles.container}>
+          {QuestionAnswerData().general.map(faq => (
+            <Question question={faq.question} answer={faq.answer} key={faq.question} />
+          ))}
+        </div>
       </Section>
       <Section title="Donation Questions" theme="white">
         <br />
-        <div className={styles.container}>{donationQuestions}</div>
+        <div className={styles.container}>
+          {QuestionAnswerData().donation.map(faq => (
+            <Question question={faq.question} answer={faq.answer} key={faq.question} />
+          ))}
+        </div>
       </Section>
       <Section title="Volunteer Questions" theme="white">
         <br />
-        <div className={styles.container}>{volunteerQuestions}</div>
+        <div className={styles.container}>
+          {QuestionAnswerData().volunteer.map(faq => (
+            <Question question={faq.question} answer={faq.answer} key={faq.question} />
+          ))}
+        </div>
       </Section>
     </Section>
   );
-};
 
 export default FAQ;
