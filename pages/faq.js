6789073ecc9acba51e5015b52bf1940c7f1eb103
JohnGoure <johnsgithub@gmail.com>
2018-09-18 13:42:55 -0500

    Removed function call, changed questions to an object.

--- pages/faq.js
+++ pages/faq.js
@@ -5,31 +5,31 @@ import QuestionAnswerData from 'components/faq/questions';
 import styles from './styles/faq.css';
 
 const FAQ = () => (
-    <Section title="General Questions" theme="white">
-      <Section theme="white" headingLines={false}>
-        <div className={styles.container}>
-          {QuestionAnswerData().general.map(faq => (
-            <Question question={faq.question} answer={faq.answer} key={faq.question} />
-          ))}
-        </div>
-      </Section>
-      <Section title="Donation Questions" theme="white">
-        <br />
-        <div className={styles.container}>
-          {QuestionAnswerData().donation.map(faq => (
-            <Question question={faq.question} answer={faq.answer} key={faq.question} />
-          ))}
-        </div>
-      </Section>
-      <Section title="Volunteer Questions" theme="white">
-        <br />
-        <div className={styles.container}>
-          {QuestionAnswerData().volunteer.map(faq => (
-            <Question question={faq.question} answer={faq.answer} key={faq.question} />
-          ))}
-        </div>
-      </Section>
+  <Section title="General Questions" theme="white">
+    <Section theme="white" headingLines={false}>
+      <div className={styles.container}>
+        {QuestionAnswerData.general.map(faq => (
+          <Question question={faq.question} answer={faq.answer} key={faq.question} />
+        ))}
+      </div>
     </Section>
-  );
+    <Section title="Donation Questions" theme="white">
+      <br />
+      <div className={styles.container}>
+        {QuestionAnswerData.donation.map(faq => (
+          <Question question={faq.question} answer={faq.answer} key={faq.question} />
+        ))}
+      </div>
+    </Section>
+    <Section title="Volunteer Questions" theme="white">
+      <br />
+      <div className={styles.container}>
+        {QuestionAnswerData().volunteer.map(faq => (
+          <Question question={faq.question} answer={faq.answer} key={faq.question} />
+        ))}
+      </div>
+    </Section>
+  </Section>
+);
 
 export default FAQ;
