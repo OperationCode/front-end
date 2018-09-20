c5ce9e1c5f78f2b87a5f53cb756773e8828fafc1
JohnGoure <johnsgithub@gmail.com>
2018-09-19 19:43:56 -0500

    Added coveragePathIgnorePattern for /components/faq/questions.js

--- jest.config.js
+++ jest.config.js
@@ -36,6 +36,7 @@ module.exports = {
 
     // No real logic to test here
     '<rootDir>/common/utils/api-utils.js',
+    '<rootDir>/components/faq/questions.js',
 
     // Ignore Next.js files
     '<rootDir>/components/head.js',
