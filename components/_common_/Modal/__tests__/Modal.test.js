c7db8cf1f04f17cca5b107e425448f4c6cdaa991
Márcio Coelho <marcio.coelho@tekzenit.com>
2018-10-02 22:15:31 +0100

    100% coverage to Modal

--- components/_common_/Modal/__tests__/Modal.test.js
+++ components/_common_/Modal/__tests__/Modal.test.js
@@ -1,13 +1,19 @@
 /* eslint-env jest */
 import React from 'react';
+import { shallow } from 'enzyme';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 import createShallowSnapshotTest from 'test-utils/createShallowSnapshotTest';
+import ReactGA from 'react-ga';
 
 import Modal from '../Modal';
 
 describe('Modal', () => {
   test('should render with just required props passed', () => {
-    createSnapshotTest(<Modal screenReaderLabel="Test">Test</Modal>);
+    createSnapshotTest(
+      <Modal screenReaderLabel="Test" onRequestClose={() => {}}>
+        Test
+      </Modal>,
+    );
   });
 
   test('should render properly with some props assigned and not being open', () => {
@@ -35,5 +41,23 @@ describe('Modal', () => {
         Test
       </Modal>,
     );
+  });
+
+  test('should call ReactGA when in prod environment', () => {
+    /* eslint-disable no-console */
+    ReactGA.initialize('foo', { testMode: true });
+
+    process.env.NODE_ENV = 'production';
+    process.env.GOOGLE_ANALYTICS_KEY = 'debug';
+
+    const props = {
+      hasCloseIcon: false,
+      onRequestClose: () => {},
+      screenReaderLabel: 'Test',
+    };
+
+    shallow(<Modal {...props}>Testing</Modal>);
+
+    expect(ReactGA.testModeAPI.calls).toContainEqual(['send', 'pageview', '/modal/Test']);
   });
 });
