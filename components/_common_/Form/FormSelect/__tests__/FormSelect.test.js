582bbd60215d1b8b7e1aa8907eda17aa4dc511dc
Jim Ulle <jrulle@gmail.com>
2018-10-01 23:15:51 -0400

    added unit tests to get forms to 100% coverage

--- components/_common_/Form/FormSelect/__tests__/FormSelect.test.js
+++ components/_common_/Form/FormSelect/__tests__/FormSelect.test.js
@@ -1,15 +1,37 @@
 /* eslint-env jest */
 import React from 'react';
+import { mount } from 'enzyme';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import FormSelect from '../FormSelect';
 
 describe('FormSelect', () => {
+  let options;
+  beforeEach(() => {
+    options = [{ label: 'test 1', value: 'TEST1' }, { label: 'test 2', value: 'TEST2' }];
+  });
   test('should render with just required props passed', () => {
-    createSnapshotTest(
-      <FormSelect
-        options={[{ label: 'test 1', value: 'TEST1' }, { label: 'test 2', value: 'TEST2' }]}
-      />,
+    createSnapshotTest(<FormSelect options={options} />);
+  });
+  test('should utilize onChange prop', () => {
+    const changeSpy = jest.fn();
+    const formSelectInstance = mount(<FormSelect options={options} onChange={changeSpy} />);
+    expect(changeSpy).not.toHaveBeenCalled();
+    formSelectInstance.find('select').simulate('blur', { target: { value: 1 } });
+    formSelectInstance.update();
+    expect(changeSpy).toHaveBeenCalled();
+  });
+  test('should utilize validationFunc prop', () => {
+    const validationFuncSpy = jest.fn();
+    const formSelectInstance = mount(
+      <FormSelect options={options} validationFunc={validationFuncSpy} />,
     );
+    expect(validationFuncSpy).not.toHaveBeenCalled();
+    formSelectInstance.find('select').simulate('blur', { target: { value: 1 } });
+    formSelectInstance.update();
+    expect(validationFuncSpy).toHaveBeenCalled();
+  });
+  test('should render with prompt prop', () => {
+    createSnapshotTest(<FormSelect options={options} prompt="true" />);
   });
 });
--- components/_common_/Form/FormTextArea/__tests__/FormTextArea.test.js
+++ components/_common_/Form/FormTextArea/__tests__/FormTextArea.test.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 import React from 'react';
+import { mount } from 'enzyme';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import FormTextArea from '../FormTextArea';
@@ -7,5 +8,12 @@ import FormTextArea from '../FormTextArea';
 describe('FormTextArea', () => {
   test('should render with just required props passed', () => {
     createSnapshotTest(<FormTextArea />);
+  });
+  test('should call onChange handler on changes', async () => {
+    const formTextAreaInstance = mount(<FormTextArea onChange={jest.fn()} />);
+    expect(formTextAreaInstance.state().value).toEqual('');
+    formTextAreaInstance.find('textarea').simulate('change', { target: { value: 'test change' } });
+    formTextAreaInstance.update();
+    expect(formTextAreaInstance.state().value).toEqual('test change');
   });
 });
