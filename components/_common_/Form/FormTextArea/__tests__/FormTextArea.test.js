175fddd9fe0b4a3d089e9ed865e7ec9a61dbdbaa
Márcio Coelho <marcio.coelho@tekzenit.com>
2018-10-02 11:08:45 +0100

    100% coverage to FormTextArea

--- components/_common_/Form/FormTextArea/__tests__/FormTextArea.test.js
+++ components/_common_/Form/FormTextArea/__tests__/FormTextArea.test.js
@@ -1,11 +1,22 @@
 /* eslint-env jest */
 import React from 'react';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
+import { shallow } from 'enzyme';
 
 import FormTextArea from '../FormTextArea';
 
 describe('FormTextArea', () => {
   test('should render with just required props passed', () => {
     createSnapshotTest(<FormTextArea />);
+  });
+
+  test('should call onChange function when change text', () => {
+    const props = { onChange: jest.fn() };
+    const event = { target: { value: 'sometext' } };
+
+    const FormTextAreaShallowInstance = shallow(<FormTextArea id="test" {...props} />);
+    FormTextAreaShallowInstance.find('textarea').simulate('change', event);
+
+    expect(props.onChange).toHaveBeenCalledTimes(1);
   });
 });
