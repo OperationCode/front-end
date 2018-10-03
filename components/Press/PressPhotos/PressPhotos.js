28be4f3ca1af8f106ac08d5d5b0017f910e804e5
Kyle Holmberg <inbox@kylemh.com>
2018-10-02 23:17:03 -0700

    Modularize /press content

--- components/Press/PressPhotos/PressPhotos.js
+++ components/Press/PressPhotos/PressPhotos.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { s3 } from 'common/constants/urls';
 import styles from './PressPhotos.css';
 
 function PressPhotos() {
   return (
     <div className={styles.photos}>
       <img
-        src="/static/images/CodeConferenceLA.jpg"
+        src={`${s3}photo_code-conference-la.jpg`}
         alt="David Molina speaks to Code Conference LA 2016 attendees."
       />
       <img
-        src="/static/images/RedHat-Summit.jpg"
+        src={`${s3}photo_red-hat-summit-1.jpg`}
         alt="Operation Code members pose at Red Hat Summit 2017"
       />
       <img
-        src="/static/images/Node-Summit.jpg"
+        src={`${s3}photo_node-summit.jpg`}
         alt="Conrad Hollomon presents in front of the Node Summit 2016 audience."
       />
       <img
-        src="/static/images/AngelHack-Boston.jpg"
+        src={`${s3}photo_angelhack-boston.jpg`}
         alt="Operation Code developers pose at AngelHack Boston 2017."
       />
       <img
-        src="/static/images/Utah-Meetup.jpg"
+        src={`${s3}photo_utah-meetup.jpg`}
         alt="Ken Collier leads a discussion at the Utah Operation Code meetup."
       />
     </div>
