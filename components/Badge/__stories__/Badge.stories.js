edea22b471d4e3b93e986b67e572268d3b227184
Angela Lin <tigclaw@gmail.com>
2018-10-08 19:48:22 -0400

    Update storybook for Badge

--- components/Badge/__stories__/Badge.stories.js
+++ components/Badge/__stories__/Badge.stories.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
+import GithubIcon from 'static/images/icons/github_logo.svg';
+import TwitterIcon from 'static/images/icons/twitter_logo.svg';
+import PinterestIcon from 'static/images/icons/pinterest_logo.svg';
 
 import Badge from '../Badge';
 
+const icons = {
+  github: <GithubIcon />,
+  twitter: <TwitterIcon />,
+  pinterest: <PinterestIcon />,
+};
+
 storiesOf('Common/Badge', module)
   .addDecorator(withKnobs)
-  .add('default', withInfo()(() => <Badge>{text('children', 'PropTypes.string or .node')}</Badge>));
+  .add(
+    'default',
+    withInfo()(() => {
+      const iconName = select('svgComponent', Object.keys(icons), 'github');
+
+      return (
+        <Badge
+          svgComponent={icons[iconName]}
+          label={text('label', 'My Awesome Badge')}
+          labelClassName={text('labelClassName', 'test-class')}
+        />
+      );
+    }),
+  );
