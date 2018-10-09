965df370a4c519c897ca3f3e34a1a259a4927530
Kyle Holmberg <inbox@kylemh.com>
2018-10-09 02:06:43 -0700

    Remove unused functions and related tests

--- common/utils/__tests__/string-utils.test.js
+++ common/utils/__tests__/string-utils.test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { capitalizeFirstLetter, containsDashCharacter, containsWhiteSpace } from '../string-utils';
+import { capitalizeFirstLetter } from '../string-utils';
 
 describe('String Utilities', () => {
   describe('capitalizeFirstLetter', () => {
@@ -14,71 +14,5 @@ describe('String Utilities', () => {
     it('should return an empty string when passed undefined', () => {
       expect(capitalizeFirstLetter(undefined)).toStrictEqual('');
     });
-  });
-});
-
-describe('containsWhiteSpace', () => {
-  it('should return true when passed a string with white space(s)', () => {
-    const testString = 'Test String';
-    expect(containsWhiteSpace(testString)).toStrictEqual(true);
-  });
-
-  it('should return false when when passing a string without white space(s)', () => {
-    const testString = 'testString';
-    expect(containsWhiteSpace(testString)).toStrictEqual(false);
-  });
-
-  it('should throw an error when passed an object', () => {
-    expect(() => {
-      containsWhiteSpace({ a: 1, b: 2 });
-    }).toThrow('is not a string');
-  });
-
-  it('should throw an error when passed a number', () => {
-    expect(() => {
-      containsWhiteSpace(22);
-    }).toThrow('is not a string');
-  });
-
-  it('should throw an error when passed a boolean', () => {
-    expect(() => {
-      containsWhiteSpace(false);
-    }).toThrow('is not a string');
-  });
-
-  it('should throw an error when passed undefined', () => {
-    expect(() => {
-      containsWhiteSpace(undefined);
-    }).toThrow('is not a string');
-  });
-
-  it('should throw an error when passed an array', () => {
-    expect(() => {
-      containsWhiteSpace([1, 3, 4]);
-    }).toThrow('is not a string');
-  });
-});
-
-describe('containsDashCharacter', () => {
-  it('should return true when passed a string with dash(-) characters', () => {
-    const someString = 'some-string';
-    expect(containsDashCharacter(someString)).toStrictEqual(true);
-  });
-
-  it('should return false when passed a string without dash(-) characters', () => {
-    const someString = 'somestring';
-    expect(containsDashCharacter(someString)).toStrictEqual(false);
-  });
-
-  it('should throw an error when passed undefined', () => {
-    expect(() => containsDashCharacter(undefined)).toThrow('Cannot read property');
-  });
-
-  it('should throw an error when passed an object', () => {
-    expect(() => containsDashCharacter({ key: 'value' })).toThrow('is not a function');
-  });
-
-  it('should throw an error when passed an array', () => {
-    expect(() => containsDashCharacter(['item1', 'item2'])).toThrow('is not a string');
   });
 });
--- common/utils/string-utils.js
+++ common/utils/string-utils.js
@@ -16,37 +16,6 @@ function capitalizeFirstLetter(someString = '') {
   return stringCopy.charAt(0).toUpperCase() + stringCopy.slice(1);
 }
 
-/**
- * Checks to see if a string contains white space
- *
- * @export
- * @param {string} someString
- * @returns {boolean} Returns a boolean. True if a white space is found
- */
-function containsWhiteSpace(someString) {
-  if (typeof someString !== 'string') {
-    throw new Error(`${someString} is not a string`);
-  }
-
-  return /\s/.test(someString);
-}
-
-/**
- * Checks to see if a string contains dash (-) characters
- *
- * @export
- * @param {string} someString
- * @returns {boolean} Returns a boolean. True if (-) is found
- */
-function containsDashCharacter(someString) {
-  if (Array.isArray(someString)) {
-    throw new Error(`${someString} is not a string`);
-  }
-  return someString.indexOf('-') >= 0;
-}
-
 module.exports = {
   capitalizeFirstLetter,
-  containsWhiteSpace,
-  containsDashCharacter,
 };
