e115f3e3a845cd9c1d2b0650fed78662926534a9
Chris Galvan <chrisgalvanr@gmail.com>
2018-10-12 17:11:04 -0500

    feat: displaying partners and join our community components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'components/head';
+// eslint-disable-next-line max-len
+import JoinThrivingSection from 'components/ReusableSections/JoinThrivingSection/JoinThrivingSection';
+import Partners from 'components/Partners/Partners';
+// import styles from './styles/index.css';
 
 export default () => (
   <div>
     <Head title="Home" />
     <h1>Home</h1>
+    <Partners />
+    <JoinThrivingSection />
   </div>
 );
