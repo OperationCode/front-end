bfe2aacdfa0c7e68d3e15d4eade94cfb5def12fa
Angela Lin <tigclaw@gmail.com>
2018-10-14 18:29:39 -0400

    Delete social media items so only three remaining, update styling for responsive design

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,88 +3,104 @@ import Link from 'next/link';
 import classNames from 'classnames';
 import OutboundLink from 'components/_common_/OutboundLink/OutboundLink';
 import SocialMedia from 'components/SocialMedia/SocialMedia';
+import Button from 'components/_common_/Button/Button';
 import styles from './Footer.css';
 
 function Footer() {
   return (
     <footer className={styles.Footer}>
-      <div className={styles.row}>
+      <div className={classNames(styles.footerWrapper, styles.row)}>
         <div className={classNames(styles.footerGrouping, styles.socialGrouping)}>
-          <div className={classNames(styles.footerGrouping, styles.logoGrouping)}>
+          <div className={classNames(styles.logoGrouping)}>
             <img src="/static/images/operation_code_logo.png" alt="Operation Code Logo" />
           </div>
-          <div className={styles.capitalize}>Connect With Us!</div>
+          <div className={classNames(styles.capitalize, styles.marginBottom)}>Connect With Us!</div>
           <SocialMedia />
         </div>
 
         <div className={classNames(styles.footerGrouping, styles.linksGrouping, styles.capitalize)}>
-          <div className={styles.linkColumn}>
-            <Link href="/about">
-              <a>About Us</a>
-            </Link>
-            <Link href="/team">
-              <a>Team</a>
-            </Link>
-            <Link href="/contact">
-              <a>Contact Us</a>
-            </Link>
-            <Link href="/faq">
-              <a>FAQ</a>
-            </Link>
+          <div className={styles.linksRow}>
+            <div className={styles.linksColumn}>
+              <Link href="/about">
+                <a>About Us</a>
+              </Link>
+              <Link href="/team">
+                <a>Team</a>
+              </Link>
+              <Link href="/contact">
+                <a>Contact Us</a>
+              </Link>
+              <Link href="/faq">
+                <a>FAQ</a>
+              </Link>
+            </div>
+            <div className={styles.linksColumn}>
+              <Link href="/who_we_serve">
+                <a>Who We Serve</a>
+              </Link>
+              <Link href="/code_schools">
+                <a>Code Schools</a>
+              </Link>
+              <Link href="/job_board">
+                <a>Job Board</a>
+              </Link>
+              <Link href="/events">
+                <a>Events</a>
+              </Link>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </div>
           </div>
-          <div className={styles.linkColumn}>
-            <Link href="/who_we_serve">
-              <a>Who We Serve</a>
-            </Link>
-            <Link href="/code_schools">
-              <a>Code Schools</a>
-            </Link>
-            <Link href="/job_board">
-              <a>Job Board</a>
-            </Link>
-            <Link href="/events">
-              <a>Events</a>
-            </Link>
-            <Link href="/blog">
-              <a>Blog</a>
-            </Link>
+          <div className={styles.linksRow}>
+            <div className={styles.linksColumn}>
+              <Link href="/get_involved">
+                <a>Get Involved</a>
+              </Link>
+              <Link href="/become_a_mentor">
+                <a>Become a Mentor</a>
+              </Link>
+              <Link href="/support">
+                <a>Support OC</a>
+              </Link>
+              <Link href="/donate">
+                <a>Donate</a>
+              </Link>
+              <Link href="/feedback">
+                <a>Feedback</a>
+              </Link>
+            </div>
+            <div className={styles.linksColumn}>
+              <Link href="/resources">
+                <a>Resources</a>
+              </Link>
+              <Link href="/press">
+                <a>Press</a>
+              </Link>
+              <Link href="/branding">
+                <a>Branding</a>
+              </Link>
+              <Link href="/sitemap">
+                <a>Sitemap</a>
+              </Link>
+            </div>
           </div>
-          <div className={styles.linkColumn}>
-            <Link href="/get_involved">
-              <a>Get Involved</a>
-            </Link>
-            <Link href="/become_a_mentor">
-              <a>Become a Mentor</a>
-            </Link>
-            <Link href="/support">
-              <a>Support OC</a>
-            </Link>
-            <Link href="/donate">
-              <a>Donate</a>
-            </Link>
-            <Link href="/feedback">
-              <a>Feedback</a>
-            </Link>
+        </div>
+
+        <div className={classNames(styles.footerGrouping, styles.newsletterGrouping)}>
+          <div className={classNames(styles.capitalize, styles.newsletterHeading)}>Newsletter</div>
+          <div className={styles.newsletterItem}>
+            Subscribe to our newsletter and never miss a beat!
           </div>
-          <div className={styles.linkColumn}>
-            <Link href="/resources">
-              <a>Resources</a>
-            </Link>
-            <Link href="/press">
-              <a>Press</a>
-            </Link>
-            <Link href="/branding">
-              <a>Branding</a>
-            </Link>
-            <Link href="/sitemap">
-              <a>Sitemap</a>
-            </Link>
-          </div>
+          <input className={styles.newsletterItem} type="text" placeholder="Email address" />
+          <Button className={styles.newsletterItem}>Subscribe</Button>
         </div>
       </div>
 
       <div className={classNames(styles.row, styles.legalGrouping)}>
-        <div className={styles.copyright}>2018 Operation Code</div>
+        <div className={styles.copyright}>
+          Copyright 2018 Operation Code™. Operation Code is a 501(c)(3) nonprofit, EIN 47-4247572.
+        </div>
         <OutboundLink analyticsEventLabel="Footer Link" href="">
           Terms of Use
         </OutboundLink>
--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import FacebookLogo from 'static/images/icons/facebook_logo.svg';
-import GitHubLogo from 'static/images/icons/github_logo.svg';
 import TwitterLogo from 'static/images/icons/twitter_logo.svg';
-import LinkedInLogo from 'static/images/icons/linkedin_logo.svg';
 import PinterestLogo from 'static/images/icons/pinterest_logo.svg';
 import SocialMediaContainer from './SocialMediaContainer/SocialMediaContainer';
 import SocialMediaItem from './SocialMediaItem/SocialMediaItem';
@@ -17,19 +15,9 @@ function SocialMedia() {
         svg={<FacebookLogo className={styles.logo} />}
       />
       <SocialMediaItem
-        href="https://github.com/operationcode"
-        name="GitHub"
-        svg={<GitHubLogo className={styles.logo} />}
-      />
-      <SocialMediaItem
         href="https://twitter.com/operation_code"
         name="Twitter"
         svg={<TwitterLogo className={styles.logo} />}
-      />
-      <SocialMediaItem
-        href="https://www.linkedin.com/groups/13400924"
-        name="LinkedIn"
-        svg={<LinkedInLogo className={styles.logo} />}
       />
       <SocialMediaItem
         href="https://www.pinterest.com/operationcode/"
