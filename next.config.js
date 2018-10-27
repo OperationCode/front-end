4c5382dc8bbc38f5481acc45bf8fc49097ea9d97
Josiah Dahl <josiah.dahl@gmail.com>
2018-10-26 21:33:53 -0700

    fix: Switch to @zeit/next-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,42 +1,44 @@
 const withCSS = require('@zeit/next-css');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const svgoConfig = require('./common/config/svgo');
 
-module.exports = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    // No need for importLoaders: 1 as its set to 1 when postcss.config.js exists
-    localIdentName: '[name]_[local]__[hash:base64:5]',
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    // eslint-disable-next-line no-param-reassign
-    config.node = { fs: 'empty' };
+module.exports = withBundleAnalyzer(
+  withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      // No need for importLoaders: 1 as its set to 1 when postcss.config.js exists
+      localIdentName: '[name]_[local]__[hash:base64:5]',
+    },
+    analyzeServer: process.env.ANALYZE,
+    analyzeBrowser: process.env.ANALYZE,
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+      },
+      browser: {
+        analyzerMode: 'server',
+        analyzerPort: 8889,
+      },
+    },
+    webpack: config => {
+      // Fixes npm packages that depend on `fs` module
+      // eslint-disable-next-line no-param-reassign
+      config.node = { fs: 'empty' };
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'react-svg-loader',
-          options: {
-            svgo: svgoConfig,
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'react-svg-loader',
+            options: {
+              svgo: svgoConfig,
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
 
-    if (process.env.ANALYZE === 1) {
-      // eslint-disable-next-line global-require
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'server',
-          analyzerPort: isServer ? 8888 : 8889,
-          openAnalyzer: true,
-        }),
-      );
-    }
-
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
