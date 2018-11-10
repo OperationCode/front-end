affb44b8d71576f94196d9fad4fa0eeb2def1645
Kyle Holmberg <inbox@kylemh.com>
2018-11-09 22:13:21 -0800

    Finish adding icons and make stylistic changes to events page

--- pages/events.js
+++ pages/events.js
@@ -1,14 +1,17 @@
 import classNames from 'classnames';
 import TrackVisibility from 'react-on-screen';
-import OutboundLink from 'components/_common_/OutboundLink/OutboundLink';
+import Head from 'components/head';
+import HeroBanner from 'components/_common_/HeroBanner/HeroBanner';
 import Section from 'components/_common_/Section/Section';
-import Button from 'components/_common_/Button/Button';
+import LinkButton from 'components/_common_/LinkButton/LinkButton';
 import Heading from 'components/_common_/Heading/Heading';
 import Badge from 'components/Badge/Badge';
-import PlaceholderIcon1 from 'static/images/icons/FontAwesome/users-solid.svg';
+import JoinSection from 'components/ReusableSections/JoinSection/JoinSection';
+import HighfivingIcon from 'static/images/icons/Custom/high_fiving.svg';
 import BullseyeIcon from 'static/images/icons/Custom/bullseye.svg';
 import ManHoldingKeyIcon from 'static/images/icons/Custom/man_holding_key.svg';
 import NetworkingIcon from 'static/images/icons/Custom/networked_people.svg';
+import ChartIcon from 'static/images/icons/Custom/chart.svg';
 import PeopleMeetingIcon from 'static/images/icons/Custom/people_meeting.svg';
 import { s3 } from 'common/constants/urls';
 import styles from './styles/events.css';
@@ -18,7 +21,7 @@ const VISIBILITY_OFFSET = 400;
 
 const mentorItems = [
   {
-    icon: <PlaceholderIcon1 />,
+    icon: <HighfivingIcon />,
     label: 'Feel More Confident',
   },
   {
@@ -37,7 +40,7 @@ const supportItems = [
     label: 'Operations',
   },
   {
-    icon: <PlaceholderIcon1 />,
+    icon: <ChartIcon />,
     label: 'Fundraising',
   },
   {
@@ -47,184 +50,170 @@ const supportItems = [
 ];
 
 export default () => (
-  <div className={styles.eventsPage}>
-    <Section theme="slate" contentClassName={styles.grid}>
-      <div className={styles.introText}>
-        <Heading
-          className={classNames(
-            styles.alignLeft,
-            styles.sectionHeading,
-            styles.sectionHeading_introHeading,
+  <>
+    <Head title="Events" />
+
+    <div className={styles.eventsPage}>
+      <HeroBanner
+        className={styles.heroBannerMobilePositioning}
+        imageSource={`${s3}redesign/heroBanners/events.jpg`}
+        title="You Can Make An Impact"
+      >
+        <>
+          <p>
+            Whether you&apos;re interested in mentoring, supporting our operations, or simply
+            donating to help military veterans, service members, and spouses launch a career in
+            software development, Operation Code is always looking for supporters like you.
+          </p>
+
+          <LinkButton href="/who_we_serve">Learn More</LinkButton>
+        </>
+      </HeroBanner>
+
+      <Section theme="mist" contentClassName={styles.alignCenter}>
+        <Heading hasHeadingLines={false} className={styles.sectionHeading} theme="slate">
+          The Power Of Mentorship
+        </Heading>
+
+        <p className={styles.marginBottom}>By mentoring one of our members, you will help them:</p>
+
+        <div className={styles.badgeGroupings}>
+          {mentorItems.map(item => (
+            <Badge
+              key={item.label}
+              svgComponent={item.icon}
+              label={item.label}
+              className={styles.badge}
+            />
+          ))}
+        </div>
+      </Section>
+
+      <Section theme="slate" contentClassName={styles.grid}>
+        <TrackVisibility offset={VISIBILITY_OFFSET}>
+          {({ isVisible }) => (
+            <div
+              className={classNames(
+                styles.alignRight,
+                styles.offsetImage,
+                isVisible && styles.showImage,
+              )}
+            >
+              <img src={`${s3}redesign/images/woman.jpg`} alt="Woman smiling at computer" />
+            </div>
           )}
-          hasHeadingLines={false}
-          theme="white"
-        >
-          YOU CAN MAKE AN IMPACT
-        </Heading>
-        <p>
-          Whether you&apos;re interested in mentoring, supporting our operations, or simply donating
-          to help military veterans, service members, and spouses launch a career in software
-          development, Operation Code is always looking for supporters like you.
-        </p>
-      </div>
-      <div>
-        <img
-          className={styles.introImage}
-          src={`${s3}heroBanners/stock_family-2.jpg`}
-          alt="Person explaining something. They're very happy."
-        />
-      </div>
-    </Section>
+        </TrackVisibility>
 
-    <Section theme="mist" contentClassName={styles.alignCenter}>
-      <Heading hasHeadingLines={false} className={styles.sectionHeading} theme="slate">
-        THE POWER OF MENTORSHIP
-      </Heading>
-      <p className={styles.marginBottom}>By mentoring one of our members, you will help them:</p>
-      <div className={styles.flexDisplay}>
-        {mentorItems.map(item => (
-          <Badge
-            key={item.label}
-            className={classNames(styles.marginBottom, styles.badgeIcon)}
-            svgComponent={item.icon}
-            label={item.label}
-          />
-        ))}
-      </div>
-    </Section>
-
-    <Section theme="slate" contentClassName={styles.grid}>
-      <TrackVisibility offset={VISIBILITY_OFFSET}>
-        {({ isVisible }) => (
-          <div
-            className={classNames(
-              styles.alignRight,
-              styles.offsetImage,
-              isVisible && styles.showImage,
-            )}
+        <div className={styles.gridContent}>
+          <Heading
+            className={classNames(styles.alignLeft, styles.sectionHeading)}
+            hasHeadingLines={false}
+            theme="white"
           >
-            <img src={`${s3}stock_paired-programming.jpg`} alt="Empower Our Community" />
-          </div>
-        )}
-      </TrackVisibility>
-      <div className={styles.gridContent}>
-        <Heading
-          className={classNames(styles.alignLeft, styles.sectionHeading)}
-          hasHeadingLines={false}
-          theme="white"
-        >
-          EMPOWER OUR COMMUNITY
-        </Heading>
-        <p>
-          Make a difference in the lives of military veterans, service members, and spouses who are
-          eager to transition into a software development career.
-        </p>
-        <Button theme="slate" className={styles.buttonOutline}>
-          BECOME A MENTOR
-        </Button>
-      </div>
-    </Section>
+            Empower Our Community
+          </Heading>
 
-    <Section theme="mist" className={styles.sectionHeading} contentClassName={styles.alignCenter}>
-      <Heading hasHeadingLines={false} theme="slate">
-        WAYS YOU CAN SUPPORT OPERATION CODE
-      </Heading>
-      <p className={classNames(styles.marginBottom, styles.centerText)}>
-        We&apos;re always looking for volunteers who are dedicated to making an impact in the lives
-        of military veterans, service members, and spouses. You can help us with:
-      </p>
-      <div className={classNames(styles.flexDisplay)}>
-        {supportItems.map(item => (
-          <Badge
-            key={item.label}
-            className={classNames(styles.marginBottom, styles.badgeIcon)}
-            svgComponent={item.icon}
-            label={item.label}
-          />
-        ))}
-      </div>
-    </Section>
+          <p>
+            Make a difference in the lives of military veterans, service members, and spouses who
+            are eager to transition into a software development career.
+          </p>
 
-    <Section theme="slate" contentClassName={styles.grid}>
-      <div className={classNames(styles.alignRight, styles.gridContent)}>
-        <Heading
-          className={classNames(styles.alignLeft, styles.sectionHeading)}
-          hasHeadingLines={false}
-          theme="white"
-        >
-          SUPPORT OUR MISSION
+          <LinkButton href="/mentor">Become A Mentor</LinkButton>
+        </div>
+      </Section>
+
+      <Section theme="mist" className={styles.sectionHeading} contentClassName={styles.alignCenter}>
+        <Heading hasHeadingLines={false} theme="slate">
+          WAYS YOU CAN SUPPORT OPERATION CODE
         </Heading>
-        <p>
-          Make a difference in the lives of military veterans, service members, and spouses who are
-          eager to transition into a software development career.
+
+        <p className={classNames(styles.marginBottom, styles.centerText)}>
+          We&apos;re always looking for volunteers who are dedicated to making an impact in the
+          lives of military veterans, service members, and spouses. You can help us with:
         </p>
-        <Button theme="slate" className={styles.buttonOutline}>
-          SUPPORT OC
-        </Button>
-      </div>
-      <TrackVisibility offset={VISIBILITY_OFFSET}>
-        {({ isVisible }) => (
-          <div
-            className={classNames(
-              styles.alignRight,
-              styles.offsetImage,
-              isVisible && styles.showImage,
-            )}
+
+        <div className={classNames(styles.badgeGroupings)}>
+          {supportItems.map(item => (
+            <Badge
+              key={item.label}
+              svgComponent={item.icon}
+              label={item.label}
+              className={styles.badge}
+            />
+          ))}
+        </div>
+      </Section>
+
+      <Section theme="slate" contentClassName={styles.grid}>
+        <div className={classNames(styles.alignRight, styles.gridContent)}>
+          <Heading
+            className={classNames(styles.alignLeft, styles.sectionHeading)}
+            hasHeadingLines={false}
+            theme="white"
           >
-            <img src={`${s3}stock_paired-programming.jpg`} alt="Support Our Mission" />
-          </div>
-        )}
-      </TrackVisibility>
-    </Section>
+            Support Our Mission
+          </Heading>
 
-    <Section theme="mist" contentClassName={styles.alignCenter}>
-      <Heading hasHeadingLines={false} theme="slate">
-        DONATE TO DEPLOY THE FUTURE
-      </Heading>
-      <p className={styles.centerText}>
-        Help us reach our fundraising goal! With your donation, we&apos;ll be able to provide our
-        members with coding school scholarships, tickets to tech conferences, and access to
-        necessary equipment.
-      </p>
-      <div className={styles.progressBarInfo}>
-        <div className={styles.donatedAmount}>
-          <span className={styles.amount}>$300.00</span>
-          <span>Donated</span>
-        </div>
-        <div className={styles.goalAmount}>
-          <span>Goal:</span>
-          <span>$100,000</span>
-        </div>
-      </div>
-      <div className={styles.progressBar}>
-        <div style={{ width: '0px' }} className={styles.progressBarFill} />
-      </div>
-      <Button>MAKE A DONATION</Button>
-    </Section>
+          <p>
+            Make a difference in the lives of military veterans, service members, and spouses who
+            are eager to transition into a software development career.
+          </p>
 
-    <Section theme="white" contentClassName={styles.alignCenter}>
-      <Heading hasHeadingLines={false} theme="slate">
-        JOIN OUR THRIVING COMMUNITY
-      </Heading>
-      <p>
-        Are you ready to begin your journey towards a career in software development?
-        <br />
-        Get the support you need by joining our members only Slack community!
-      </p>
-      <div className={classNames(styles.alignCenter, styles.marginBottom)}>
-        <form className={styles.slackForm}>
-          <input placeholder="Email address" />
-          <Button className={styles.slackForm_button} name="submit" type="submit">
-            JOIN OUR SLACK
-          </Button>
-        </form>
-        <p className={styles.slackText}>
-          Slack is a community based collaboration tool where all the magic happens!
+          <LinkButton href="/get_involved">Get Involved</LinkButton>
+        </div>
+
+        <TrackVisibility offset={VISIBILITY_OFFSET}>
+          {({ isVisible }) => (
+            <div
+              className={classNames(
+                styles.alignRight,
+                styles.offsetImage,
+                isVisible && styles.showImage,
+              )}
+            >
+              <img
+                src={`${s3}redesign/images/utah_meetup.jpg`}
+                alt="Operation Code members collaborate on a problem"
+              />
+            </div>
+          )}
+        </TrackVisibility>
+      </Section>
+
+      {/* TODO: REMOVE PLACEHOLDER CONTENT */}
+      <Section theme="mist" contentClassName={styles.alignCenter}>
+        <Heading hasHeadingLines={false} theme="slate">
+          Donate To Deploy The Future
+        </Heading>
+
+        <p className={styles.centerText}>
+          Help us reach our fundraising goal! With your donation, we&apos;ll be able to provide our
+          members with coding school scholarships, tickets to tech conferences, and access to
+          necessary equipment.
         </p>
-        <OutboundLink href="https://slack.com/" analyticsEventLabel="Learn more (Slack)">
-          Learn more
-        </OutboundLink>
-      </div>
-    </Section>
-  </div>
+
+        <div className={styles.progressBarInfo}>
+          <div className={styles.donatedAmount}>
+            <span className={styles.amount}>$300.00</span>
+            <span>Donated</span>
+          </div>
+          <div className={styles.goalAmount}>
+            <span>Goal:</span>
+            <span>$100,000</span>
+          </div>
+        </div>
+
+        <div className={styles.progressBar}>
+          <div style={{ width: '0px' }} className={styles.progressBarFill} />
+        </div>
+
+        <LinkButton href="/donate" isFilled>
+          Make A Donation
+        </LinkButton>
+      </Section>
+      {/* PLACEHOLDER CONTENT ABOVE */}
+
+      <JoinSection />
+    </div>
+  </>
 );
