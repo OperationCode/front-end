ff8399edc47521f482eaf1c09e17e52a8263a5e1
K MacLeod <ksmacleod99@gmail.com>
2018-11-12 15:08:40 -0500

    adds color section
    adds color section; adds color card component. Get excited.

--- components/BrandingContent/ColorSection/ColorSection.js
+++ components/BrandingContent/ColorSection/ColorSection.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import Section from 'components/_common_/Section/Section';
-// import styles from './colors.css';
+import ColorBlock from './ColorBlock/ColorBlock';
+import styles from './colors.css';
 
 function ColorSection() {
-  return <Section title="Colors" />;
+  return (
+    <Section title="Colors" theme="grey">
+      <h3>Primary Pallete</h3>
+      <div className={styles.colorPalette}>
+        <ColorBlock colorName="Blue" colorHex="#249CBC" />
+
+        <ColorBlock colorName="Red" colorHex="#D1665A" />
+
+        <ColorBlock colorName="Slate" colorHex="#47566B" />
+      </div>
+
+      <h3>Tints</h3>
+      <div className={styles.colorPalette}>
+        <ColorBlock colorName="Gray" colorHex="#9BAAB5" />
+
+        <ColorBlock colorName="Light Gray" colorHex="#D0D5DA" />
+
+        <ColorBlock colorName="Mist" colorHex="#F0F2F2" />
+      </div>
+    </Section>
+  );
 }
 
 export default ColorSection;
