99bbe6913f099c0117e789020eabf4a919604872
Kyle Holmberg <inbox@kylemh.com>
2018-11-13 02:11:13 -0800

    Resolve lint error

--- pages/branding.js
+++ pages/branding.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Head from 'components/head';
 import LogoSection from 'components/BrandingContent/LogoSection/LogoSection';
 import ColorSection from 'components/BrandingContent/ColorSection/ColorSection';
