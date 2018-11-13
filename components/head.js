ac5c9e7effac9f1a964d2e62e2aa4fb51df079a9
Kyle Holmberg <inbox@kylemh.com>
2018-11-13 00:53:06 -0800

    Create custom _document

--- components/head.js
+++ components/head.js
@@ -23,27 +23,16 @@ function Head({ description, ogImage, title, url }) {
       <meta charSet="UTF-8" />
       <title>Operation Code | {title}</title>
       <meta name="description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-      <link rel="icon" sizes="192x192" href="/static/apple-icon-180x180.png" />
-      <link rel="apple-touch-icon" href="/static/apple-icon-180x180.png" />
-      <link rel="icon" href="/static/favicon.ico" />
 
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
 
       <meta name="twitter:site" content={url} />
-      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content={ogImage} />
       <meta property="og:image" content={ogImage} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
 
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i"
-      />
+      {/* Constant meta tags are supplied in `_document.js` */}
     </NextHead>
   );
 }
