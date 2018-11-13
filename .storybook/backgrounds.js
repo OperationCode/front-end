1d92aead8f5583db601bd151bb43499a44f1b7a6
Kyle Holmberg <inbox@kylemh.com>
2018-11-12 16:03:48 -0800

    Remove unnecessary comment

--- .storybook/backgrounds.js
+++ .storybook/backgrounds.js
@@ -2,7 +2,6 @@ import addonBackgrounds from '@storybook/addon-backgrounds';
 import { brandColorsObject } from 'common/styles/styleExports';
 import { capitalizeFirstLetter } from 'common/utils/string-utils';
 
-// For Backgrounds Addon
 const backgroundsPaletteArray = Object.keys(brandColorsObject).map(name => ({
   name: capitalizeFirstLetter(name),
   value: brandColorsObject[name],
