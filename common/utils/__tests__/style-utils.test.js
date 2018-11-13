3022ee96171bd62af5134919b9c2450d630cf90e
Kyle Holmberg <inbox@kylemh.com>
2018-11-12 16:49:04 -0800

    Add tests for getBreakpoints

--- common/utils/__tests__/style-utils.test.js
+++ common/utils/__tests__/style-utils.test.js
@@ -4,6 +4,7 @@ import {
   hasPixelSuffix,
   isHexColor,
   getSortedBreakpointValues,
+  getBreakpoints,
 } from '../style-utils';
 
 describe('Style Utilities', () => {
@@ -71,6 +72,32 @@ describe('Style Utilities', () => {
 
     it('should return breakpoint-related values in order from least to greatest', () => {
       expect(getSortedBreakpointValues(testValues)).toStrictEqual([...sortedBreakpointValues]);
+    });
+  });
+
+  describe('getBreakpoints', () => {
+    const testValues = ['768px', '1000px', '900px', '500px'];
+
+    const errorMessage = `We require a small, medium, large, and extra-large breakpoint. Ensure
+      "common/styles/variables.css" has exactly 4 breakpoint values.`;
+
+    it('should throw an error when passed an array with fewer than 4 items', () => {
+      expect(() => getBreakpoints([testValues[0], testValues[1], testValues[2]])).toThrow(
+        errorMessage,
+      );
+    });
+
+    it('should throw an error when passed an array with more than 4 items', () => {
+      expect(() => getBreakpoints([...testValues, '1200px'])).toThrow(errorMessage);
+    });
+
+    it('should return a breakpoint object when passed an array of 4 pixel-suffixed values', () => {
+      expect(getBreakpoints(testValues)).toStrictEqual({
+        sm: 500,
+        md: 768,
+        lg: 900,
+        xl: 1000,
+      });
     });
   });
 });
