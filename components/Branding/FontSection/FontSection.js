43edc9b3453f88cb93490733757267c7559cb45a
Kyle Holmberg <inbox@kylemh.com>
2018-11-17 15:42:20 -0800

    Clean up font section

--- components/Branding/FontSection/FontSection.js
+++ components/Branding/FontSection/FontSection.js
@@ -3,15 +3,26 @@ import Section from 'components/_common_/Section/Section';
 import styles from './FontSection.css';
 
 function FontSection() {
+  // Every letter of the alphabet in one string
+  const demoText = 'Sphinx of black quartz, judge my vow!';
+
   return (
     <Section title="Fonts" theme="mist">
-      <div className={styles.brandingFonts}>
-        <h6>PF DIN DISPLAY PRO REGULAR</h6>
-        <p className={styles.pfDinDisplay}>Sphinx of black quartz, judge my vow!</p>
-        <p>&nbsp;</p>
-        <h6>NOTO SERIF REGULAR</h6>
-        <p className={styles.notoSerif}>Sphinx of black quartz, judge my vow!</p>
-      </div>
+      <ul className={styles.fontsList}>
+        <li className={styles.primaryFontFamily}>
+          <div>
+            <h6>PF Din Display Pro Regular</h6>
+            <p>{demoText}</p>
+          </div>
+        </li>
+
+        <li className={styles.secondaryFontFamily}>
+          <div>
+            <h6>Noto Serif Regular</h6>
+            <p>{demoText}</p>
+          </div>
+        </li>
+      </ul>
     </Section>
   );
 }
