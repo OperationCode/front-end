ee142f41f790daff963236a425ac521c73b89a56
Kyle Holmberg <inbox@kylemh.com>
2018-11-24 11:28:06 -0800

    Use postcss.config.js inside Storybook

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,9 @@
 /* eslint-disable global-require */
-module.exports = ({ file, options /* env is also available */ }) => {
-  const plugins = {
-    'postcss-import': { root: file.dirname },
-    autoprefixer: {
-      ...options.autoprefixer,
-      flexbox: 'no-2009',
-    },
-  };
+module.exports = ({ file, options, env }) => {
+  const ENV = env.toLowerCase(); // eslint-disable-line no-unused-vars
 
-  return { plugins };
+  return {
+    ...options,
+    plugins: [require('postcss-import')({ root: file.dirname }), require('autoprefixer')()],
+  };
 };
