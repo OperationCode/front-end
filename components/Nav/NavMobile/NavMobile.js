ad46b45541bedafe26161abc3384d34c374d2dfd
Julien Mayer <jalexismayer@gmail.com>
2018-11-29 16:15:28 -0600

    WIP

--- components/Nav/NavMobile/NavMobile.js
+++ components/Nav/NavMobile/NavMobile.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 import { s3 } from 'common/constants/urls';
 import { navItems } from 'common/constants/navigation';
-// import PropTypes from 'prop-types';
 import HamburgerIcon from 'static/images/icons/hamburger.svg';
 import styles from './NavMobile.css';
 
 export default class NavMobile extends Component {
-  // static propTypes = {
-  //   navItems: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       name: PropTypes.string.isRequired,
-  //       href: PropTypes.string.isRequired,
-  //       shouldPrefetch: PropTypes.bool.isRequired,
-  //       sublinks: PropTypes.arrayOf(
-  //         PropTypes.shape({
-  //           name: PropTypes.string,
-  //           href: PropTypes.string,
-  //         }),
-  //       ).isRequired,
-  //     }),
-  //   ).isRequired,
-  // };
-
   constructor(props) {
     super(props);
     this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+    this.links = this.links.bind(this);
   }
 
   state = {
     dropdownShow: false,
   };
+
+  // navItems is dumb
+  links = () => navItems.map(navItem => [navItem, navItem.sublinks]).flat(2);
 
   toggleDropdown() {
     this.setState(prevState => ({ dropdownShow: !prevState.dropdownShow }));
@@ -40,21 +28,39 @@ export default class NavMobile extends Component {
   }
 
   render() {
-    const { state, handleOnMouseOver } = this;
+    const { state, handleOnMouseOver, links } = this;
     const { dropdownShow } = state;
 
     return (
-      <div className={styles.NavMobile}>
-        <img
-          src={`${s3}branding/logos/small-blue-logo.png`}
-          alt="operation code logo"
-          className={styles.logo}
-        />
-        <ul className={styles.menu} onMouseOver={handleOnMouseOver} onFocus={() => {}}>
-          <HamburgerIcon className={styles.icon} />
-          {dropdownShow && navItems}
-        </ul>
-      </div>
+      <header>
+        <div className={styles.NavMobile}>
+          <img
+            src={`${s3}branding/logos/small-blue-logo.png`}
+            alt="operation code logo"
+            className={styles.logo}
+          />
+
+          <HamburgerIcon
+            className={styles.icon}
+            onMouseOver={handleOnMouseOver}
+            onFocus={() => {}}
+          />
+
+          {dropdownShow && (
+            <div className={styles.dropdown}>
+              <ul className={styles.ul}>
+                {links().map(navlink => (
+                  <li className={styles.li}>
+                    <Link href={navlink.href}>
+                      <a className={styles.a}>{navlink.name}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </header>
     );
   }
 }
