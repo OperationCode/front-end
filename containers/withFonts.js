72ad4bfda3718f6e0ddae892d324bdafe0250cd2
Nic Young <nryoung@gmail.com>
2018-11-30 20:09:28 -0700

    feat: add fontfaceobserver and implement non-blocking font loading

---
+++ containers/withFonts.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
+
+const fonts = [
+  {
+    fontFamily: 'Encode Sans',
+    url: 'https://fonts.googleapis.com/css?family=Encode+Sans:400,700',
+  },
+  {
+    fontFamily: 'DIN Condensed Bold',
+    url: null,
+  },
+];
+
+const withFonts = WrappedComponent =>
+  class extends Component {
+    componentDidMount() {
+      const observers = fonts.map(font => {
+        if (font.url) {
+          const link = document.createElement('link');
+          link.href = font.url;
+          link.rel = 'stylesheet';
+          document.head.appendChild(link);
+        }
+
+        const observer = new FontFaceObserver(font.fontFamily);
+        return observer.load();
+      });
+
+      Promise.all(observers).then(() => {
+        document.documentElement.classList.add('fonts-loaded');
+      });
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+
+export default withFonts;
