d675dc35f36636c691cd5101b90ec3cbbc8ffd9d
markchernov <mark.chernov@gmail.com>
2018-12-01 21:56:58 -0500

    added schools filtering

--- pages/code_schools.js
+++ pages/code_schools.js
@@ -1,19 +1,56 @@
+import Select from 'react-select';
 import Head from 'components/head';
 import { getCodeSchoolsPromise } from 'common/constants/api';
 import OutboundLink from 'components/_common_/OutboundLink/OutboundLink';
 import Section from 'components/_common_/Section/Section';
+import Button from 'components/_common_/Button/Button';
 import SchoolCard from 'components/Cards/SchoolCard/SchoolCard';
 import { s3 } from 'common/constants/urls';
+import States from 'pages/CodeSchools/States';
+import styles from './styles/code_schools.css';
 
 export default class CodeSchools extends React.Component {
   state = {
-    schools: [],
+    allSchools: [],
+    filteredSchools: [],
   };
 
   async componentDidMount() {
     const { data } = await getCodeSchoolsPromise();
-    this.setState({ schools: data });
+    this.setState({ allSchools: data });
+    this.setState({ filteredSchools: data });
   }
+
+  filterVaApproved = () => {
+    const { allSchools } = this.state;
+    const vaApproved = allSchools.filter(school => {
+      const vaFound = school.locations.some(location => location.va_accepted === true);
+      return vaFound;
+    });
+    this.setState({ filteredSchools: vaApproved });
+  };
+
+  filterOnline = () => {
+    const { allSchools } = this.state;
+    const onlineSchools = allSchools.filter(school => school.has_online);
+    this.setState({ filteredSchools: onlineSchools });
+  };
+
+  showAll = () => {
+    const { allSchools } = this.state;
+    this.setState({ filteredSchools: allSchools });
+  };
+
+  filterByState = selectedState => {
+    const { allSchools } = this.state;
+    const stateSchools = allSchools.filter(school => {
+      const stateLocatonFound = school.locations.some(
+        location => location.state === selectedState.value,
+      );
+      return stateLocatonFound;
+    });
+    this.setState({ filteredSchools: stateSchools });
+  };
 
   render() {
     const { state } = this;
@@ -21,8 +58,13 @@ export default class CodeSchools extends React.Component {
     return (
       <>
         <Head title="Code Schools" />
-
-        <Section theme="white" title="Code Schools">
+        <Section theme="gray" title="Code Schools">
+          <p>
+            Whether you&apos;re trying to find out more about a chosen school, or are just gettting
+            started in your search, we&apos;re here to help. We&apos;ve even partnered with some
+            schools to offer scholarships, and discounts for our members.
+          </p>
+          <h6>What Are Code Schools?</h6>
           <p>
             Code schools are accelerated learning programs that will prepare you for a career in
             software development. Each school listed below ranges in length, vary in tuition costs,
@@ -46,26 +88,48 @@ export default class CodeSchools extends React.Component {
             </p>
           </aside>
         </Section>
+
+        <Button theme="primary" onClick={this.showAll}>
+          All Schools{' '}
+        </Button>
+        <Button theme="primary" onClick={this.filterVaApproved}>
+          VA Approved Schools{' '}
+        </Button>
+        <Button theme="primary" onClick={this.filterOnline}>
+          Online Schools{' '}
+        </Button>
+        <Select
+          className={styles.select}
+          placeholder="Start typing a state..."
+          options={States}
+          autoBlur
+          autosize
+          multi
+          onChange={this.filterByState}
+        />
+
         <Section theme="gray" title="Schools" hasHeadingLines={false}>
-          {state.schools.map(school => (
-            <div key={`${school.name}`}>
-              <SchoolCard
-                hasHardwareIncluded={school.hardware_included}
-                hasHousing={school.has_housing}
-                hasOnline={school.has_online}
-                hasOnlyOnline={school.online_only}
-                isFullTime={school.full_time}
-                locations={school.locations}
-                logoSource={`${s3}codeSchoolLogos/${school.name
-                  .trim()
-                  .split(' ')
-                  .join('_')
-                  .toLowerCase()}.jpg`}
-                name={school.name}
-                website={school.url}
-              />
-            </div>
-          ))}
+          <div className={styles.flexGrid}>
+            {state.filteredSchools.map(school => (
+              <div key={`${school.name}`}>
+                <SchoolCard
+                  hasHardwareIncluded={school.hardware_included}
+                  hasHousing={school.has_housing}
+                  hasOnline={school.has_online}
+                  hasOnlyOnline={school.online_only}
+                  isFullTime={school.full_time}
+                  locations={school.locations}
+                  logoSource={`${s3}codeSchoolLogos/${school.name
+                    .trim()
+                    .split(' ')
+                    .join('_')
+                    .toLowerCase()}.jpg`}
+                  name={school.name}
+                  website={school.url}
+                />
+              </div>
+            ))}
+          </div>
         </Section>
       </>
     );
