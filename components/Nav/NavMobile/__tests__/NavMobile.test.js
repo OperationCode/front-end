e6fd6f534c496c735a825a7c25eec380f3079716
Kyle Holmberg <inbox@kylemh.com>
2018-12-08 15:16:02 -0800

    Fix test names and remove dupe tests

--- components/Nav/NavMobile/__tests__/NavMobile.test.js
+++ components/Nav/NavMobile/__tests__/NavMobile.test.js
@@ -2,41 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import createShallowSnapshotTest from 'test-utils/createShallowSnapshotTest';
 
-import CloseButton from 'components/_common_/CloseButton/CloseButton';
 import NavMobile from '../NavMobile';
 
 describe('NavMobile', () => {
   it('should render', () => createShallowSnapshotTest(<NavMobile />));
 
-  it('dropdown is not visible without hamburger cutton click', () => {
+  it('should not immediately render nav items list', () => {
     const wrapper = shallow(<NavMobile />);
 
     expect(wrapper.find('ul')).not.toExist();
   });
 
-  it('dropdown is visible with hamburger button click', () => {
+  it('should render nav items list after hamburger button is clicked', () => {
     const wrapper = shallow(<NavMobile />);
 
     wrapper.find('.hamburgerButton').simulate('click');
 
     expect(wrapper.find('ul')).toExist();
-  });
-
-  it('visible dropdown should have 10 links', () => {
-    const wrapper = shallow(<NavMobile />);
-
-    wrapper.find('.hamburgerButtonWrapper').simulate('click');
-
-    expect(wrapper.find('ul').find('a')).toHaveLength(10);
-  });
-
-  it('visible dropdown dissappears on close button click', () => {
-    const wrapper = shallow(<NavMobile />);
-
-    wrapper.find('.hamburgerButton').simulate('click');
-    expect(wrapper.find('ul')).toExist();
-    wrapper.find(CloseButton).simulate('click');
-
-    expect(wrapper.find('ul')).not.toExist();
   });
 });
