6637bbea5311cc6299ca3f1fe0f019380e07d411
Kyle Holmberg <inbox@kylemh.com>
2018-12-16 15:11:32 -0800

    Simplify test file and add unit test on short-circuit

--- components/FlatCard/__tests__/FlatCard.test.js
+++ components/FlatCard/__tests__/FlatCard.test.js
@@ -1,27 +1,31 @@
 /* eslint-env jest */
 import React from 'react';
+import { shallow } from 'enzyme';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import { s3 } from 'common/constants/urls';
 import FlatCard from '../FlatCard';
 
-const testImageUrl = `${s3}headshots/david_molina.jpg`;
-
 describe('FlatCard', () => {
+  const requiredProps = {
+    imageSource: `${s3}headshots/david_molina.jpg`,
+    imageAlt: "David Molina's face",
+    children: (
+      <>
+        <p>Example content goes here</p>
+        <p>More content goes here</p>
+      </>
+    ),
+  };
+
   it('should render with required props', () => {
-    createSnapshotTest(
-      <FlatCard imageSource={testImageUrl} imageAlt="Image caption">
-        <>
-          <p>Example content goes here</p>
-          <p>More content goes here</p>
-        </>
-      </FlatCard>,
-    );
+    createSnapshotTest(<FlatCard {...requiredProps} />);
   });
 
   it('should render with many props assigned', () => {
     createSnapshotTest(
       <FlatCard
+        {...requiredProps}
         className="test-class"
         header={
           <>
@@ -29,13 +33,19 @@ describe('FlatCard', () => {
             <h6>Sub heading</h6>
           </>
         }
-        imageSource={testImageUrl}
-        imageAlt="Image Caption"
-      >
-        <div>
-          <p>Even more test content!</p>
-        </div>
-      </FlatCard>,
+      />,
     );
+  });
+
+  it('should not render a horizontal ruler when header is undefined', () => {
+    const wrapper = shallow(<FlatCard {...requiredProps} header={undefined} />);
+
+    expect(wrapper.find('hr').exists()).toStrictEqual(false);
+  });
+
+  it('should render a horizontal ruler when header is passed', () => {
+    const wrapper = shallow(<FlatCard {...requiredProps} header={<h1>Howdy!</h1>} />);
+
+    expect(wrapper.find('hr').exists()).toStrictEqual(true);
   });
 });
