f8fa9b805122ac7cf0279ef7872f74be92a1fd86
Roger K <raevlogic@gmail.com>
2018-12-20 17:30:42 -0700

    Update ContentContainer theme prop, CSS, tests

--- components/ContentContainer/ContentContainer.js
+++ components/ContentContainer/ContentContainer.js
@@ -7,19 +7,21 @@ ContentContainer.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
   backgroundImageSource: PropTypes.string,
+  theme: PropTypes.oneOf(['gray', 'secondary', 'white']),
 };
 
 ContentContainer.defaultProps = {
   className: undefined,
   backgroundImageSource: undefined,
   children: undefined,
+  theme: 'gray',
 };
 
-function ContentContainer({ children, className, backgroundImageSource }) {
+function ContentContainer({ children, className, backgroundImageSource, theme }) {
   return (
     <div
       backgroundImageSource={backgroundImageSource}
-      className={classNames(className, styles.ContentContainer)}
+      className={classNames(className, styles.ContentContainer, styles[theme])}
     >
       {children}
     </div>
--- components/ContentContainer/__tests__/ContentContainer.test.js
+++ components/ContentContainer/__tests__/ContentContainer.test.js
@@ -5,11 +5,19 @@ import createSnapshotTest from 'test-utils/createSnapshotTest';
 import ContentContainer from '../ContentContainer';
 
 describe('ContentContainer', () => {
-  it('should render with required props', () => {
-    createSnapshotTest(<ContentContainer>Test</ContentContainer>);
+  it('should render with many props assigned', () => {
+    createSnapshotTest(
+      <ContentContainer className="test-class" backgroundImageSource="testImage" theme="gray">
+        Test Children
+      </ContentContainer>,
+    );
   });
 
-  it('should render with many props assigned', () => {
-    createSnapshotTest(<ContentContainer className="test-class">Test</ContentContainer>);
+  it('should render common use case', () => {
+    createSnapshotTest(
+      <ContentContainer className="test-class" theme="secondary">
+        Test Children 2
+      </ContentContainer>,
+    );
   });
 });
