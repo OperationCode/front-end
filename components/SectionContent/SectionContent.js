49a363dda0e564a48262c2412abd56370a6991a1
Kyle Holmberg <kylemh.email@gmail.com>
2019-01-13 13:21:11 +0100

    Simplify component

--- components/SectionContent/SectionContent.js
+++ components/SectionContent/SectionContent.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import styles from './SectionContent.css';
 
 SectionContent.propTypes = {
-  className: PropTypes.string,
-  columns: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired, // can be JSX, elements, or strings
   id: PropTypes.string,
   title: PropTypes.string,
 };
 
 SectionContent.defaultProps = {
-  className: undefined,
   id: undefined,
   title: undefined,
 };
 
-function SectionContent({ className, columns, id, title }) {
+function SectionContent({ columns, id, title }) {
   return (
-    <section className={classNames(styles.SectionContent, className)} id={id}>
+    <section className={styles.SectionContent} id={id}>
       <h3 className={styles.title}>{title}</h3>
 
       <div className={styles.columnsContainer}>
-        {columns.map((Column, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Column key={index} />
-        ))}
+        {/* eslint-disable-next-line react/no-array-index-key */}
+        {columns.map((column, index) => React.cloneElement(column, { key: index }))}
       </div>
     </section>
   );
