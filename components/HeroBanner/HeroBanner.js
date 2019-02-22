a326be81aff37ab61947e5e3bc34f25c2696594a
Kyle Holmberg <kylemh.email@gmail.com>
2019-02-22 01:22:37 -0800

    Adjust <HeroBanner> to use <Container> and remove unnecessary test cases

--- components/HeroBanner/HeroBanner.js
+++ components/HeroBanner/HeroBanner.js
@@ -1,41 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Container from 'components/_common_/Container/Container';
 import styles from './HeroBanner.css';
 
 HeroBanner.propTypes = {
+  backgroundImageSource: PropTypes.string.isRequired,
   children: PropTypes.node,
-  className: PropTypes.string,
-  imageSource: PropTypes.node.isRequired,
-  isFullViewHeight: PropTypes.bool,
-  title: PropTypes.node.isRequired,
+  isFullViewportHeight: PropTypes.bool,
+  title: PropTypes.string.isRequired,
 };
 
 HeroBanner.defaultProps = {
   children: undefined,
-  className: undefined,
-  isFullViewHeight: false,
+  isFullViewportHeight: false,
 };
 
-function HeroBanner({ children, className, imageSource, isFullViewHeight, title }) {
-  // See https://css-tricks.com/tinted-images-multiple-backgrounds/ for explanation
-  const darkOverlay = 'linear-gradient(rgba(33, 48, 69, 0.65),rgba(33, 48, 69, 0.65))';
-  const dynamicBackgroundImage = {
-    backgroundImage: `${darkOverlay}, url(${imageSource})`,
-  };
-
+function HeroBanner({ backgroundImageSource, children, isFullViewportHeight, title }) {
   return (
-    <div
-      style={dynamicBackgroundImage}
-      className={classNames(className, styles.HeroBanner, {
-        [styles.fullViewHeight]: isFullViewHeight,
-      })}
+    <Container
+      backgroundImageSource={backgroundImageSource}
+      className={styles.HeroBanner}
+      isFullViewportHeight={isFullViewportHeight}
     >
-      <div className={styles.content}>
-        <h1 className={classNames({ [styles.underline]: children })}>{title}</h1>
-        {children}
-      </div>
-    </div>
+      <h1 className={classNames({ [styles.underline]: children })}>{title}</h1>
+      {children}
+    </Container>
   );
 }
 
--- components/HeroBanner/__tests__/HeroBanner.test.js
+++ components/HeroBanner/__tests__/HeroBanner.test.js
@@ -1,6 +1,5 @@
 /* eslint-env jest */
 import React from 'react';
-import { shallow } from 'enzyme';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import { s3 } from 'common/constants/urls';
@@ -10,28 +9,14 @@ describe('HeroBanner', () => {
   const testImageUrl = `${s3}heroBanner/stock_family-2.jpg`;
 
   it('should render with required props', () => {
-    createSnapshotTest(<HeroBanner title="Test" imageSource={testImageUrl} />);
+    createSnapshotTest(<HeroBanner title="Test" backgroundImageSource={testImageUrl} />);
   });
 
   it('should render with many props assigned', () => {
     createSnapshotTest(
-      <HeroBanner className="testing-123" title="Test" imageSource={testImageUrl}>
+      <HeroBanner title="Test" backgroundImageSource={testImageUrl} isFullViewportHeight>
         Testing 123
       </HeroBanner>,
     );
-  });
-
-  it('should have "fullViewHeight" class when passed isFullViewHeight', () => {
-    const wrapper = shallow(
-      <HeroBanner title="Test" imageSource={testImageUrl} isFullViewHeight />,
-    );
-
-    expect(wrapper.find('.HeroBanner').hasClass('fullViewHeight')).toStrictEqual(true);
-  });
-
-  it('should NOT have "fullViewHeight" class when NOT passed isFullViewHeight', () => {
-    const wrapper = shallow(<HeroBanner title="Test" imageSource={testImageUrl} />);
-
-    expect(wrapper.find('.HeroBanner').hasClass('fullViewHeight')).toStrictEqual(false);
   });
 });
