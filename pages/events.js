641a2ff71bae97c065ac528cb7af4bb9631d9049
Kyle Holmberg <kylemh.email@gmail.com>
2019-02-22 19:10:30 -0800

    Use <Content> instead of <Section> on /event

--- pages/events.js
+++ pages/events.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import TrackVisibility from 'react-on-screen';
 import Head from 'components/head';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
-import Section from 'components/_common_/Section/Section';
+import Content from 'components/Content/Content';
 import LinkButton from 'components/_common_/LinkButton/LinkButton';
 import Heading from 'components/_common_/Heading/Heading';
 import Badge from 'components/Badge/Badge';
@@ -53,58 +53,46 @@ export default () => (
   <>
     <Head title="Events" />
 
-    <div className={styles.eventsPage}>
-      <HeroBanner
-        backgroundImageSource={`${s3}redesign/heroBanners/events.jpg`}
-        className={styles.heroBannerMobilePositioning}
-        title="You Can Make An Impact"
-      >
-        <>
-          <p>
-            Whether you&apos;re interested in mentoring, supporting our operations, or simply
-            donating to help military veterans, service members, and spouses launch a career in
-            software development, Operation Code is always looking for supporters like you.
-          </p>
+    <HeroBanner
+      backgroundImageSource={`${s3}redesign/heroBanners/events.jpg`}
+      className={styles.heroBannerMobilePositioning}
+      title="You Can Make An Impact"
+    >
+      <>
+        <p>
+          Whether you&apos;re interested in mentoring, supporting our operations, or simply donating
+          to help military veterans, service members, and spouses launch a career in software
+          development, Operation Code is always looking for supporters like you.
+        </p>
 
-          <LinkButton href="/who_we_serve">Learn More</LinkButton>
-        </>
-      </HeroBanner>
+        <LinkButton href="/who_we_serve">Learn More</LinkButton>
+      </>
+    </HeroBanner>
 
-      <Section theme="gray" contentClassName={styles.alignCenter}>
-        <Heading hasHeadingLines={false} className={styles.sectionHeading} theme="secondary">
-          The Power Of Mentorship
-        </Heading>
-
-        <p className={styles.marginBottom}>By mentoring one of our members, you will help them:</p>
-
+    <Content
+      title="The Power of Mentorship"
+      theme="gray"
+      columns={[
+        <p>By mentoring one of our members, you will help them:</p>,
         <div className={styles.badgeGroupings}>
           {mentorItems.map(item => (
             <Badge key={item.label} icon={item.icon} label={item.label} className={styles.badge} />
           ))}
-        </div>
-      </Section>
+        </div>,
+      ]}
+    />
 
-      <Section theme="secondary" contentClassName={styles.grid}>
+    <Content
+      columns={[
         <TrackVisibility offset={VISIBILITY_OFFSET}>
           {({ isVisible }) => (
-            <div
-              className={classNames(
-                styles.alignRight,
-                styles.offsetImage,
-                isVisible && styles.showImage,
-              )}
-            >
+            <div className={classNames(styles.image, isVisible && styles.showImage)}>
               <img src={`${s3}redesign/images/woman.jpg`} alt="Woman smiling at computer" />
             </div>
           )}
-        </TrackVisibility>
-
-        <div className={styles.gridContent}>
-          <Heading
-            className={classNames(styles.alignLeft, styles.sectionHeading)}
-            hasHeadingLines={false}
-            theme="white"
-          >
+        </TrackVisibility>,
+        <div>
+          <Heading hasHeadingLines={false} theme="white">
             Empower Our Community
           </Heading>
 
@@ -113,34 +101,36 @@ export default () => (
             are eager to transition into a software development career.
           </p>
 
-          <LinkButton href="/mentor">Become A Mentor</LinkButton>
-        </div>
-      </Section>
+          <div className={styles.centeredText}>
+            <LinkButton href="/mentor">Become A Mentor</LinkButton>
+          </div>
+        </div>,
+      ]}
+    />
 
-      <Section theme="gray" className={styles.sectionHeading} contentClassName={styles.alignCenter}>
-        <Heading hasHeadingLines={false} theme="secondary">
-          WAYS YOU CAN SUPPORT OPERATION CODE
-        </Heading>
-
-        <p className={classNames(styles.marginBottom, styles.centerText)}>
-          We&apos;re always looking for volunteers who are dedicated to making an impact in the
-          lives of military veterans, service members, and spouses. You can help us with:
-        </p>
-
+    <Content
+      theme="gray"
+      title="Ways You Can Support Operation Code"
+      columns={[
+        <div>
+          <p>
+            We&apos;re always looking for volunteers who are dedicated to making an impact in the
+            lives of military veterans, service members, and spouses.
+          </p>
+          <p className={styles.centeredText}>You can help us with:</p>
+        </div>,
         <div className={classNames(styles.badgeGroupings)}>
           {supportItems.map(item => (
             <Badge key={item.label} icon={item.icon} label={item.label} className={styles.badge} />
           ))}
-        </div>
-      </Section>
+        </div>,
+      ]}
+    />
 
-      <Section theme="secondary" contentClassName={styles.grid}>
-        <div className={classNames(styles.alignRight, styles.gridContent)}>
-          <Heading
-            className={classNames(styles.alignLeft, styles.sectionHeading)}
-            hasHeadingLines={false}
-            theme="white"
-          >
+    <Content
+      columns={[
+        <div>
+          <Heading hasHeadingLines={false} theme="white">
             Support Our Mission
           </Heading>
 
@@ -149,61 +139,41 @@ export default () => (
             are eager to transition into a software development career.
           </p>
 
-          <LinkButton href="/get_involved">Get Involved</LinkButton>
-        </div>
-
+          <div className={styles.centeredText}>
+            <LinkButton href="/get_involved">Get Involved</LinkButton>
+          </div>
+        </div>,
         <TrackVisibility offset={VISIBILITY_OFFSET}>
           {({ isVisible }) => (
-            <div
-              className={classNames(
-                styles.alignRight,
-                styles.offsetImage,
-                isVisible && styles.showImage,
-              )}
-            >
+            <div className={classNames(styles.image, isVisible && styles.showImage)}>
               <img
                 src={`${s3}redesign/images/utah_meetup.jpg`}
                 alt="Operation Code members collaborate on a problem"
               />
             </div>
           )}
-        </TrackVisibility>
-      </Section>
+        </TrackVisibility>,
+      ]}
+    />
 
-      {/* TODO: REMOVE PLACEHOLDER CONTENT */}
-      <Section theme="gray" contentClassName={styles.alignCenter}>
-        <Heading hasHeadingLines={false} theme="secondary">
-          Donate To Deploy The Future
-        </Heading>
-
+    {/* TODO: REMOVE PLACEHOLDER CONTENT */}
+    <Content
+      title="Donate To Deploy The Future"
+      theme="gray"
+      columns={[
         <p className={styles.centerText}>
           Help us reach our fundraising goal! With your donation, we&apos;ll be able to provide our
           members with coding school scholarships, tickets to tech conferences, and access to
           necessary equipment.
-        </p>
-
-        <div className={styles.progressBarInfo}>
-          <div className={styles.donatedAmount}>
-            <span className={styles.amount}>$300.00</span>
-            <span>Donated</span>
-          </div>
-          <div className={styles.goalAmount}>
-            <span>Goal:</span>
-            <span>$100,000</span>
-          </div>
-        </div>
-
-        <div className={styles.progressBar}>
-          <div style={{ width: '0px' }} className={styles.progressBarFill} />
-        </div>
-
+        </p>,
+        <div className={styles.progressBar}>Progress Bar Here Eventually...</div>,
         <LinkButton href="/donate" isFilled>
           Make A Donation
-        </LinkButton>
-      </Section>
-      {/* PLACEHOLDER CONTENT ABOVE */}
+        </LinkButton>,
+      ]}
+    />
+    {/* PLACEHOLDER CONTENT ABOVE */}
 
-      <JoinSection />
-    </div>
+    <JoinSection />
   </>
 );
