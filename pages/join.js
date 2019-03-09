8b777323b0eea51344cfaf85d5c248e55400e945
Kyle Holmberg <kylemh.email@gmail.com>
2019-03-09 02:10:50 -0800

    Hide <RegistrationForm> from /join

--- pages/join.js
+++ pages/join.js
@@ -1,7 +1,7 @@
 import Head from 'components/head';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
-import Content from 'components/Content/Content';
-import RegistrationForm from 'components/RegistrationForm/RegistrationForm';
+// import Content from 'components/Content/Content';
+// import RegistrationForm from 'components/RegistrationForm/RegistrationForm';
 
 export default () => (
   <>
@@ -9,6 +9,6 @@ export default () => (
 
     <HeroBanner title="Join" />
 
-    <Content theme="gray" title="Join" hasTitleUnderline columns={[<RegistrationForm />]} />
+    {/* <Content theme="gray" title="Join" hasTitleUnderline columns={[<RegistrationForm />]} /> */}
   </>
 );
