6918ab611117731e0656a30b6b096a0a9b025c95
Kyle Holmberg <kylemh.email@gmail.com>
2019-03-11 00:55:51 -0700

    Add all required inputs to registration form and style it

---
+++ components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,210 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
+import { minPasswordCharNum, validationErrorMessages } from 'common/constants/validations';
+import { capitalizeFirstLetter } from 'common/utils/string-utils';
+import { isMinPasswordStrength, isValidZipcode } from 'common/utils/validator-utils';
+import Button from 'components/Button/Button';
+import Form from 'components/Form/Form';
+import Input from 'components/Form/Input/Input';
+import Alert from 'components/Alert/Alert';
+import styles from './RegistrationForm.css';
+
+/*
+ * NOTE: We're repeating hardcode between the registration schema and passing an asterisk
+ * to the label of required fields. This seems to an unfortunate negative aspect of an otherwise
+ * awesome library. More importantly, it looks like the lib's author has plans to remedy the
+ * situation. For now, our forms wont change much, so we should be okay.
+ */
+const registrationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('Required')
+    .email(validationErrorMessages.email),
+  'confirm-email': Yup.string()
+    .required('Required')
+    .oneOf([Yup.ref('email')], validationErrorMessages.emailMatch),
+  password: Yup.string()
+    .required('Required')
+    .min(minPasswordCharNum, validationErrorMessages.length(minPasswordCharNum))
+    .test('password-strength', validationErrorMessages.password, isMinPasswordStrength),
+  'confirm-password': Yup.string()
+    .required('Required')
+    .oneOf([Yup.ref('password')], validationErrorMessages.passwordMatch),
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  zipcode: Yup.string()
+    .required('Required')
+    .test('zipcode', validationErrorMessages.zipcode, isValidZipcode),
+});
+
+// TODO: Define links for terms of service / privacy policy and place on this page
+
+class RegistrationForm extends Component {
+  static propTypes = {
+    register: PropTypes.func.isRequired, // essentially onSubmit
+    onSuccess: PropTypes.func.isRequired,
+    initialValues: PropTypes.shape({
+      email: PropTypes.string,
+      'confirm-email': PropTypes.string,
+      password: PropTypes.string,
+      'confirm-password': PropTypes.string,
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      zipcode: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }),
+  };
+
+  static defaultProps = {
+    initialValues: {
+      email: '',
+      'confirm-email': '',
+      password: '',
+      'confirm-password': '',
+      firstName: '',
+      lastName: '',
+      zipcode: '',
+    },
+  };
+
+  state = {
+    errorMsg: '',
+  };
+
+  handleSubmit = async (values, actions) => {
+    const { register, onSuccess } = this.props;
+
+    try {
+      await register({ ...values });
+      actions.setSubmitting(false);
+      actions.resetForm();
+
+      onSuccess();
+    } catch (error) {
+      actions.setSubmitting(false);
+
+      const { data } = error.response;
+
+      // TODO: Create back-end ticket for checking if email has been taken for a debounced,
+      // client-side validation of emails instead of waiting for submission.
+      const errorMsg = Object.keys(data)
+        .map(key => {
+          const fieldName = capitalizeFirstLetter(key);
+
+          // example: Email has already been taken.
+          return `${fieldName} ${data[key][0]}.`;
+        })
+        .join('\n');
+
+      this.setState({ errorMsg });
+    }
+  };
+
+  closeAlert = () => this.setState({ errorMsg: '' });
+
+  render() {
+    const { props, state } = this;
+
+    return (
+      <Formik
+        initialValues={props.initialValues}
+        onSubmit={this.handleSubmit}
+        validationSchema={registrationSchema}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className={styles.row}>
+              <Field
+                type="email"
+                name="email"
+                label="Email*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="username email"
+              />
+
+              <Field
+                type="email"
+                name="confirm-email"
+                label="Confirm Email*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="username email"
+              />
+            </div>
+
+            <div className={styles.row}>
+              <Field
+                type="password"
+                name="password"
+                label="Password*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="new-password"
+              />
+
+              <Field
+                type="password"
+                name="confirm-password"
+                label="Confirm Password*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="new-password"
+              />
+            </div>
+
+            <div className={styles.row}>
+              <Field
+                type="text"
+                name="firstName"
+                label="First Name*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="given-name"
+              />
+
+              <Field
+                type="text"
+                name="lastName"
+                label="Last Name*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="family-name"
+              />
+            </div>
+
+            <div className={styles.row}>
+              <Field
+                type="text"
+                name="zipcode"
+                label="Zipcode*"
+                component={Input}
+                disabled={isSubmitting}
+                autoComplete="postal-code"
+              />
+            </div>
+
+            <div className={styles.row}>
+              <Alert isOpen={Boolean(state.errorMsg)} type="error" onToggle={this.closeAlert}>
+                {state.errorMsg}
+              </Alert>
+            </div>
+
+            <div className={styles.row}>
+              <Button
+                className={styles.topSpacing}
+                type="submit"
+                theme="secondary"
+                disabled={isSubmitting}
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+}
+
+export default RegistrationForm;
