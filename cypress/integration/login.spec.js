6efc068c1d1d1bd7e411c526511278c4fb28b958
Kyle Holmberg <kylemh.email@gmail.com>
2019-03-12 17:43:19 -0700

    Submit some e2e tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,26 @@
-describe('My First Test', function() {
-  it('should not do much!', () => {
-    expect(true).to.equal(true);
+beforeEach(() => {
+  cy.clearCookies();
+});
+
+describe('login', function() {
+  it('should be able to login with valid credentials', () => {
+    cy.getCookies().should('have.length', 0);
+    cy.visit('/');
+    cy.get('[data-testid="Nav Item Member Login"]').should('not.be.visible');
+    cy.get('[data-testid="Nav Item Member Login"]').click({ force: true });
+    cy.get('h1').should('have.text', 'Login');
+    cy.get('input#email').type('kylemh.email12@gmail.com');
+    cy.get('input#password').type('Testing1');
+    cy.get('button[type="submit"]').click();
+    cy.get('h1').should('have.text', 'Profile');
+    cy.get('p').contains('Hello Kyle!');
+    cy.getCookies()
+      .should('have.length', 4)
+      .then(cookies => {
+        expect(cookies[0].value).to.contain('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9');
+        expect(cookies[1].value).to.equal('Kyle');
+        expect(cookies[2].value).to.equal('Holmberg');
+        expect(cookies[3].value).to.equal('90630');
+      });
   });
 });
---
+++ cypress/integration/register.spec.js
@@ -0,0 +1,56 @@
+const createRandomEmail = () => {
+  const listOfTLDs = ['com', 'org', 'gov.uk', 'gov.us', 'edu', 'io', 'dev', 'us', 'es', 'co'];
+
+  const randomString1 = Math.random()
+    .toString(36)
+    .substring(2);
+
+  const randomString2 = Math.random()
+    .toString(36)
+    .substring(2);
+
+  const randomTLD = listOfTLDs[Math.floor(Math.random() * listOfTLDs.length)];
+
+  return `${randomString1}@${randomString2}.${randomTLD}`;
+};
+
+beforeEach(() => {
+  cy.clearCookies();
+});
+
+describe('register', function() {
+  it('should be able to register with valid data', () => {
+    // Fake email generator with rare conflicts
+
+    const newUser = {
+      email: createRandomEmail(),
+      password: 'Testing1',
+      firstName: 'Test',
+      lastName: 'User',
+      zipcode: 12345,
+    };
+
+    cy.getCookies().should('have.length', 0);
+    cy.visit('/');
+    cy.get('[data-testid="Nav Item Join"]').should('not.be.visible');
+    cy.get('[data-testid="Nav Item Join"]').click({ force: true });
+    cy.get('h1').should('have.text', 'Join');
+    cy.get('input#email').type(newUser.email);
+    cy.get('input#confirm-email').type(newUser.email);
+    cy.get('input#password').type(newUser.password);
+    cy.get('input#confirm-password').type(newUser.password);
+    cy.get('input#firstName').type(newUser.firstName);
+    cy.get('input#lastName').type(newUser.lastName);
+    cy.get('input#zipcode').type(newUser.zipcode);
+    cy.get('button[type="submit"]').click();
+    cy.get('h1').should('have.text', 'Profile');
+    cy.get('p').contains(`Hello ${newUser.firstName}!`);
+    cy.getCookies()
+      .should('have.length', 4)
+      .then(cookies => {
+        expect(cookies[1].value).to.equal(newUser.firstName);
+        expect(cookies[2].value).to.equal(newUser.lastName);
+        expect(cookies[3].value).to.equal(`${newUser.zipcode}`); // number to string 🤷‍♂️
+      });
+  });
+});
