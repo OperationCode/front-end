db89e95e3e6affa92958b9b2c8696ed4d5415daf
Kyle Holmberg <inbox@kylemh.com>
2019-04-02 00:26:34 -0700

    Resolve issues preventing rendering of /team

--- pages/team.js
+++ pages/team.js
@@ -1,22 +1,22 @@
+import HeroBanner from 'components/HeroBanner/HeroBanner';
+import Content from 'components/Content/Content';
+import Card from 'components/Cards/Card/Card';
+import FlatCard from 'components/Cards/FlatCard/FlatCard';
 import { getTeamMembersPromise } from 'common/constants/api';
 import { s3 } from 'common/constants/urls';
-import QuoteBanner from 'components/HeroBanner/HeroBanner';
-import Section from 'components/_common_/Section/Section';
-import TeamCard from 'components/_common_/TeamCard/TeamCard';
 import styles from './styles/team.css';
 
 export default class team extends React.Component {
-    state = {
-      boardMembers: null,
-      staffMembers: null,
-    };
-  
+  state = {
+    boardMembers: [],
+    staffMembers: [],
+  };
 
   async componentDidMount() {
     const { data } = await getTeamMembersPromise();
-    const json = data;
-    const boardMembers = json.filter(x => x.group === 'board');
-    const staffMembers = json.filter(x => x.group === 'team');
+    const boardMembers = data.filter(x => x.group === 'board');
+    const staffMembers = data.filter(x => x.group === 'team');
+
     const boardChair = 'David Molina';
     const CEO = 'Conrad Hollomon';
 
@@ -36,54 +36,77 @@ export default class team extends React.Component {
 
   render() {
     const { boardMembers, staffMembers } = this.state;
+
     return (
       <div>
-        <QuoteBanner
+        <HeroBanner
+          backgroundImageSource={`${s3}heroBanners/lincoln.jpg`}
           className={styles.lincoln}
-          imageSource={`${s3}heroBanners/lincoln.jpg`}
-          author="Abraham Lincoln"
-          quote="To care for him who shall have borne the battle and for his widow, and his orphan."
+        >
+          Abraham Lincoln To care for him who shall have borne the battle and for his widow, and his
+          orphan.
+        </HeroBanner>
+
+        <Content
+          columns={[
+            <div className={styles.boardMembers}>
+              {boardMembers.map(({ name, role, image_src: imageSource, description }) => (
+                <FlatCard
+                  key={`${name} + ${role}`}
+                  header={
+                    <>
+                      <h3>{name}</h3>
+                      <br />
+                      <h5>{role}</h5>
+                    </>
+                  }
+                  imageSource={imageSource}
+                >
+                  {description}
+                </FlatCard>
+              ))}
+            </div>,
+            <div className={styles.foundingMembers}>
+              <p>
+                Operation Code deeply appreciates the time, energy, and hard work of our{' '}
+                <b>Founding Board Members</b>, including Mark Kerr (Chair), Laura Gomez (Vice
+                Chair), Dr. Tyrone Grandison (Vice Chair), Dr. Stacy Chin (Director of Fundraising
+                Committee), Liza Rodewald (Director of Military Families Committee), Pete Runyon
+                (Secretary/ Treasurer), Josh Carter, Nick Frost, and Aimee Knight on their support,
+                dedication and commitment in the early days.
+              </p>
+
+              <p style={{ textAlign: 'center' }}>
+                <em>Thank you for setting us up for success!</em>
+              </p>
+            </div>,
+          ]}
+          title="Our Board"
+          theme="white"
         />
-        <Section title="Our Board" theme="white">
-          <div className={styles.boardMembers}>
-            {boardMembers.map(boardMember => (
-              <TeamCard
-                key={`${boardMember.name} + ${boardMember.role}`}
-                name={boardMember.name}
-                role={boardMember.role}
-                description={boardMember.description}
-                imageSrc={boardMember.image_src}
-              />
-            ))}
-          </div>
-          <div className={styles.foundingMembers}>
-            <p>
-              Operation Code deeply appreciates the time, energy, and hard work of our{' '}
-              <b>Founding Board Members</b>, including Mark Kerr (Chair), Laura Gomez (Vice Chair),
-              Dr. Tyrone Grandison (Vice Chair), Dr. Stacy Chin (Director of Fundraising Committee),
-              Liza Rodewald (Director of Military Families Committee), Pete Runyon (Secretary/
-              Treasurer), Josh Carter, Nick Frost, and Aimee Knight on their support, dedication and
-              commitment in the early days.
-            </p>
-            <p style={{ textAlign: 'center' }}>
-              <em>Thank you for setting us up for success!</em>
-            </p>
-          </div>
-        </Section>
-        <Section title="Our Staff" theme="white">
-          <div className={styles.staffMembers}>
-            {staffMembers.map(staffMember => (
-              <TeamCard
-                key={`${staffMember.name} + ${staffMember.role}`}
-                name={staffMember.name}
-                role={staffMember.role}
-                slack={staffMember.slackUsername}
-                email={staffMember.email}
-                isBoard={false}
-              />
-            ))}
-          </div>
-        </Section>
+
+        <Content
+          columns={staffMembers.map(({ name, role, slackUsername, email }) => (
+            <Card key={`${name} + ${role}`}>
+              <h4>{name}</h4>
+
+              <hr />
+
+              <h5>{role}</h5>
+
+              <br />
+
+              <p>
+                Slack: <i>{slackUsername}</i>
+              </p>
+              <p>
+                Email: <i>{email}</i>
+              </p>
+            </Card>
+          ))}
+          title="Our Staff"
+          theme="white"
+        />
       </div>
     );
   }
