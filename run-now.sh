734e434f3afc793b3104b1b188d58842fbf53053
Kyle Holmberg <kylemh.email@gmail.com>
2019-04-07 13:40:00 -0700

    build after e2e

--- run-now.sh
+++ run-now.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -eu
-curl "https://api.zeit.co/v2/now/deployments" \
-  -H "Authorization: Bearer $NOW_TOKEN"
 yarn add now
 now --local-config=./now.master.json --token $NOW_TOKEN
 now alias --local-config=./now.master.json --token $NOW_TOKEN
