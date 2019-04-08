ced134ccc7c93c110bfb4c1102bce9b7e6561a09
Kyle Holmberg <kylemh.email@gmail.com>
2019-04-08 00:12:25 -0700

    Resolve margin issue

--- components/FAQ/FAQItem/FAQItem.js
+++ components/FAQ/FAQItem/FAQItem.js
@@ -8,11 +8,14 @@ FAQItem.propTypes = {
 };
 
 function FAQItem({ question, answer }) {
+  // Create an HTML-safe ID for input query selecting
+  const questionID = question.replace(/[.,/#!?@#$%^&*;:{}=\-_'`~()]/g, '').replace(/\s{2,}/g, ' ');
+
   return (
     <div className={styles.accordionSingle}>
-      <input className={styles.accordionSingleHidden} type="checkbox" id={question} />
+      <input className={styles.accordionSingleHidden} type="checkbox" id={questionID} />
 
-      <label className={styles.accordionSingleQuestion} htmlFor={question}>
+      <label className={styles.accordionSingleQuestion} htmlFor={questionID}>
         {question}
       </label>
 
