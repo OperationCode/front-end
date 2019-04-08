b8002d69fbd5a13aaa2f6c632ac1fb0970e6661e
Kyle Holmberg <kylemh.email@gmail.com>
2019-04-07 18:53:11 -0700

    Put a real form in <JoinSection> and add tests

--- components/ReusableSections/JoinSection/JoinSection.js
+++ components/ReusableSections/JoinSection/JoinSection.js
@@ -1,29 +1,112 @@
 import React from 'react';
-import Container from 'components/Container/Container';
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
+import { addMailchimpSubscriber } from 'common/constants/api';
+import successStateMessages from 'common/constants/successStateMessages';
+import { validationErrorMessages } from 'common/constants/validations';
+import Alert from 'components/Alert/Alert';
 import Button from 'components/Button/Button';
+import Container from 'components/Container/Container';
+import Form from 'components/Form/Form';
+import Input from 'components/Form/Input/Input';
 import OutboundLink from 'components/OutboundLink/OutboundLink';
+import styles from './JoinSection.css';
 
-const JoinSection = () => (
-  <Container theme="white">
-    <h3>Join Our Thriving Community</h3>
-
-    <p>
-      Are you ready to begin your journey towards a career in software development?
-      <br />
-      Get the support you need by joining our members only Slack community!
-    </p>
-
-    <form>
-      <input placeholder="Email address" />
-      <Button type="submit">Join our Slack</Button>
-    </form>
-
-    <p>Slack is a community based collaboration tool where all the magic happens!</p>
-
-    <OutboundLink href="https://slack.com/" analyticsEventLabel="Learn More About Slack">
-      Learn more
-    </OutboundLink>
-  </Container>
+// eslint-disable-next-line react/prop-types
+const CustomInput = ({ form, field, ...otherProps }) => (
+  <Input
+    className={styles.newsletterInput}
+    shouldHideError
+    shouldHideLabel
+    form={form}
+    field={field}
+    {...otherProps}
+  />
 );
+
+const newsletterSchema = Yup.object().shape({
+  email: Yup.string()
+    .required(validationErrorMessages.required)
+    .email(validationErrorMessages.email),
+});
+
+class JoinSection extends React.Component {
+  state = {
+    errorMsg: '',
+    successMsg: '',
+  };
+
+  handleSubmit = async (values, actions) => {
+    try {
+      await addMailchimpSubscriber(values);
+
+      actions.setSubmitting(false);
+      actions.resetForm();
+
+      this.setState({ successMsg: successStateMessages.newsletterSubscription });
+    } catch (error) {
+      actions.setSubmitting(false);
+
+      const { data } = error.response;
+
+      this.setState({ errorMsg: `Error ${data}` });
+    }
+  };
+
+  render() {
+    const { state } = this;
+
+    return (
+      <Container theme="white">
+        <h3>Join Our Thriving Community</h3>
+
+        <p>
+          Are you ready to begin your journey towards a career in software development?
+          <br />
+          Get the support you need by joining our members only Slack community!
+        </p>
+
+        <Formik
+          initialValues={{ email: '' }}
+          onSubmit={this.handleSubmit}
+          validationSchema={newsletterSchema}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div className={styles.row}>
+                <Field
+                  type="email"
+                  name="email"
+                  label="Email*"
+                  component={CustomInput}
+                  disabled={isSubmitting}
+                  autoComplete="username email"
+                />
+
+                <Button type="submit" disabled={isSubmitting} className={styles.newsletterButton}>
+                  Submit
+                </Button>
+              </div>
+
+              <Alert isOpen={Boolean(state.successMsg)} type="success">
+                {state.successMsg}
+              </Alert>
+
+              <Alert isOpen={Boolean(state.errorMsg)} type="error">
+                {state.errorMsg}
+              </Alert>
+            </Form>
+          )}
+        </Formik>
+
+        <p>Slack is a community based collaboration tool where all the magic happens!</p>
+
+        <OutboundLink href="https://slack.com/" analyticsEventLabel="Learn More About Slack">
+          Learn more
+        </OutboundLink>
+      </Container>
+    );
+  }
+}
 
 export default JoinSection;
--- components/ReusableSections/JoinSection/__tests__/JoinSection.test.js
+++ components/ReusableSections/JoinSection/__tests__/JoinSection.test.js
@@ -1,5 +1,8 @@
 /* eslint-env jest */
 import React from 'react';
+import { mount } from 'enzyme';
+import successStateMessages from 'common/constants/successStateMessages';
+import asyncWrapperRender from 'test-utils/asyncRenderUpdate';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import JoinSection from '../JoinSection';
@@ -7,5 +10,35 @@ import JoinSection from '../JoinSection';
 describe('JoinSection', () => {
   it('should render with required props', () => {
     createSnapshotTest(<JoinSection />);
+  });
+
+  it('should not display Input error message when blurring past email input', async () => {
+    const wrapper = mount(<JoinSection />);
+
+    wrapper.find('input#email').simulate('blur');
+
+    await asyncWrapperRender(wrapper);
+
+    expect(wrapper.find('Input[type="email"]').find('Alert')).not.toExist();
+  });
+
+  it('should show success message when submitting a valid email', async () => {
+    const wrapper = mount(<JoinSection />);
+
+    wrapper
+      .find('input#email')
+      .simulate('change', { target: { id: 'email', value: 'email12jk331b312@email.com' } })
+      .simulate('blur');
+
+    await asyncWrapperRender(wrapper);
+
+    wrapper.find('Button').simulate('submit');
+
+    await asyncWrapperRender(wrapper, 500);
+
+    const successAlert = wrapper.find('Alert').findWhere(t => t.props().type === 'success');
+    expect(successAlert).toExist();
+    expect(successAlert.props().isOpen).toStrictEqual(true);
+    expect(successAlert.text()).toStrictEqual(successStateMessages.newsletterSubscription);
   });
 });
