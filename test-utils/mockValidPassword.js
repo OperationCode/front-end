af9a06276d7b7d6634d3fa02e69d2ff6c7f63eef
Kyle Holmberg <kyle.holmberg@nike.com>
2019-04-26 22:11:09 -0700

    Create a utility method to mock a valid password

---
+++ test-utils/mockValidPassword.js
@@ -0,0 +1,6 @@
+import faker from 'faker';
+import { minPasswordCharNum } from 'common/constants/validations';
+
+export default function mockValidPassword() {
+  return `${faker.internet.password(minPasswordCharNum)}!1Aa`;
+}
