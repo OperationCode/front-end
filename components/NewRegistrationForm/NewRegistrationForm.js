c3439e1e4d42b91e69726fa938c5b8034bdf9bc6
Kyle Holmberg <kylemh.email@gmail.com>
2019-05-02 00:44:21 -0700

    Finalize manner of providing steps to MultiStepForm

--- components/NewRegistrationForm/NewRegistrationForm.js
+++ components/NewRegistrationForm/NewRegistrationForm.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { login } from 'common/utils/auth-utils';
+import { insertIf } from 'common/utils/array-utils';
 import MultiStepForm from 'components/Form/MultiStepForm';
-import { steps, entireFormInitialValues } from './Steps';
+import {
+  InitialStep,
+  PersonalDetailsStep,
+  ClassificationStep,
+  MilitaryStep,
+  TechnologyStep,
+} from './Steps';
 
 class NewRegistrationForm extends Component {
   static propTypes = {
@@ -11,24 +18,86 @@ class NewRegistrationForm extends Component {
 
   static defaultProps = {
     initialValues: {
-      ...entireFormInitialValues,
+      // order doesn't matter here
+      ...InitialStep.initialValues,
+      ...PersonalDetailsStep.initialValues,
+      ...ClassificationStep.initialValues,
+      ...MilitaryStep.initialValues,
+      ...TechnologyStep.initialValues,
     },
   };
 
-  handleSuccess = ({ token, user }) => {
-    login({ token, user });
+  state = {
+    shouldShowMilitaryStep: false,
+  };
+
+  requireMilitaryStep = () => {
+    this.setState({ shouldShowMilitaryStep: true });
+  };
+
+  hideMilitaryStep = () => {
+    this.setState({ shouldShowMilitaryStep: false });
+  };
+
+  // This method was only created to modularize logic
+  defineSteps = () => {
+    const { shouldShowMilitaryStep } = this.state;
+
+    // Define each step to provide control of each step (particularly, the rendering)
+    const Initial = {
+      render: props => <InitialStep {...props} />,
+      validationSchema: InitialStep.validationSchema,
+      submitHandler: InitialStep.submitHandler,
+    };
+
+    const PersonalDetails = {
+      render: props => <PersonalDetailsStep {...props} />,
+      validationSchema: PersonalDetailsStep.validationSchema,
+      submitHandler: PersonalDetailsStep.submitHandler,
+    };
+
+    const Classification = {
+      render: props => (
+        <ClassificationStep
+          {...props}
+          onSelectMilitary={this.requireMilitaryStep}
+          onSelectOther={this.hideMilitaryStep}
+        />
+      ),
+      validationSchema: ClassificationStep.validationSchema,
+      submitHandler: ClassificationStep.submitHandler,
+    };
+
+    const MilitaryInfo = {
+      render: props => <MilitaryStep {...props} />,
+      validationSchema: MilitaryStep.validationSchema,
+      submitHandler: MilitaryStep.submitHandler,
+    };
+
+    const TechnologyPreferences = {
+      render: props => <TechnologyStep {...props} />,
+      validationSchema: TechnologyStep.validationSchema,
+      submitHandler: TechnologyStep.submitHandler,
+    };
+
+    // Define structure of form
+    const orderedArrayOfSteps = [
+      Initial,
+      PersonalDetails,
+      Classification,
+      ...insertIf(shouldShowMilitaryStep, MilitaryInfo),
+      TechnologyPreferences,
+    ];
+
+    return orderedArrayOfSteps;
   };
 
   render() {
     const { initialValues } = this.props;
 
-    return (
-      <MultiStepForm
-        initialValues={initialValues}
-        onFinalStepSuccess={this.handleSuccess}
-        steps={steps}
-      />
-    );
+    const steps = this.defineSteps();
+
+    return <MultiStepForm initialValues={initialValues} onFinalStepSuccess={login} steps={steps} />;
   }
 }
 
--- components/NewRegistrationForm/Steps/index.js
+++ components/NewRegistrationForm/Steps/index.js
@@ -1,30 +1,7 @@
-import React from 'react';
 import InitialStep from './InitialStep/InitialStep';
 import PersonalDetailsStep from './PersonalDetailsStep/PersonalDetailsStep';
 import ClassificationStep from './ClassificationStep/ClassificationStep';
 import MilitaryStep from './MilitaryStep/MilitaryStep';
 import TechnologyStep from './TechnologyStep/TechnologyStep';
 
-const orderedSteps = [
-  InitialStep,
-  PersonalDetailsStep,
-  ClassificationStep,
-  MilitaryStep,
-  TechnologyStep,
-];
-
-export const steps = orderedSteps.map(Step => {
-  // if fields are changed/added/removed, please adjust propTypes inside MultiStepForm
-  return {
-    render: props => <Step {...props} />,
-    validationSchema: Step.validationSchema,
-    submitHandler: Step.submitHandler,
-  };
-});
-
-export const entireFormInitialValues = orderedSteps.reduce((previousValue, component) => {
-  return {
-    ...previousValue,
-    ...component.initialValues,
-  };
-}, {});
+export { InitialStep, PersonalDetailsStep, ClassificationStep, MilitaryStep, TechnologyStep };
