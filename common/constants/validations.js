e136fd70daa8c29352e828871c8eff4ff3f9a35d
Kyle Holmberg <kylemh.email@gmail.com>
2019-05-03 22:26:28 -0700

    Update password regex and add tests

--- common/constants/validations.js
+++ common/constants/validations.js
@@ -1,2 +1,2 @@
 export const minimumPasswordLength = 8;
-export const passwordStrengthRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/;
+export const passwordStrengthRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+/;
