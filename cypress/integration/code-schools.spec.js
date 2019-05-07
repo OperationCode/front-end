c684f026168e1fb2672153280b02a22abcbd5e06
chrismgonzalez <cmgonza89@gmail.com>
2019-05-06 20:23:10 -0500

    add beforeEach method; DRY'd up the code

--- cypress/integration/code-schools.spec.js
+++ cypress/integration/code-schools.spec.js
@@ -1,12 +1,16 @@
-// Assert that when the user navigates to the page, the code schools are all rendered on the page
-describe('visit page', () => {
-  it('visits /code-schools page', () => {
-    cy.visit('/code_schools');
-  });
-});
+describe('code schools', function() {
+  beforeEach(() => {
+    cy.server();
+    cy.route('GET', '/api/v1/code_schools').as('codeSchools');
 
-describe('render code school cards', () => {
-  it('should render code school cards', () => {
-    cy.get('div.code_schools_schoolCardsWrapper__SXWXI');
+    cy.visitAndWaitFor('/code_schools');
+
+    cy.get('h1').should('have.text', 'Code Schools');
+  });
+
+  describe('render code school cards', () => {
+    it('should render code school cards', () => {
+      cy.get('div.code_schools_schoolCardsWrapper__SXWXI');
+    });
   });
 });
