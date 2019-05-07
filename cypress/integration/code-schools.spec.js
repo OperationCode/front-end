7d71062ece7c8b7edc0adda362f1384996fa58ab
Phong Doan <phong.v.doan89@outlook.com>
2019-05-07 00:01:23 -0700

    Added render tests for code schools

--- cypress/integration/code-schools.spec.js
+++ cypress/integration/code-schools.spec.js
@@ -14,4 +14,31 @@ describe('code schools', function() {
     // 40 is arbitrary, but it proves that the API is working and leading to rendered content
     cy.get('[data-testid="SchoolCard"]').should('have.length.greaterThan', 40);
   });
+
+  it('should render Cincy and Tech Elevator after selecting Ohio', () => {
+    cy.wait('@codeSchools');
+    cy.get('input#react-select-state_select-input')
+      .type('Ohio', { force: true })
+      .type('{enter}');
+
+    cy.get('[data-testid="SchoolCard"]').should('have.length', 2);
+  });
+
+  it('should render GI Bill accepted schools after hitting VA button', () => {
+    cy.wait('@codeSchools');
+    cy.contains('VA Approved Schools').click();
+
+    cy.get('[data-testid="SchoolCard"]').should('have.length.greaterThan', 20);
+  });
+
+  // /Online Schools test
+
+  it('should render no schools after selecting Alaska', () => {
+    cy.wait('@codeSchools');
+    cy.get('input#react-select-state_select-input')
+      .type('Alaska', { force: true })
+      .type('{enter}');
+
+    cy.get('[data-testid="SchoolCard"]').should('have.length', 0);
+  });
 });
