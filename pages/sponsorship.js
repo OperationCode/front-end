4ed7fe46234db31274e34368e866846c5f031ea0
CherieLee-Mason <clsoar@gmail.com>
2019-05-13 09:52:21 -0500

    Become a sponsor content section added

--- pages/sponsorship.js
+++ pages/sponsorship.js
@@ -1,5 +1,7 @@
 import Head from 'components/head';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
+import Content from 'components/Content/Content';
+import styles from './styles/sponsorship.css';
 
 export default () => {
   const pageTitle = 'Corporate Sponsorship';
@@ -13,6 +15,18 @@ export default () => {
         {/* Call-to-action goes here */}
       </HeroBanner>
 
+      <Content
+        title="Become A Corporate Sponsor Today"
+        theme="white"
+        className={styles.justifyAlign}
+        columns={[
+          <p className="changeToSomething">
+            Operation Code is pleased to invite America’s leading technology companies to become
+            shared value sponsors. Please join us and help make our mission a success. Together, we
+            will create a new and secure future for today’s veterans and military spouses.
+          </p>,
+        ]}
+      />
       {/* Rest of page content goes in here */}
     </>
   );
