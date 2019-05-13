83dc354b5b506863d06b3935f0c932ff36ea3707
Shobhit Agarwal <shobhit657@gmail.com>
2019-05-12 22:30:38 -0700

    finalize removing unwanted organize import changes

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import classNames from 'classnames';
 import { bool, node, number, oneOf, oneOfType, string } from 'prop-types';
+import classNames from 'classnames';
 import styles from './Container.css';
 
 Container.propTypes = {
