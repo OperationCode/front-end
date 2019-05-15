a5b152577c907453c74ed0e9ebc118bc6d8e9324
Kevin K. Lee <kevin.kik.lee@gmail.com>
2019-05-15 08:05:27 -0400

    wip

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -12,54 +12,117 @@ export default class Pagination extends React.Component {
 
   static defaultProps = {
     className: undefined,
-    perPageCount: 10,
+    perPageCount: 2,
   };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      startPageNumber: 1,
-      currentPageNumber: 1,
+      startIndex: 0,
+      currentIndex: 0,
     };
   }
 
-  buildPageNumbers = () => {
-    const { perPageCount } = this.props;
-    const { startPageNumber, currentPageNumber } = this.state;
-    const pageNumbers = Array(perPageCount)
-      .fill(0)
-      .map((_, index) => index + startPageNumber);
+  buildPageIndices = () => {
+    const {
+      children: { length },
+      perPageCount,
+    } = this.props;
 
-    return pageNumbers.map(pageNumber => (
+    const { startIndex, currentIndex } = this.state;
+    const totalPageCount = Math.floor(length / perPageCount);
+    const pageIndices = Array(totalPageCount)
+      .fill(0)
+      .map((_, index) => index + startIndex);
+
+    return pageIndices.map(pageIndex => (
       <div
-        className={pageNumber === currentPageNumber ? styles.pageNumberActive : styles.pageNumber}
-        key={pageNumber}
+        className={pageIndex === currentIndex ? styles.pageIndexActive : styles.pageIndex}
+        key={pageIndex}
+        data-page-index={pageIndex}
+        data-action="selectPage"
       >
-        {pageNumber}
+        {pageIndex + 1}
       </div>
     ));
+  };
+  /*
+    c = 3
+    l = 8
+    0 1 2 / 3 4 5 / 6 7
+    1 2 3 / 4 5 6 / 7 8
+  */
+
+  handleControlOnClick = event => {
+    const {
+      children: { length },
+      perPageCount,
+    } = this.props;
+    const { action, pageIndex } = event.target.dataset;
+    console.log({ action });
+    console.log({ pageIndex });
+
+    this.setState(state => {
+      let { currentIndex, startIndex } = state;
+
+      if (action === 'firstPage') {
+        startIndex = 0;
+        currentIndex = 0;
+      } else if (action === 'previousPage') {
+        const shiftedStartIndex = startIndex - perPageCount;
+        startIndex = shiftedStartIndex <= 0 ? 1 : shiftedStartIndex;
+        currentIndex = startIndex;
+      } else if (action === 'nextPage') {
+        const shiftedStartIndex = startIndex + perPageCount;
+        startIndex = shiftedStartIndex >= length ? length : shiftedStartIndex;
+        currentIndex = startIndex;
+      } else if (action === 'lastPage') {
+        startIndex = Math.floor(length / perPageCount) * perPageCount + 1;
+        currentIndex = startIndex;
+      } else if (action === 'selectPage') {
+        startIndex = parseInt(pageIndex, 10) + 1;
+      }
+      console.log({ startIndex });
+      console.log({ currentIndex });
+      return { ...state, startIndex, currentIndex };
+    });
   };
 
   buildControls = () => {
     return (
-      <div className={styles.controls}>
-        <div className={styles.btnFirst}>First</div>
-        <div className={styles.btnPrevious}>Previous</div>
-        <div className={styles.rest}>{this.buildPageNumbers()}</div>
-        <div className={styles.btnNext}>Next</div>
-        <div className={styles.btnLast}>Last</div>
+      <div
+        className={styles.controls}
+        onClick={this.handleControlOnClick}
+        onKeyDown={this.handleControlOnKeyDown}
+        role="presentation"
+      >
+        <div data-action="firstPage" className={styles.btnFirstPage}>
+          First
+        </div>
+        <div data-action="previousPage" className={styles.btnPreviousPage}>
+          Previous
+        </div>
+        <div className={styles.rest}>{this.buildPageIndices()}</div>
+        <div data-action="nextPage" className={styles.btnNextPage}>
+          Next
+        </div>
+        <div data-action="lastPage" className={styles.btnLastPage}>
+          Last
+        </div>
       </div>
     );
   };
 
   render() {
     const { className, children, perPageCount } = this.props;
-    const { startPageNumber } = this.state;
+    const { startIndex } = this.state;
+    const endIndex =
+      startIndex + perPageCount >= children.length ? children.length : startIndex + perPageCount;
 
     return (
       <div className={classNames(className, styles.Pagination)}>
-        {children.slice(startPageNumber - 1, perPageCount + 1)}
+        {children.slice(startIndex, endIndex)}
         {this.buildControls()}
       </div>
     );
