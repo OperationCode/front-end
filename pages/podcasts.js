20835c4bc769790e05bfbb1b5483e92dc29569f3
cbituin <inbetweenwebdesigns@gmail.com>
2019-05-19 06:53:48 -0700

    Adds /podcast page

---
+++ pages/podcasts.js
@@ -0,0 +1,19 @@
+import Head from 'components/head';
+import HeroBanner from 'components/HeroBanner/HeroBanner';
+
+export default () => {
+  const pageTitle = 'Podcasts';
+
+  return (
+    <>
+      <Head title={pageTitle} />
+
+      <HeroBanner title={pageTitle}>
+        {/* Don't forget to define the imageSource prop in the HeroBanner Component */}
+        {/* Call-to-action goes here */}
+      </HeroBanner>
+
+      {/* Rest of page content goes in here */}
+    </>
+  );
+};
