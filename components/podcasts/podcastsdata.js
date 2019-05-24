156e55e3e08bab01eeb90e04b58b2a827a6a412a
cbituin <inbetweenwebdesigns@gmail.com>
2019-05-23 22:47:04 -0700

    /podcasts has complete cards for each episode. Data is hard coded in components/podcasts/podcastsdata

---
+++ components/podcasts/podcastsdata.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const podcastdata = {
+  general: [
+    {
+      source: 'http://traffic.libsyn.com/operationcode/Mike.mp3',
+      name: 'Mike Rodriguez',
+      story: (
+        <>
+          Mike Rodriguez is an former Marine who became a Windows XP administrator in the service,
+          and he&apos;s stayed with it. He wanted to be a tank crewman, but unfortunately his
+          partial color blindness limited the jobs he could perform inside a tank, so he had to take
+          a different career path, and now here he is! There are all kinds of ways people come to
+          computers. Mike was deployed to Iraq in 2005. Now he works for Teradata as an Application
+          Security Analyst.
+        </>
+      ),
+    },
+    {
+      source: 'http://traffic.libsyn.com/operationcode/Mike.mp3',
+      name: 'Msdhgsgz',
+      story: (
+        <>
+          Mike Rodriguez sdfgsdgis! There are all kinds of ways people come to computers. Mike was
+          deployed to Iraq in 2005. Now he works for Teradata as an Application Security Analyst.
+        </>
+      ),
+    },
+  ],
+};
+
+export default podcastdata;
--- pages/podcasts.js
+++ pages/podcasts.js
@@ -1,14 +1,13 @@
 // TODO: Add google/apple podcast buttons to page
 import Head from 'components/head';
-import Card from 'components/Cards/Card/Card';
+import ImageCard from 'components/Cards/ImageCard/ImageCard';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
 import Content from 'components/Content/Content';
-import Link from 'next/link';
-// import FAQItem from 'components/FAQ/FAQItem/FAQItem';
-// TODO: to be replaced by data for podcasts
-// import QuestionAnswerData from 'components/FAQ/questions';
+import FAQItem from 'components/FAQ/FAQItem/FAQItem';
+import PodcastData from 'components/podcasts/podcastsdata';
 import ReactAudioPlayer from 'react-audio-player';
-import styles from './styles/about.css';
+import { s3 } from 'common/constants/urls';
+import styles from './styles/podcast.css';
 
 export default () => {
   const pageTitle = 'Operation Code Podcast';
@@ -19,7 +18,8 @@ export default () => {
 
       <HeroBanner title={pageTitle}>
         {/* Don't forget to define the imageSource prop in the HeroBanner Component */}
-        {/* Call-to-action goes here */}
+        Interviews with military veterans, spouses, and others from the Operation Code community on
+        their transition to careers to technology.
       </HeroBanner>
 
       {/* <Content
@@ -30,7 +30,7 @@ export default () => {
         ))}
       /> */}
 
-      <Content
+      {/* <Content
         title="Episodes"
         theme="white"
         columns={[
@@ -47,14 +47,48 @@ export default () => {
               <p className={styles.justifyAlign}>
                 We at Operation Code strive to provide an efficient way into a tech career for
                 veterans and their families.
-                <Link href="/history" prefetch>
-                  <a>organization&apos;s history</a>
-                </Link>
-                .
               </p>
             </Card>
           </>,
         ]}
+      /> */}
+      <Content
+        title="Episodes"
+        theme="white"
+        columns={PodcastData.general.map(faq => (
+          <ImageCard
+            alt="Default podcast Image"
+            className={styles.content}
+            imageSource={`${s3}image_unavailable_placeholder.png`}
+          >
+            <ReactAudioPlayer
+              // title="Title"
+              // art="http://static.libsyn.com/p/assets/2/0/a/3/20a3052760331f2c/Logo-resized.png" // podcast image
+              src={faq.source} // podcast Link
+              // autoPlay
+              controls
+            />
+            <FAQItem question={faq.name} answer={faq.story} key={faq.name} />
+          </ImageCard>
+        ))}
+        // columns={[
+        //   <>
+        //     <Card>
+        //       <ReactAudioPlayer
+        //         title="Title"
+        //         art="http://static.libsyn.com/p/assets/2/0/a/3/20a3052760331f2c/Logo-resized.png" // podcast image
+        //         src="http://traffic.libsyn.com/operationcode/Mike.mp3" // podcast Link
+        //         // autoPlay
+        //         controls
+        //       />
+
+        //       <p className={styles.justifyAlign}>
+        //         We at Operation Code strive to provide an efficient way into a tech career for
+        //         veterans and their families.
+        //       </p>
+        //     </Card>
+        //   </>,
+        // ]}
       />
 
       {/* Rest of page content goes in here */}
