92143152b775126b6e57afc5922544ac932d721e
Allen Anthes <abanthes@gmail.com>
2019-05-26 11:47:35 -0500

    Elevate propTypes to their correct position in the grand order of things

--- components/ReusableSections/JoinSection/JoinSection.js
+++ components/ReusableSections/JoinSection/JoinSection.js
@@ -6,34 +6,37 @@ import OutboundLink from 'components/OutboundLink/OutboundLink';
 import LinkButton from 'components/LinkButton/LinkButton';
 import styles from './JoinSection.css';
 
-export const JoinSection = ({ isLoggedIn }) =>
-  !isLoggedIn && (
-    <Container theme="white">
-      <h3>Join Our Thriving Community</h3>
-
-      <p className={styles.justifyAlign}>
-        Are you ready to begin your journey towards a career in software development? Get the
-        support you need by joining our members-only Slack community!
-      </p>
-
-      <LinkButton href="/join" theme="secondary" className={styles.verticalSpacing}>
-        Register Now
-      </LinkButton>
-
-      <p>Slack is a community based collaboration tool where all the magic happens!</p>
-
-      <OutboundLink
-        href="https://get.slack.help/hc/en-us/categories/360000049043-Getting-Started"
-        analyticsEventLabel="Learn More Slack"
-      >
-        Never heard of Slack before?
-      </OutboundLink>
-    </Container>
-  );
-
 JoinSection.propTypes = {
   isLoggedIn: bool.isRequired,
 };
+
+export function JoinSection({ isLoggedIn }) {
+  return (
+    !isLoggedIn && (
+      <Container theme="white">
+        <h3>Join Our Thriving Community</h3>
+
+        <p className={styles.justifyAlign}>
+          Are you ready to begin your journey towards a career in software development? Get the
+          support you need by joining our members-only Slack community!
+        </p>
+
+        <LinkButton href="/join" theme="secondary" className={styles.verticalSpacing}>
+          Register Now
+        </LinkButton>
+
+        <p>Slack is a community based collaboration tool where all the magic happens!</p>
+
+        <OutboundLink
+          href="https://get.slack.help/hc/en-us/categories/360000049043-Getting-Started"
+          analyticsEventLabel="Learn More Slack"
+        >
+          Never heard of Slack before?
+        </OutboundLink>
+      </Container>
+    )
+  );
+}
 
 const mapStateToProps = state => ({
   isLoggedIn: state.isLoggedIn,
