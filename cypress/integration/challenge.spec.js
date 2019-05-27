87c3caa8953307a354a4c45cb3799da45b2532e9
Kyle Holmberg <kylemh.email@gmail.com>
2019-05-26 19:43:49 -0700

    Remove visual regression tests

--- cypress/integration/challenge.spec.js
+++
@@ -1,6 +0,0 @@
-describe('challenge visual regression test', () => {
-  it('should match previous screenshot "challenge page"', () => {
-    cy.visit('/challenge');
-    cy.matchImageSnapshot();
-  });
-});
--- cypress/integration/index.spec.js
+++
@@ -1,6 +0,0 @@
-describe('index visual regression test', () => {
-  it('should match previous screenshot "index page"', () => {
-    cy.visit('/index');
-    cy.matchImageSnapshot();
-  });
-});
