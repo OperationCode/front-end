6f417f2a5336c3f504722fae9e27747823995872
Kyle Holmberg <kylemh.email@gmail.com>
2019-06-09 10:44:16 -0700

    Use correct API URL

--- common/config/environment.js
+++ common/config/environment.js
@@ -1,17 +1,9 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-// Use staging environment locally, otherwise use environment variable to define API URL
-// TODO: Use GitHub Actions to enable environment-based Now deploys
-// export const apiUrl =
-//   isProduction && Boolean(process.env.API_URL)
-//     ? process.env.API_URL
-//     : 'https://api.staging.operationcode.org';
-
-// temporary hack
-export const apiUrl =
-  isProduction && (window && window.location.hostname === 'operationcode.org')
-    ? process.env.API_URL
-    : 'https://api.staging.operationcode.org';
+// TODO: Use GH Actions to enable environment-based Now deploys and stop using prod on PR deploys
+export const apiUrl = isProduction
+  ? 'https://api.operationcode.org'
+  : 'https://api.staging.operationcode.org';
 
 export const facebookKey = process.env.OC_FACEBOOK_KEY || '399113557601038';
 export const googleKey =
