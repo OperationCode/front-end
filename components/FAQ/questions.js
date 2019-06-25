e2c95a5a6f8b1e631b3c73081b1b16278b7bc232
soris-codes <soris.cox@gmail.com>
2019-06-25 21:54:30 +0100

    Installing yarn

--- components/FAQ/questions.js
+++ components/FAQ/questions.js
@@ -97,6 +97,16 @@ const questions = {
       ),
     },
     {
+      question:
+        'Are mentors available? If so, how do I request one?',
+      answer: (
+        <>
+          While we do not have a long-term mentorship program, we do have mentors available for 30-minute sessions to assist you with things like mock interviews, code reviews, or general guidance.
+          To request a mentorship session, type /mentor in any of our Slack channels and fill out the form.
+        </>
+      ),
+    },
+    {
       question: 'What are the hours of operation for Operation Code?',
       answer: (
         <>
