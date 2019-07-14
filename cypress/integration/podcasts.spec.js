30216307bccc24af0e11886df63a9843106fcb02
Kyle Holmberg <kylemh.email@gmail.com>
2019-07-13 17:18:18 -0700

    Update e2e tests

--- cypress/integration/podcasts.spec.js
+++ cypress/integration/podcasts.spec.js
@@ -1,32 +1,18 @@
 describe('podcasts', () => {
-  beforeEach(() => {
-    cy.server();
-    cy.visitAndWaitFor('/podcasts');
-    cy.get('h1').should('have.text', 'Podcasts');
+  describe('when server responds successfully', () => {
+    beforeEach(() => {
+      cy.server();
+      cy.visitAndWaitFor('/podcasts');
+      cy.get('h1').should('have.text', 'Podcasts');
+    });
+
+    it('renders many podcast cards', () => {
+      cy.get('[data-testid="Podcast Card"]').should('have.length.greaterThan', 0);
+    });
   });
 
-  it('renders many podcast cards', () => {
-    cy.get('[data-testid="Card"]').should('have.length.greaterThan', 0);
-  });
-
-  it('renders "Episodes" after filtering for Card', () => {
-    cy.get('[data-testid="podcast-card"]')
-      .type('podcast-card', { force: true })
-      .type('{enter}');
-
-    cy.get('[data-testid="podcast-card"]').should('exist');
-  });
-
-  it('checks if audio is playing after play button is clicked', () => {
-    cy.contains('episode.source')
-      .click()
-      .audio()
-      .should('be.playing', true);
-  });
-
-  it('checks if play button is clicked', () => {
-    cy.get('button')
-      .click('controls')
-      .should('be.clicked', true);
+  describe('when server does not respond', () => {
+    // unfortunately, Cypress doesn't have the ability to stub network requests, so it's not
+    // possible to test for this situation. Maybe some day!
   });
 });
