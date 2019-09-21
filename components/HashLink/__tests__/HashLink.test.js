0c5922cba1d1f7917923befeddee614bf2e3197b
Walley Yang <walleyyang@gmail.com>
2019-09-21 13:42:49 -0400

    Added more tests

--- components/HashLink/__tests__/HashLink.test.js
+++ components/HashLink/__tests__/HashLink.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 
 import HashLink from '../HashLink';
@@ -10,5 +11,45 @@ describe('HashLink', () => {
 
   it('should render with many props assigned', () => {
     createSnapshotTest(<HashLink className="test-class">Test</HashLink>);
+  });
+
+  it('should contain id without spaces', () => {
+    const requiredProps = {
+      id: 'Who We Serve',
+    };
+
+    const wrapper = shallow(<HashLink {...requiredProps} />);
+
+    expect(wrapper.find('#who-we-serve')).toExist(true);
+  });
+
+  it('should contain id without an exclamation mark', () => {
+    const requiredProps = {
+      id: 'JOIN TODAY!',
+    };
+
+    const wrapper = shallow(<HashLink {...requiredProps} />);
+
+    expect(wrapper.find('#join-today')).toExist(true);
+  });
+
+  it('should contain id without a question mark', () => {
+    const requiredProps = {
+      id: 'WANT TO BECOME A SPONSOR?',
+    };
+
+    const wrapper = shallow(<HashLink {...requiredProps} />);
+
+    expect(wrapper.find('#want-to-become-a-sponsor')).toExist(true);
+  });
+
+  it('should contain hashlink', () => {
+    const requiredProps = {
+      id: 'GENERAL QUESTIONS',
+    };
+
+    const wrapper = shallow(<HashLink {...requiredProps} />);
+
+    expect(wrapper.find({ href: '#general-questions' })).toExist(true);
   });
 });
