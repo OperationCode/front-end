07dcf27de60f1db1643da488557df9d6cb3d5ab6
Russ yev <ruslanyev@yahoo.com>
2019-10-05 16:12:39 -0700

    added GitHub logo

--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FacebookLogo from 'static/images/icons/facebook_logo.svg';
 import TwitterLogo from 'static/images/icons/twitter_logo.svg';
+import GithubLogo from 'static/images/icons/github_logo.svg';
 import InstagramLogo from 'static/images/icons/instagram_logo.svg';
 import SocialMediaContainer from './SocialMediaContainer/SocialMediaContainer';
 import SocialMediaItem from './SocialMediaItem/SocialMediaItem';
@@ -23,6 +24,11 @@ function SocialMedia() {
         href="https://www.instagram.com/operation_code/"
         name="Instagram"
         svg={<InstagramLogo className={styles.logo} />}
+      />
+      <SocialMediaItem
+        href="https://github.com/OperationCode/"
+        name="GitHub"
+        svg={<GithubLogo className={styles.logo} />}
       />
     </SocialMediaContainer>
   );
