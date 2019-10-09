fa540062725b616e6aa712c1483ce3c40950293b
Kyle Holmberg <kholmberg@acorns.com>
2019-10-08 23:36:41 -0700

    Simplify Heading tests

--- components/Heading/__tests__/Heading.test.js
+++ components/Heading/__tests__/Heading.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 import Heading from '../Heading';
 
 describe('Heading', () => {
-  afterEach(cleanup);
+  const requiredProps = {
+    text: 'Test',
+  };
 
   it('should render with required props', () => {
-    createSnapshotTest(<Heading text="Test" />);
+    createSnapshotTest(<Heading {...requiredProps} />);
   });
 
   it('should render with many props assigned', () => {
-    createSnapshotTest(
-      <Heading text="Test" className="test-class" id="test-heading-1" hasHeadingLines={false} />,
-    );
+    createSnapshotTest(<Heading {...requiredProps} headingLevel={5} hasHashLink />);
   });
 });
