73dccd9119b75633a131bd611f271af696813c09
Prem Kumar <prmsrswt@gmail.com>
2019-10-09 10:19:08 +0530

    use easier to understand test names for Pagination

--- components/Pagination/__tests__/Pagination.test.js
+++ components/Pagination/__tests__/Pagination.test.js
@@ -8,39 +8,29 @@ describe('Pagination', () => {
   afterEach(cleanup);
 
   it('should render with required props', () => {
-    createSnapshotTest(<Pagination currentPage={3} totalPages={5} maxElements={11} />);
+    createSnapshotTest(<Pagination currentPage={3} totalPages={5} />);
   });
 
-  it('should not have seperatorStart if currentPage - 1 is less than (maxElements + 1) / 2', () => {
-    const { queryByTestId } = render(
-      <Pagination currentPage={6} totalPages={11} maxElements={11} />,
-    );
+  it('should not have a seperator at start if not needed', () => {
+    const { queryByTestId } = render(<Pagination currentPage={6} totalPages={11} />);
 
     expect(queryByTestId('Pagination Item - seperatorStart')).not.toBeInTheDocument();
   });
 
-  // eslint-disable-next-line max-len
-  it('should not have seperatorEnd if totalPages - currentPage is less than (maxElements + 1) / 2', () => {
-    const { queryByTestId } = render(
-      <Pagination currentPage={6} totalPages={11} maxElements={11} />,
-    );
+  it('should not have a seperator at end if not needed', () => {
+    const { queryByTestId } = render(<Pagination currentPage={6} totalPages={11} />);
 
     expect(queryByTestId('Pagination Item - seperatorEnd')).not.toBeInTheDocument();
   });
 
-  it('should have seperatorStart if currentPage - 1 is greater than (maxElements - 1) / 2', () => {
-    const { queryByTestId } = render(
-      <Pagination currentPage={7} totalPages={11} maxElements={11} />,
-    );
+  it('should have a seperator at start if needed', () => {
+    const { queryByTestId } = render(<Pagination currentPage={7} totalPages={11} />);
 
     expect(queryByTestId('Pagination Item - seperatorStart')).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line max-len
-  it('should have seperatorEnd if totalPages - currentPage is greater than (maxElements - 1) / 2', () => {
-    const { queryByTestId } = render(
-      <Pagination currentPage={5} totalPages={11} maxElements={11} />,
-    );
+  it('should have a seperator at end if needed', () => {
+    const { queryByTestId } = render(<Pagination currentPage={5} totalPages={11} />);
 
     expect(queryByTestId('Pagination Item - seperatorEnd')).toBeInTheDocument();
   });
