c45d0ddc15af5619976113e5424002b6bd614f73
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-10-09 01:18:22 +0100

    Migrate Button, CloseButton, Select, PartnerLogoLink and Article Group to use hooks.

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { any, bool, func, node, number, oneOf, oneOfType, string } from 'prop-types';
 import classNames from 'classnames';
 import ReactGA from 'react-ga';
@@ -6,71 +6,76 @@ import { googleAnalyticsEventPropType } from 'common/constants/custom-props';
 import { getDataAttributes, getAriaAttributes } from 'common/utils/prop-utils';
 import styles from './Button.css';
 
-class Button extends Component {
-  static propTypes = {
-    analyticsObject: googleAnalyticsEventPropType,
-    children: oneOfType([node, string]).isRequired,
-    className: string,
-    disabled: bool,
-    fullWidth: bool,
-    onClick: func,
-    tabIndex: oneOfType([string, number]),
-    theme: oneOf(['primary', 'secondary']),
-    type: oneOf(['button', 'reset', 'submit']),
-    datum: any, // eslint-disable-line react/forbid-prop-types
-  };
+Button.propTypes = {
+  analyticsObject: googleAnalyticsEventPropType,
+  children: oneOfType([node, string]).isRequired,
+  className: string,
+  disabled: bool,
+  fullWidth: bool,
+  onClick: func,
+  tabIndex: oneOfType([string, number]),
+  theme: oneOf(['primary', 'secondary']),
+  type: oneOf(['button', 'reset', 'submit']),
+  datum: any, // eslint-disable-line react/forbid-prop-types
+};
 
-  static defaultProps = {
-    analyticsObject: {
-      action: 'Button Selected',
-      category: 'Interactions',
-    },
-    className: undefined,
-    datum: '',
-    disabled: false,
-    fullWidth: false,
-    onClick: () => {},
-    tabIndex: 0,
-    theme: 'primary',
-    type: 'button',
-  };
+Button.defaultProps = {
+  analyticsObject: {
+    action: 'Button Selected',
+    category: 'Interactions',
+  },
+  className: undefined,
+  datum: '',
+  disabled: false,
+  fullWidth: false,
+  onClick: () => {},
+  tabIndex: 0,
+  theme: 'primary',
+  type: 'button',
+};
 
-  clickHandler = () => {
-    const { props } = this;
+export default function Button(props) {
+  const customDataAttributes = getDataAttributes(props);
+  const ariaAttributes = getAriaAttributes(props);
 
+  const {
+    analyticsObject,
+    children,
+    className,
+    disabled,
+    fullWidth,
+    onClick,
+    tabIndex,
+    theme,
+    type,
+  } = props;
+
+  const clickHandler = () => {
     if (process.env.NODE_ENV === 'production') {
-      ReactGA.event(props.analyticsObject);
+      ReactGA.event(analyticsObject);
     } else {
       // eslint-disable-next-line no-console
-      console.log('Analytics Disabled', props.analyticsObject);
+      console.log('Analytics Disabled', analyticsObject);
     }
-    props.onClick();
+    onClick();
   };
 
-  render() {
-    const { props } = this;
-    const customDataAttributes = getDataAttributes(props);
-    const ariaAttributes = getAriaAttributes(props);
-
-    /* eslint-disable react/button-has-type */
-    return (
-      <button
-        className={classNames(styles.Button, props.className, styles[props.theme], {
-          [styles.disabled]: props.disabled,
-          [styles.fullWidth]: props.fullWidth,
-        })}
-        disabled={props.disabled}
-        onClick={this.clickHandler}
-        tabIndex={props.tabIndex}
-        type={props.type}
-        {...customDataAttributes}
-        {...ariaAttributes}
-      >
-        {props.children}
-      </button>
-    );
-    /* eslint-enable react/button-has-type */
-  }
+  /* eslint-disable react/button-has-type */
+  return (
+    <button
+      className={classNames(styles.Button, className, styles[theme], {
+        [styles.disabled]: disabled,
+        [styles.fullWidth]: fullWidth,
+      })}
+      disabled={disabled}
+      onClick={clickHandler}
+      tabIndex={tabIndex}
+      type={type}
+      {...customDataAttributes}
+      {...ariaAttributes}
+    >
+      {children}
+    </button>
+  );
+  /* eslint-enable react/button-has-type */
 }
-
-export default Button;
