da7097a45617967a5334fda089747206679d6f44
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-10-10 14:37:46 +0530

    refactor to hooks, minor test changes

--- components/Nav/NavListItem/NavListItem.js
+++ components/Nav/NavListItem/NavListItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import Link from 'next/link';
 import { arrayOf, bool, shape, string } from 'prop-types';
@@ -6,44 +6,10 @@ import PlusIcon from 'static/images/icons/plus.svg';
 import MinusIcon from 'static/images/icons/minus.svg';
 import styles from './NavListItem.css';
 
-export default class NavListItem extends Component {
-  static propTypes = {
-    href: string.isRequired,
-    name: string.isRequired,
-    shouldPrefetch: bool,
-    sublinks: arrayOf(
-      shape({
-        name: string.isRequired,
-        href: string.isRequired,
-        shouldPrefetch: bool,
-      }),
-    ),
-  };
+const NavListItem = ({ sublinks, href, shouldPrefetch, name }) => {
+  const [areSublinksVisible, changeSublinkVisibility] = useState(false);
 
-  static defaultProps = {
-    shouldPrefetch: false,
-    sublinks: [],
-  };
-
-  state = {
-    areSublinksVisible: false,
-  };
-
-  hideSublinks = () => {
-    this.setState({ areSublinksVisible: false });
-  };
-
-  showSublinks = () => {
-    this.setState({ areSublinksVisible: true });
-  };
-
-  toggleSublinkVisibility = () => {
-    this.setState(previousState => ({ areSublinksVisible: !previousState.areSublinksVisible }));
-  };
-
-  handleKeyDown = (event, indexKeyedOn) => {
-    const { sublinks } = this.props;
-
+  const handleKeyDown = (event, indexKeyedOn) => {
     const lastSublinkIndex = sublinks.length - 1;
     const isLastSublink = indexKeyedOn === lastSublinkIndex;
     const isFirstSublink = indexKeyedOn === 0;
@@ -56,76 +22,92 @@ export default class NavListItem extends Component {
       (isLastSublink && didTabForward) || (isFirstSublink && didTabBackward);
 
     if (shouldHideSublinks) {
-      this.hideSublinks();
+      changeSublinkVisibility(false);
     }
   };
 
-  render() {
-    const { props, state } = this;
+  const hasSublinks = sublinks.length > 0;
 
-    const hasSublinks = props.sublinks.length > 0;
+  return (
+    <li className={styles.NavListItem}>
+      <Link href={href} prefetch={shouldPrefetch}>
+        <a
+          className={classNames(styles.link, styles.navItemLink)}
+          onMouseEnter={() => changeSublinkVisibility(true)}
+          onMouseLeave={() => changeSublinkVisibility(false)}
+          role="link"
+          tabIndex={0}
+          data-testid={`Nav Item ${name}`}
+        >
+          <span className={styles.linkContent}>{name}</span>
+        </a>
+      </Link>
 
-    return (
-      <li className={styles.NavListItem}>
-        <Link href={props.href} prefetch={props.shouldPrefetch}>
-          <a
-            className={classNames(styles.link, styles.navItemLink)}
-            onMouseEnter={this.showSublinks}
-            onMouseLeave={this.hideSublinks}
-            role="link"
-            tabIndex={0}
-            data-testid={`Nav Item ${props.name}`}
+      {hasSublinks && (
+        <>
+          <button
+            aria-expanded={areSublinksVisible}
+            aria-haspopup={hasSublinks}
+            aria-label="submenu"
+            className={styles.sublinkToggleButton}
+            onClick={() => changeSublinkVisibility(!areSublinksVisible)}
+            onMouseEnter={() => changeSublinkVisibility(true)}
+            onMouseLeave={() => changeSublinkVisibility(false)}
+            type="button"
           >
-            <span className={styles.linkContent}>{props.name}</span>
-          </a>
-        </Link>
+            {areSublinksVisible ? (
+              <MinusIcon className={styles.icon} data-testid="minus-icon" />
+            ) : (
+              <PlusIcon className={styles.icon} data-testid="plus-icon" />
+            )}
+          </button>
 
-        {hasSublinks && (
-          <>
-            <button
-              aria-expanded={state.areSublinksVisible}
-              aria-haspopup={hasSublinks}
-              aria-label="submenu"
-              className={styles.sublinkToggleButton}
-              onClick={this.toggleSublinkVisibility}
-              onMouseEnter={this.showSublinks}
-              onMouseLeave={this.hideSublinks}
-              type="button"
-            >
-              {state.areSublinksVisible ? (
-                <MinusIcon className={styles.icon} data-testid="minus-icon" />
-              ) : (
-                <PlusIcon className={styles.icon} data-testid="plus-icon" />
-              )}
-            </button>
+          <ul
+            className={classNames(styles.sublinksList, {
+              [styles.invisible]: !areSublinksVisible,
+            })}
+            onMouseEnter={() => changeSublinkVisibility(true)}
+            onMouseLeave={() => changeSublinkVisibility(false)}
+          >
+            {sublinks.map((sublink, index) => (
+              <li className={styles.sublinkListItem} key={sublink.name}>
+                <Link href={sublink.href} prefetch={sublink.shouldPrefetch || false}>
+                  <a
+                    className={styles.link}
+                    key={sublink.name}
+                    role="link"
+                    tabIndex={0}
+                    data-testid={`Nav Item ${sublink.name}`}
+                    onKeyDown={event => handleKeyDown(event, index)}
+                  >
+                    <span className={styles.linkContent}>{sublink.name}</span>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </li>
+  );
+};
 
-            <ul
-              className={classNames(styles.sublinksList, {
-                [styles.invisible]: !state.areSublinksVisible,
-              })}
-              onMouseEnter={this.showSublinks}
-              onMouseLeave={this.hideSublinks}
-            >
-              {props.sublinks.map((sublink, index) => (
-                <li className={styles.sublinkListItem} key={sublink.name}>
-                  <Link href={sublink.href} prefetch={sublink.shouldPrefetch || false}>
-                    <a
-                      className={styles.link}
-                      key={sublink.name}
-                      role="link"
-                      tabIndex={0}
-                      data-testid={`Nav Item ${sublink.name}`}
-                      onKeyDown={event => this.handleKeyDown(event, index)}
-                    >
-                      <span className={styles.linkContent}>{sublink.name}</span>
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </li>
-    );
-  }
-}
+NavListItem.propTypes = {
+  href: string.isRequired,
+  name: string.isRequired,
+  shouldPrefetch: bool,
+  sublinks: arrayOf(
+    shape({
+      name: string.isRequired,
+      href: string.isRequired,
+      shouldPrefetch: bool,
+    }),
+  ),
+};
+
+NavListItem.defaultProps = {
+  shouldPrefetch: false,
+  sublinks: [],
+};
+
+export default NavListItem;
