b3f2dba172dbe5bd0ec43a8d689887cff7e0c1ad
Chris Galvan <chrisgalvanr@gmail.com>
2019-10-10 09:24:20 -0500

    fix: moving prop definitions above component

--- components/ProgressIndicator/ProgressIndicator.js
+++ components/ProgressIndicator/ProgressIndicator.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { number } from 'prop-types';
 import styles from './ProgressIndicator.css';
 
+ProgressIndicator.propTypes = {
+  stepNumber: number,
+  totalSteps: number,
+};
+
+ProgressIndicator.defaultProps = {
+  stepNumber: 0,
+  totalSteps: 0,
+};
+
 const ProgressIndicator = ({ stepNumber, totalSteps }) => {
   if (totalSteps < 1) return null;
 
@@ -17,16 +27,6 @@ const ProgressIndicator = ({ stepNumber, totalSteps }) => {
       </div>
     </div>
   );
-};
-
-ProgressIndicator.propTypes = {
-  stepNumber: number,
-  totalSteps: number,
-};
-
-ProgressIndicator.defaultProps = {
-  stepNumber: 0,
-  totalSteps: 0,
 };
 
 export default ProgressIndicator;
