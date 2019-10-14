0bef2d8e335ff842c8da24f8b3a8de31ac279edc
Kyle Holmberg <kholmberg@acorns.com>
2019-10-13 19:30:30 -0700

    Remove snapshot test and add button handling test

--- components/Cards/ResourceCard/__tests__/ResourceCard.test.js
+++ components/Cards/ResourceCard/__tests__/ResourceCard.test.js
@@ -1,16 +1,42 @@
 /* eslint-env jest */
 import React from 'react';
-import createSnapshotTest from 'test-utils/createSnapshotTest';
+import { fireEvent, render } from '@testing-library/react';
 
 import ResourceCard from '../ResourceCard';
+// import ResourceCard, { possibleUserVotes } from '../ResourceCard';
 
 describe('ResourceCard', () => {
-  it('should render with required props', () => {
-    createSnapshotTest(
-      <ResourceCard
-        imageSource="/static/images/icons/javascript_logo.svg"
-        name="JavaScript for Dummies"
-      />,
+  const requiredProps = {
+    href: 'https://all-the-javascripts.com/',
+    imageSource: '/static/images/icons/javascript_logo.svg',
+    name: 'JavaScript for Dummies',
+  };
+
+  // Swallowing warnings from ReactGA
+  const realConsoleWarn = console.warn;
+  beforeAll(() => {
+    console.warn = () => {};
+  });
+  afterAll(() => {
+    console.warn = realConsoleWarn;
+  });
+
+  it('fires appropriate method when upvote button clicked', () => {
+    const onUpvoteMock = jest.fn();
+    const onDownvoteMock = jest.fn();
+
+    const component = render(
+      <ResourceCard {...requiredProps} onDownvote={onDownvoteMock} onUpvote={onUpvoteMock} />,
     );
+
+    const UpvoteButton = component.queryByTestId('Upvote Button');
+    expect(onUpvoteMock).not.toHaveBeenCalled();
+    fireEvent.click(UpvoteButton);
+    expect(onUpvoteMock).toHaveBeenCalledTimes(1);
+
+    const DownvoteButton = component.queryByTestId('Downvote Button');
+    expect(onDownvoteMock).not.toHaveBeenCalled();
+    fireEvent.click(DownvoteButton);
+    expect(onDownvoteMock).toHaveBeenCalledTimes(1);
   });
 });
