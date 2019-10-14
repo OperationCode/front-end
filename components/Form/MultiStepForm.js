d84d37cf196e3d40a6a29812a0b4855089c17905
Chongyoon Nah <chongyoonaaronnah@gmail.com>
2019-10-14 14:24:51 -0400

    Further code cleanup: destructuring, state setter change, etc

--- components/Form/MultiStepForm.js
+++ components/Form/MultiStepForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable unicorn/prevent-abbreviations */
+import React, { useState } from 'react';
 import { arrayOf, func, object } from 'prop-types';
 import noop from 'lodash/noop';
 import { Formik } from 'formik';
@@ -9,74 +10,63 @@ import Alert from 'components/Alert/Alert';
 import ProgressIndicator from 'components/ProgressIndicator/ProgressIndicator';
 import styles from './MultiStepForm.css';
 
-export class MultiStepForm extends React.Component {
-  static propTypes = {
-    // initialValues must be object where entire form's shape is described
-    initialValues: object.isRequired,
+MultiStepForm.propTypes = {
+  // initialValues must be object where entire form's shape is described
+  initialValues: object.isRequired,
 
-    getErrorMessage: func.isRequired,
-    onEachStepSubmit: func,
-    onFinalSubmit: func.isRequired, // to be considered onSuccess
-    steps: arrayOf(validStep).isRequired,
-  };
+  getErrorMessage: func.isRequired,
+  onEachStepSubmit: func,
+  onFinalSubmit: func.isRequired, // to be considered onSuccess
+  steps: arrayOf(validStep).isRequired,
+};
 
-  static defaultProps = {
-    onEachStepSubmit: noop,
-  };
+MultiStepForm.defaultProps = {
+  onEachStepSubmit: noop,
+};
 
-  state = {
+export function MultiStepForm({
+  steps,
+  initialValues,
+  onEachStepSubmit,
+  onFinalSubmit,
+  getErrorMessage,
+}) {
+  const [state, setState] = useState({
     errorMessage: '',
     stepNumber: 0,
-  };
+  });
 
-  isLastStep = () => {
-    const { steps } = this.props;
-    const { stepNumber } = this.state;
+  const { errorMessage, stepNumber } = state;
 
+  const isLastStep = () => {
     return stepNumber === steps.length - 1;
   };
 
   // We assume this method cannot be called on the last step
-  showNextStep = ({ setFieldTouched }) => {
-    const { steps } = this.props;
-    const { stepNumber } = this.state;
-
+  const showNextStep = ({ setFieldTouched }) => {
     // TODO: Only untouch if value is '' or []
     const nextStepFieldNames = Object.keys(steps[stepNumber + 1].initialValues);
     nextStepFieldNames.forEach(fieldName => setFieldTouched(fieldName, false));
-
-    this.setState(previousState => ({
-      stepNumber: previousState.stepNumber + 1,
-    }));
+    setState(prevState => ({ ...prevState, stepNumber: prevState.stepNumber + 1 }));
   };
 
   // We assume this method cannot be called on the first step
-  showPreviousStep = ({ setFieldTouched }) => {
-    const { steps } = this.props;
-    const { stepNumber } = this.state;
-
+  const showPreviousStep = ({ setFieldTouched }) => {
     // TODO: Only untouch if value is '' or []
     const previousStepFieldNames = Object.keys(steps[stepNumber - 1].initialValues);
     previousStepFieldNames.forEach(fieldName => setFieldTouched(fieldName, false));
 
-    this.setState(previousState => ({
-      stepNumber: previousState.stepNumber - 1,
-    }));
+    setState(prevState => ({ ...prevState, stepNumber: prevState.stepNumber - 1 }));
   };
 
-  handleError = error => {
-    const { getErrorMessage } = this.props;
-
-    this.setState({ errorMessage: getErrorMessage(error) });
+  const handleError = error => {
+    setState(prevState => ({ ...prevState, errorMessage: getErrorMessage(error) }));
   };
 
-  handleSubmit = async (values, formikBag) => {
-    const { steps, onEachStepSubmit, onFinalSubmit } = this.props;
-    const { errorMessage, stepNumber } = this.state;
-
+  const handleSubmit = async (values, formikBag) => {
     if (errorMessage) {
       // reset error message each submit
-      this.setState({ errorMessage: '' });
+      setState(prevState => ({ ...prevState, errorMessage: '' }));
     }
 
     try {
@@ -85,82 +75,77 @@ export class MultiStepForm extends React.Component {
       const currentStepSubmitHandler = steps[stepNumber].submitHandler;
       await currentStepSubmitHandler(values);
 
-      if (this.isLastStep()) {
+      if (isLastStep()) {
         await onFinalSubmit(values);
         formikBag.setSubmitting(false);
         formikBag.resetForm();
       } else {
         formikBag.setSubmitting(false);
-        this.showNextStep(formikBag);
+        showNextStep(formikBag);
       }
     } catch (error) {
       formikBag.setSubmitting(false);
-      this.handleError(error);
+      handleError(error);
     }
   };
 
-  render() {
-    const { initialValues, steps } = this.props;
-    const { errorMessage, stepNumber } = this.state;
+  const CurrentStep = steps[stepNumber];
+  const isFirstStep = stepNumber === 0;
 
-    const CurrentStep = steps[stepNumber];
-    const isFirstStep = stepNumber === 0;
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={CurrentStep.validationSchema}
+      onSubmit={handleSubmit}
+      render={formikBag => (
+        <Form className={styles.MultiStepForm} onSubmit={formikBag.handleSubmit}>
+          <h3 className={styles.centerAligned}>{CurrentStep.title}</h3>
 
-    return (
-      <Formik
-        initialValues={initialValues}
-        validationSchema={CurrentStep.validationSchema}
-        onSubmit={this.handleSubmit}
-        render={formikBag => (
-          <Form className={styles.MultiStepForm} onSubmit={formikBag.handleSubmit}>
-            <h3 className={styles.centerAligned}>{CurrentStep.title}</h3>
+          <ProgressIndicator stepNumber={stepNumber} totalSteps={steps.length} />
 
-            <ProgressIndicator stepNumber={stepNumber} totalSteps={steps.length} />
+          <CurrentStep {...formikBag} />
 
-            <CurrentStep {...formikBag} />
+          <div className={styles.errorMessage}>
+            {errorMessage && <Alert type="error">{errorMessage}</Alert>}
+          </div>
 
-            <div className={styles.errorMessage}>
-              {errorMessage && <Alert type="error">{errorMessage}</Alert>}
-            </div>
+          <div className={styles.buttonGrouping}>
+            {!isFirstStep && (
+              <Button
+                theme="secondary"
+                disabled={formikBag.isSubmitting}
+                onClick={() => showPreviousStep(formikBag)}
+                data-testid="Previous Step Button"
+              >
+                ← Previous
+              </Button>
+            )}
 
-            <div className={styles.buttonGrouping}>
-              {!isFirstStep && (
-                <Button
-                  theme="secondary"
-                  disabled={formikBag.isSubmitting}
-                  onClick={() => this.showPreviousStep(formikBag)}
-                  data-testid="Previous Step Button"
-                >
-                  ← Previous
-                </Button>
-              )}
-
-              {this.isLastStep() ? (
-                <Button
-                  type="submit"
-                  theme="secondary"
-                  disabled={formikBag.isSubmitting}
-                  data-testid="Submit Multi-Step Form"
-                >
-                  Submit ✓
-                </Button>
-              ) : (
-                <Button
-                  type="submit"
-                  theme="secondary"
-                  disabled={formikBag.isSubmitting}
-                  fullWidth={isFirstStep}
-                  data-testid="Submit Step Button"
-                >
-                  Next →
-                </Button>
-              )}
-            </div>
-          </Form>
-        )}
-      />
-    );
-  }
+            {isLastStep() ? (
+              <Button
+                type="submit"
+                theme="secondary"
+                disabled={formikBag.isSubmitting}
+                data-testid="Submit Multi-Step Form"
+              >
+                Submit ✓
+              </Button>
+            ) : (
+              <Button
+                type="submit"
+                theme="secondary"
+                disabled={formikBag.isSubmitting}
+                fullWidth={isFirstStep}
+                data-testid="Submit Step Button"
+              >
+                Next →
+              </Button>
+            )}
+          </div>
+        </Form>
+      )}
+    />
+  );
 }
 
 export default MultiStepForm;
