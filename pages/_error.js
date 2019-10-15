81efe18db19fb10b40216ff415901d965c3aa24f
Levi Butcher <levibutcher355@gmail.com>
2019-10-15 00:59:06 -0400

    Refactor _error to hooks

--- pages/_error.js
+++ pages/_error.js
@@ -1,20 +1,18 @@
 import ErrorDisplay from 'components/ErrorDisplay/ErrorDisplay';
+import { number } from 'prop-types';
+
+Error.getInitialProps = ({ response, error }) => {
+  const currentError = error ? error.statusCode : null;
+  const statusCode = response ? response.statusCode : currentError;
+  return { statusCode };
+};
+
+Error.propTypes = { statusCode: number };
+Error.defaultProps = { statusCode: undefined };
 
 // This acts as an override necessary to use a custom ErrorDisplay handler
-class Error extends React.Component {
-  // eslint-disable-next-line unicorn/prevent-abbreviations
-  static getInitialProps({ res, error }) {
-    const currentError = error ? error.statusCode : null;
-    const statusCode = res ? res.statusCode : currentError;
-    return { statusCode };
-  }
-
-  render() {
-    const { props } = this;
-
-    // eslint-disable-next-line react/prop-types
-    return <ErrorDisplay statusCode={props.statusCode} />;
-  }
+function Error({ statusCode }) {
+  return <ErrorDisplay statusCode={statusCode} />;
 }
 
 export default Error;
