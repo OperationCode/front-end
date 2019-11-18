93101c64a8b3904d40e3447915c4232c0d85cf56
Chongyoon Nah <chongyoonaaronnah@gmail.com>
2019-11-18 15:52:37 -0500

    modified "removeRootDirectory" to turn regexp pattern into glob pattern

--- nyc.config.js
+++ nyc.config.js
@@ -1,15 +1,10 @@
 const config = require('./jest.config');
 
 const removeRootDirectory = pathArray => {
-  return pathArray.map(path => path.slice(10));
+  return pathArray.map(path => path.replace('(.*)', '**').slice(10));
 };
 
 module.exports = {
   include: [...removeRootDirectory(config.collectCoverageFrom), 'pages/**/*.js'],
-  exclude: [
-    ...removeRootDirectory(config.coveragePathIgnorePatterns),
-    'pages/api/__coverage__.js',
-    'common/**/index.js',
-    'components/**/index.js',
-  ],
+  exclude: [...removeRootDirectory(config.coveragePathIgnorePatterns), 'pages/api/__coverage__.js'],
 };
