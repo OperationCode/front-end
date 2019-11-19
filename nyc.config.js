5277b42d7263d140ce298782a71cd1a494d94fdc
Chongyoon Nah <chongyoonaaronnah@gmail.com>
2019-11-19 17:40:52 -0500

    small change to what is included in Cypress test coverage

--- nyc.config.js
+++ nyc.config.js
@@ -1,4 +1,10 @@
+const config = require('./jest.config');
+
+const removeRootDirectoryFromPaths = pathArray => {
+  return pathArray.map(path => path.replace('<rootDir>/', ''));
+};
+
 module.exports = {
-  include: ['pages/**/*.js', 'decorators/withAuthSync/withAuthSync.js'],
+  include: ['pages/**/*.js', ...removeRootDirectoryFromPaths(config.collectCoverageFrom)],
   exclude: ['pages/api/__coverage__.js'],
 };
