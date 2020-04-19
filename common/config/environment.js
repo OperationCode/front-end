54bc11db279561d0b59d7deaff38c6a947707e84
Kyle Holmberg <kholmberg@acorns.com>
2020-04-19 03:41:43 -0700

    Add other Sentry variables

--- common/config/environment.js
+++ common/config/environment.js
@@ -3,6 +3,12 @@
  */
 const isProduction = process.env.NODE_ENV === 'production';
 
+const sentryEnvironmentVariables = {
+  SENTRY_DSN: 'https://90edfb8d1d9640cf86d8aefd57218d71@sentry.io/1443656',
+  SENTRY_ORG: 'operation-code',
+  SENTRY_PROJECT: 'front-end',
+};
+
 // These are all exposed by the client, so there's no way to protect them anyways.
 export const clientTokens = isProduction
   ? {
@@ -10,14 +16,15 @@ export const clientTokens = isProduction
       LOGROCKET: 'uquzri',
       OC_FACEBOOK_KEY: '207055783236912',
       OC_GOOGLE_KEY: '448638408285-6ego1u9ic6qcbsiitr2m173pp1tbs27k.apps.googleusercontent.com',
-      SENTRY_DSN: 'https://90edfb8d1d9640cf86d8aefd57218d71@sentry.io/1443656',
+      ...sentryEnvironmentVariables,
     }
   : {
       GOOGLE_ANALYTICS: '',
       LOGROCKET: '',
       OC_FACEBOOK_KEY: '399113557601038',
       OC_GOOGLE_KEY: '973655216990-vvl9vfp9v13lfoq7ccm36e8ouukrgdfh.apps.googleusercontent.com',
-      SENTRY_DSN: '',
+      ...sentryEnvironmentVariables,
+      SENTRY_DSN: '', // empty string this one in dev
     };
 
 // TODO: Use GH Actions to enable environment-based Now deploys and stop using prod on PR deploys
