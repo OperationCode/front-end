56fe46f8a9b717852ba3cd510912ab5d8e36eb67
Kyle Holmberg <kholmberg@acorns.com>
2020-04-23 23:12:58 -0700

    Avoid 404s in local development

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -1,7 +1,13 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'components/head';
+import HeroBanner from 'components/HeroBanner/HeroBanner';
 
-// Why we have this: https://github.com/zeit/next.js/issues/10488
+/**
+ * @description This is only to avoid 404s locally on `/resources`. `now.json` redirects on prod
+ * @see https://github.com/zeit/next.js/issues/10488
+ * @returns {null} will client-side redirect on render for devs
+ */
 function ResourcesPageIndex() {
   const router = useRouter();
 
@@ -9,7 +15,12 @@ function ResourcesPageIndex() {
     router.push('/resources/1');
   }, []);
 
-  return null;
+  return (
+    <>
+      <Head title="Resources" />
+      <HeroBanner title="Resources" />
+    </>
+  );
 }
 
 export default ResourcesPageIndex;
