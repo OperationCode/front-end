1c9d471f75436465208b48c11d577d0251ab4a35
Kyle Holmberg <kholmberg@acorns.com>
2020-04-24 01:22:54 -0700

    Convert resources page to client-side data fetching

--- pages/resources/[page].js
+++ pages/resources/[page].js
@@ -1,44 +1,55 @@
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import get from 'lodash/get';
+import isNumber from 'lodash/isNumber';
 import Content from 'components/Content/Content';
 import Head from 'components/head';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
-import ResourceCard from 'components/Cards/ResourceCard/ResourceCard';
 import Pagination from 'components/Pagination/Pagination';
+import ResourceCard from 'components/Cards/ResourceCard/ResourceCard';
 import { getResourcesPromise } from 'common/constants/api';
 import styles from '../styles/resources.module.css';
 
-ResourcesPage.propTypes = {
-  currentPage: PropTypes.number.isRequired,
-  pathname: PropTypes.string.isRequired,
-  resources: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number, // integer, unique ID
-      name: PropTypes.title,
-      notes: PropTypes.string, // possibly null
-      url: PropTypes.string.isRequired,
-      upvotes: PropTypes.number,
-      downvotes: PropTypes.number,
-    }),
-  ).isRequired,
-  totalPages: PropTypes.number.isRequired,
-};
+function ResourcesPage() {
+  const router = useRouter();
+  const { pathname, query } = router;
+  const currentPage = parseInt(query.page, 10);
 
-ResourcesPage.getInitialProps = async ({ pathname, query }) => {
-  const { page = 1 } = query;
+  if (!isNumber(currentPage)) {
+    // TODO: Handle situation where user tried some funny business, like `/resources/fuck-you`
+    throw new Error(`'currentPage' is not a number`);
+  }
 
-  const response = await getResourcesPromise({ page });
+  const [isLoading, setIsLoading] = useState(true);
+  const [resources, setResources] = useState([]);
+  const [totalPages, setTotalPages] = useState(currentPage);
 
-  const { data: resources, number_of_pages: totalPages, page: currentPage } = response.data;
+  useEffect(() => {
+    getResourcesPromise({ page: query.page })
+      .then(response => {
+        const fetchedResources = get(response, 'data.data', []);
+        const fetchedNumberOfPages = get(response, 'data.number_of_pages', 0);
 
-  return {
-    currentPage,
-    pathname,
-    resources,
-    totalPages,
-  };
-};
+        if (fetchedResources.length === 0 || fetchedNumberOfPages === 0) {
+          // TODO: set state for variable which conditionally renders "No resources" view
+          return;
+        }
 
-function ResourcesPage({ currentPage, pathname, resources, totalPages }) {
+        setResources(fetchedResources);
+        setTotalPages(fetchedNumberOfPages);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        // TODO: Do something with erroror state
+        console.log('error', error);
+        setIsLoading(false);
+      });
+
+    return () => {
+      setIsLoading(true);
+    };
+  }, [query]);
+
   return (
     <>
       <Head title="Resources" />
@@ -47,21 +58,35 @@ function ResourcesPage({ currentPage, pathname, resources, totalPages }) {
         theme="white"
         columns={[
           <section className={styles.fullWidth}>
-            <div className={styles.fullWidth}>
-              {resources.map(resource => (
-                <ResourceCard
-                  key={resource.id}
-                  description={resource.notes}
-                  downvotes={resource.downvotes}
-                  upvotes={resource.upvotes}
-                  href={resource.url || ''}
-                  name={resource.name}
-                  className={styles.resourceCard}
-                />
-              ))}
-            </div>
+            {isLoading ? (
+              <p style={{ margin: '2rem auto', textAlign: 'center' }}>
+                {/* TODO: Create with skeleton loading screen to avoid jank between routing */}
+                Loading...
+              </p>
+            ) : (
+              <>
+                <div className={styles.fullWidth}>
+                  {resources.map(resource => (
+                    <ResourceCard
+                      key={resource.id}
+                      description={resource.notes}
+                      downvotes={resource.downvotes}
+                      upvotes={resource.upvotes}
+                      href={resource.url || ''}
+                      name={resource.name}
+                      className={styles.resourceCard}
+                    />
+                  ))}
+                </div>
 
-            <Pagination currentPage={currentPage} totalPages={totalPages} pathname={pathname} />
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  pathname={pathname}
+                  query={query}
+                />
+              </>
+            )}
           </section>,
         ]}
       />
