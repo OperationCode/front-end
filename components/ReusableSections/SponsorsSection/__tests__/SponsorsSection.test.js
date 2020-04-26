7cbbd84ea49de5ba621b86b8ddda0ae7281abbcf
Kyle Holmberg <kholmberg@acorns.com>
2020-04-26 16:46:44 -0700

    Simplify tests and update storyshots

--- components/ReusableSections/SponsorsSection/__tests__/SponsorsSection.test.js
+++ components/ReusableSections/SponsorsSection/__tests__/SponsorsSection.test.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import createSnapshotTest from 'test-utils/createSnapshotTest';
+import { render } from '@testing-library/react';
+import partners from 'common/constants/partners';
 
 import SponsorsSection from '../SponsorsSection';
 
 describe('SponsorsSection', () => {
-  it('should render with required props', () => {
-    createSnapshotTest(<SponsorsSection>Test</SponsorsSection>);
-  });
+  it('should render a secure link and image for each partner', () => {
+    const component = render(<SponsorsSection>Test</SponsorsSection>);
 
-  it('should render with many props assigned', () => {
-    createSnapshotTest(<SponsorsSection className="test-class">Test</SponsorsSection>);
+    partners.forEach(partner => {
+      const image = component.queryByAltText(`${partner.name} logo`);
+      const link = image.parentNode;
+
+      expect(image).toBeInTheDocument();
+      expect(link.href.startsWith('https://')).toStrictEqual(true);
+    });
   });
 });
