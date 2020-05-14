5147703557dbeab0737ee443859dc1e482b92f6c
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-05-14 23:50:05 +1000

    update snapshots/skeleton test

--- components/Cards/ResourceCard/__tests__/ResourceSkeletonCard.test.js
+++ components/Cards/ResourceCard/__tests__/ResourceSkeletonCard.test.js
@@ -4,9 +4,10 @@ import { SKELETON_CARD } from 'common/constants/testIDs';
 import ResourceSkeletonCard from '../ResourceSkeletonCard';
 
 describe('ResourceSkeletonCard', () => {
+  const numberOfSkeletons = 5;
   it('renders the correct amount of skeletons', () => {
-    const component = render(<ResourceSkeletonCard numberOfSkeletons={5} />);
+    const component = render(<ResourceSkeletonCard numberOfSkeletons={numberOfSkeletons} />);
     const Skeletons = component.queryAllByTestId(SKELETON_CARD);
-    expect(Skeletons).toHaveLength(5);
+    expect(Skeletons).toHaveLength(numberOfSkeletons);
   });
 });
