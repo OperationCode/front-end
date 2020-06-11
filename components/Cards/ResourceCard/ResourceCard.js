0ced09943185cceeaeab5e4259b95056a9621b89
Kyle Holmberg <kylemh.email@gmail.com>
2020-06-10 23:06:02 -0700

    hide unused variables

--- components/Cards/ResourceCard/ResourceCard.js
+++ components/Cards/ResourceCard/ResourceCard.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { string, number, func, oneOf, oneOfType, array, bool } from 'prop-types';
-import classNames from 'classnames';
+// import { string, number, func, oneOf, oneOfType, array, bool } from 'prop-types';
+import { string, oneOfType, array, bool } from 'prop-types';
+// import classNames from 'classnames';
 import Accordion from 'components/Accordion/Accordion';
 import OutboundLink from 'components/OutboundLink/OutboundLink';
-import ScreenReaderOnly from 'components/ScreenReaderOnly/ScreenReaderOnly';
+// import ScreenReaderOnly from 'components/ScreenReaderOnly/ScreenReaderOnly';
 import {
-  UPVOTE_BUTTON,
-  DOWNVOTE_BUTTON,
+  // UPVOTE_BUTTON,
+  // DOWNVOTE_BUTTON,
   RESOURCE_CARD,
   RESOURCE_TITLE,
 } from 'common/constants/testIDs';
-import ThumbsUp from 'static/images/icons/FontAwesome/thumbs-up.svg';
-import ThumbsDown from 'static/images/icons/FontAwesome/thumbs-down.svg';
+// import ThumbsUp from 'static/images/icons/FontAwesome/thumbs-up.svg';
+// import ThumbsDown from 'static/images/icons/FontAwesome/thumbs-down.svg';
 import styles from './ResourceCard.module.css';
 
 export const possibleUserVotes = {
@@ -22,42 +23,42 @@ export const possibleUserVotes = {
 
 ResourceCard.propTypes = {
   description: string,
-  downvotes: number,
+  // downvotes: number,
   href: string.isRequired,
   name: string.isRequired,
   category: string,
   languages: oneOfType([string, array]),
   isPaid: bool,
-  onDownvote: func,
-  onUpvote: func,
-  upvotes: number,
-  userVote: oneOf(Object.values(possibleUserVotes)),
+  // onDownvote: func,
+  // onUpvote: func,
+  // upvotes: number,
+  // userVote: oneOf(Object.values(possibleUserVotes)),
 };
 
 ResourceCard.defaultProps = {
   description: '',
-  downvotes: 0,
+  // downvotes: 0,
   category: '',
   languages: [],
   isPaid: false,
-  onDownvote: () => {},
-  onUpvote: () => {},
-  upvotes: 0,
-  userVote: possibleUserVotes.none,
+  // onDownvote: () => {},
+  // onUpvote: () => {},
+  // upvotes: 0,
+  // userVote: possibleUserVotes.none,
 };
 
 function ResourceCard({
   description,
-  downvotes,
+  // downvotes,
   href,
   name,
   category,
   languages,
   isPaid,
-  onDownvote,
-  onUpvote,
-  upvotes,
-  userVote,
+  // onDownvote,
+  // onUpvote,
+  // upvotes,
+  // userVote,
 }) {
   // const didUpvote = userVote === possibleUserVotes.upvote;
   // const didDownvote = userVote === possibleUserVotes.downvote;
