e76d9c247270313f68f1778e4730b15e0d34825e
Jeffrey <jeffsnff@gmail.com>
2020-07-07 13:16:51 -0600

    Starts style of the profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -7,6 +7,7 @@ import LinkButton from 'components/LinkButton/LinkButton';
 import withAuthSync from 'decorators/withAuthSync/withAuthSync';
 import { getUserPromise } from 'common/constants/api';
 import styles from '../styles/profile.module.css';
+import { boolean } from 'yup';
 
 const pageTitle = 'Profile';
 
@@ -14,20 +15,31 @@ Profile.propTypes = {
   firstName: string.isRequired,
   lastName: string.isRequired,
   username: string.isRequired,
+  companyName: string.isRequired,
+  branchOfService: string.isRequired,
+  companyRole: string.isRequired,
+  createdAt: string.isRequired,
+  disciplines: string.isRequired,
+  email: string.isRequired,
+  employmentStatus: string.isRequired,
+  isMentor: boolean.isRequired,
+  militaryStatus: string.isRequired,
+  programmingLanguages: string.isRequired,
+  yearsOfService: string.isRequired
 };
 
 Profile.getInitialProps = async ctx => {
   const { firstName, lastName, token } = nextCookie(ctx);
   const { data } = await getUserPromise({ token });
 
-  const { username } = data;
+  const { username, companyName, branchOfService, companyRole, createdAt, disciplines, email, employmentStatus, isMentor, militaryStatus, programmingLanguages, yearsOfService } = data;
 
-  // console.log(data)
+  console.log(data)
 
-  return { firstName, lastName, username };
+  return { firstName, lastName, username, companyName, branchOfService, companyRole, createdAt, disciplines, email, employmentStatus, isMentor, militaryStatus, programmingLanguages, yearsOfService };
 };
 
-function Profile({ firstName, lastName, username }) {
+function Profile({ firstName, lastName, username, companyName, branchOfService, companyRole, createdAt, disciplines, email, employmentStatus, isMentor, militaryStatus, programmingLanguages, yearsOfService }) {
   return (
     <>
       <Head title={pageTitle} />
@@ -37,9 +49,21 @@ function Profile({ firstName, lastName, username }) {
       <Content
         theme="gray"
         columns={[
-          <p>
-            Hello {firstName} {lastName} {username}!
-          </p>,
+          <div style={{width: '100%'}}>
+            <h3 style={{textAlign: 'center'}}>
+              Hello Jeffrey Seneff!
+            </h3>
+            <p style={{border: '1px solid red', width: 'auto', display: 'inline', fontWeight: 'bold'}}>Employment Status :</p> <p style={{border: '1px solid blue', display: 'inline'}}>{employmentStatus}</p>
+            <p>Company Name : {companyName}</p>
+            <p>Company Role : {companyRole}</p>
+            <p>Military Status : {militaryStatus}</p>
+            <p>Branch of Service : {branchOfService}</p>
+            <p>Years of Service : {yearsOfService}</p>
+            <p>Programming Language Interest : {programmingLanguages}</p>
+            <p>Career Interest : {disciplines}</p>
+
+          </div>,
+          
           <div className={styles.actionItems}>
             <LinkButton theme="secondary" href="/profile/update" shouldPrefetch>
               Update Profile
