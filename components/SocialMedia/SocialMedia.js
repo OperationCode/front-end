4366754f92c7acb14e3ed381bf32c0e2392ff5f4
calincionca35 <36063669+calincionca35@users.noreply.github.com>
2020-08-31 10:53:31 -0700

    Create youtube_logo.svg

--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -3,6 +3,7 @@ import FacebookLogo from 'static/images/icons/facebook_logo.svg';
 import TwitterLogo from 'static/images/icons/twitter_logo.svg';
 import GitHubLogo from 'static/images/icons/github_logo_circle.svg';
 import InstagramLogo from 'static/images/icons/instagram_logo.svg';
+import YouTubeLogo from 'static/images/icons/youtube_logo.svg';
 import LinkedInLogo from 'static/images/icons/linkedin_logo_circle.svg';
 import SocialMediaContainer from './SocialMediaContainer/SocialMediaContainer';
 import SocialMediaItem from './SocialMediaItem/SocialMediaItem';
@@ -25,6 +26,11 @@ function SocialMedia() {
         href="https://www.instagram.com/operation_code/"
         name="Instagram"
         svg={<InstagramLogo className={styles.logo} />}
+      />
+      <SocialMediaItem
+        href="https://www.youtube.com/channel/UCAoJt4a_KEBmgXfoQUrNbSA"
+        name="YouTube"
+        svg={<YouTubeLogo className={styles.logo} />}
       />
       <SocialMediaItem
         href="https://www.linkedin.com/school/operationcode/"
