f938e2f6aebdb45e7a77b7c980a1c7b50ed22781
Kyle Holmberg <kylemh.email@gmail.com>
2020-09-19 21:31:22 -0700

    remove custom proptype

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import { string, bool } from 'prop-types';
+import { string, bool, oneOf } from 'prop-types';
 import classNames from 'classnames';
 import kebabCase from 'lodash/kebabCase';
 import ScreenReaderOnly from 'components/ScreenReaderOnly/ScreenReaderOnly';
 import LinkIcon from 'static/images/icons/FontAwesome/link-solid.svg';
 import styles from './Heading.module.css';
 
-function verifyHeadingLevel(props, propertyName, componentName) {
-  const cName = componentName || 'ANONYMOUS';
-
-  if ({ props }[propertyName]) {
-    const value = props[propertyName];
-    if (typeof value === 'number') {
-      return value >= 1 && value <= 6
-        ? null
-        : new Error(`${propertyName} in ${cName} is not within 1 to 6`);
-    }
-  }
-
-  // assume all ok
-  return null;
-}
-
 Heading.propTypes = {
   className: string,
   hasHashLink: bool,
   hasTitleUnderline: bool,
-  headingLevel: verifyHeadingLevel,
+  headingLevel: oneOf([1, 2, 3, 4, 5, 6]),
   text: string.isRequired,
 };
 
