83498b279c3c5a573bd212d49c1a9ae58dd6fe6d
thedevilx <aadithya794@gmail.com>
2020-10-02 01:48:39 +0530

    Added Secure Code Warrior Logo

--- common/constants/partners.js
+++ common/constants/partners.js
@@ -79,6 +79,12 @@ const partners = [
     url: 'https://vercel.com/home?utm_source=operationcode',
     type: PARTNER_TYPES.KIND,
   },
+  {
+    name: 'Secure Code Warrior',
+    logoSource: `https://user-images.githubusercontent.com/51661129/92973323-3d540b80-f484-11ea-85dd-d202fc751cd9.png`,
+    url: 'https://securecodewarrior.com/?utm_source=operationcode',
+    type: PARTNER_TYPES.KIND,
+  },
 ];
 
 export default sortBy(partners, 'name');
