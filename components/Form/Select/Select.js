62ea901235edad3c0d82e4115d120f2b9fb964e2
Murat Tishkul <tishkul@unist.ac.kr>
2020-10-01 16:31:10 +0600

    feat: added closeMenuOnSelect prop to select component

--- components/Form/Select/Select.js
+++ components/Form/Select/Select.js
@@ -125,6 +125,7 @@ export default function Select({
           onChange={onChangeHandler}
           options={options}
           value={value || ''}
+          closeMenuOnSelect={!isMulti}
         />
 
         <ErrorMessage
