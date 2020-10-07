9126ef03925fcd1419341849b9a8845cc98069d7
scratchy <scratchy@rutabaga.groudon>
2020-10-07 14:31:54 +0100

    Created stories for all forms

---
+++ components/Forms/ChangePasswordForm/__stories__/ChangePasswordForm.stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import ChangePasswordForm from '../ChangePasswordForm';
+
+export default {
+  component: ChangePasswordForm,
+  title: 'Forms/ChangePasswordForm',
+};
+
+const Template = arguments_ => {
+  return (
+    <>
+      <span>Change password form</span>
+      <ChangePasswordForm {...arguments_} />
+    </>
+  );
+};
+
+// Default Input supplied with only required args
+export const Default = Template.bind({});
+Default.args = {
+  initialValues: {
+    newPassword1: '',
+    newPassword2: '',
+  },
+  onSubmit: () => {},
+  onSuccess: () => {},
+};
