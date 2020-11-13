76ba209eb54235c71f04b06a339ecc756b828743
cbituin <inbetweenwebdesigns@gmail.com>
2020-11-13 05:39:19 -0600

    Removed content to start clean up and prep for importing blog data

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,9 +2,12 @@ import fs from 'fs';
 import { join } from 'path';
 import PropTypes from 'prop-types';
 import { ONE_DAY } from 'common/constants/unitsOfTime';
+// import Card from 'components/Cards/Card/Card';
 import Head from 'components/head';
+// import Heading from 'components/Heading/Heading';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
-import Link from 'next/link';
+// import Link from 'next/link';
+// import styles from 'styles/podcast.module.css';
 
 const pageTitle = 'Blogs';
 
@@ -26,22 +29,28 @@ BlogIndex.propTypes = {
 
 function BlogIndex({ articles }) {
   return (
-    <>
-      <Head title={pageTitle} />
-      <HeroBanner title={pageTitle} />
-      <ul>
-        {articles.map(article => {
-          return (
-            <li key={article}>
-              <Link href="/blog/[article]" as={`/blog/${article}`}>
-                <a>{article}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    // TODO: Need to create css file for Blog Cards
+    // <div className={styles.Blog}>
+    <div>
+    <Head title={pageTitle} />
+
+    <HeroBanner title={pageTitle} />
+
+    <p>{articles}</p>
+
+    {/* //   {articles.map(article => {
+    //     return (
+    //       //  TODO: Update classname for .blogCard
+    //       <>
+          
+    //       <Card data-testid="Blog Card" className={styles.podcastCard} key={article}>
+    //           <Link href="/blog/[article]" as={`/blog/${article}`} />
+    //           <p>{article.author}</p>
+    //       </Card>
+    //       </>
+    //     );
+    //   })} */}
+    </div>
   );
 }
-
 export default BlogIndex;
