3de84544cac5ba6321c492ade113151eddf6ef92
cbituin <inbetweenwebdesigns@gmail.com>
2020-11-24 14:16:47 -0600

    Initial api functions created for Blog

--- pages/api/blogIndex.js
+++ pages/api/blogIndex.js
@@ -1,4 +1,5 @@
 import matter from 'gray-matter';
+import marked from 'marked';
 
 export async function getAllPosts() {
   const context = require.context('../../blogArticles', false, /\.md$/)
@@ -7,11 +8,24 @@ export async function getAllPosts() {
   for (const key of context.keys()) {
     const post = key.slice(2);
     const content = await import(`../../blogArticles/${post}`);
-    const meta = matter(content.default)
+    const meta = matter(content.default);
     posts.push({
-      slug: post.replace('.md',''),
+      slug: post.replace('.mdx',''),
       title: meta.data.title
-    })
+    });
   };
   return posts;
 };
+
+export async function getPostBySlug(slug) {
+  const fileContent = await import(`../../blogArticles/${slug}.mdx`);
+  const meta = matter(fileContent.default);
+  const content = marked(meta.content);
+
+  return {
+    author: meta.data.author,
+    publishDate: meta.data.publishDate,
+    title: meta.data.title,
+    body: content,
+  }
+}
