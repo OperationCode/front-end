f472c7e5e0587a2791668a3758c2b60f183d3e6b
Kyle Holmberg <kylemh.email@gmail.com>
2021-01-09 11:10:23 -0800

    remove changes to color section of branding page

--- components/Branding/ColorSection/ColorSection.js
+++ components/Branding/ColorSection/ColorSection.js
@@ -5,7 +5,6 @@ import Content from 'components/Content/Content';
 import styles from './ColorSection.module.css';
 
 function ColorSection() {
-  // TODO: Name our primary and secondary colors
   const primaryColor = { name: 'Primary', hexCode: brandColorsObject.primary };
   const secondaryColor = { name: 'Secondary', hexCode: brandColorsObject.secondary };
 
@@ -40,52 +39,6 @@ function ColorSection() {
         columns={otherColorNames.map(colorName => (
           <Swatch colorName={colorName} hexCode={brandColorsObject[colorName]} key={colorName} />
         ))}
-      />
-
-      <Content
-        title="Primary Color Palette"
-        hasTitleUnderlinetheme="gray"
-        columns={[
-          <div>
-            <h3 className={styles.centeredText}>OC Light Blue</h3>
-            <Swatch colorName={primaryColor.name} hexCode="#3ED6F0" />
-          </div>,
-          <div>
-            <h3 className={styles.centeredText}>Navy</h3>
-            <Swatch colorName={primaryColor.name} hexCode="#252E3E" />
-          </div>,
-          <div>
-            <h3 className={styles.centeredText}>Off-White</h3>
-            <Swatch colorName={primaryColor.name} hexCode="#F7F7F7" />
-          </div>,
-          <div>
-            <h3 className={styles.centeredText}>Light Grey</h3>
-            <Swatch colorName={primaryColor.name} hexCode="#CCCCCC" />
-          </div>,
-          <div>
-            <h3 className={styles.centeredText}>Black</h3>
-            <Swatch colorName={primaryColor.name} hexCode="#000000" />
-          </div>,
-        ]}
-      />
-
-      <Content
-        title="Secondary Color Palette"
-        hasTitleUnderlinetheme="white"
-        columns={[
-          <div>
-            <h3 className={styles.centeredText}>OC Red</h3>
-            <Swatch colorname={secondaryColor.name} hexCode="#D1665A" />
-          </div>,
-          <div>
-            <h3 className={styles.centeredText}>Slate</h3>
-            <Swatch colorName={secondaryColor.name} hexCode="#3F566D" />
-          </div>,
-          <div>
-            <h3 className={styles.centeredText}>Dark Grey</h3>
-            <Swatch colorName={secondaryColor.name} hexCode="#666666" />
-          </div>,
-        ]}
       />
     </>
   );
