cef2e2fadb86ef1cfbac8f553c69b383791b5087
calincionca35 <36063669+calincionca35@users.noreply.github.com>
2021-02-22 19:21:29 -0800

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -45,6 +45,13 @@ function Footer() {
         <div className={classNames(styles.capitalize, styles.marginBottom)}>Connect With Us!</div>
         <SocialMedia />
       </div>
+      <div>
+        <img
+          src="https://user-images.githubusercontent.com/51661129/108640355-749a0000-7499-11eb-89af-4fdb6efaff97.gif"
+          alt="GuideStar Gold Transparency Seal"
+          className={styles.sealImg}
+        />
+      </div>
       <div className={classNames(styles.footerWrapper, styles.row)}>
         <div className={classNames(styles.footerGrouping, styles.linksGrouping, styles.capitalize)}>
           <div className={styles.linksRow}>
