3d28d4096e8621a5833be2fa58ab0700048d3dbe
Calin <36063669+calincionca35@users.noreply.github.com>
2021-02-25 15:44:24 -0800

    Center Footer nav columns calin (#1395)

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -61,9 +61,6 @@ function Footer() {
             <ul className={styles.linksColumn}>
               {footerItems.column2.map(link => renderLink(link))}
             </ul>
-          </div>
-
-          <div className={styles.linksRow}>
             <ul className={styles.linksColumn}>
               {footerItems.column3.map(link => renderLink(link))}
             </ul>
