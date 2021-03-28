1d962d00ce3cc1540ed3ce01d0139c858ad989df
calincionca35 <36063669+calincionca35@users.noreply.github.com>
2021-03-28 09:35:59 -0700

    create slack guide page faq placeholders

--- pages/slack_guide.js
+++ pages/slack_guide.js
@@ -1,11 +1,31 @@
 import Head from 'components/head';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
+import Content from 'components/Content/Content';
+import Accordion from 'components/Accordion/Accordion';
+import styles from 'styles/faq.module.css';
 
 const pageTitle = 'Slack Guide';
 
+const questions = {
+  slack: [
+    {
+      title: 'Question?',
+      content: <>Answer</>,
+    },
+    {
+      title: 'Question?',
+      content: <>Answer</>,
+    },
+    {
+      title: 'Question?',
+      content: <>Answer</>,
+    },
+  ],
+};
+
 function SlackGuide() {
   return (
-    <div>
+    <div className={styles.FAQ}>
       <Head title={pageTitle} />
 
       <HeroBanner title={pageTitle}>
@@ -18,6 +38,20 @@ function SlackGuide() {
           height="425"
         />
       </HeroBanner>
+
+      <HeroBanner title="Slack Frequently Asked Questions" />
+
+      <Content
+        columns={questions.slack.map(faq => (
+          <Accordion
+            className={styles.FAQAccordion}
+            content={{
+              headingChildren: <h6>{faq.title}</h6>,
+              bodyChildren: <p>{faq.content}</p>,
+            }}
+          />
+        ))}
+      />
     </div>
   );
 }
