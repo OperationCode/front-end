c77751879edca5d51870e8a96614347553a5318b
alexzw7 <alexzw@umich.edu>
2021-04-21 23:32:59 -0400

    added why donate

--- pages/donate.js
+++ pages/donate.js
@@ -8,15 +8,27 @@ function DonatePage() {
   return (
     <>
       <Head title={pageTitle} />
-
       <HeroBanner title={pageTitle} />
-
       <Container theme="white">
+        <h1>Why Donate?</h1>
+        <p>
+          All donations directly help provide our nation&apos;s finest who&apos;ve worn the uniform
+          and their families the opportunity to learn software development, enter the tech industry,
+          and code the future.
+        </p>
+        <p>
+          Your donation helps keep our mentorship program up and running, allows us to provide
+          coding scholarships and licenses, and more.
+        </p>
+        <p>
+          We pride ourselves in our transparency, making sure that our community knows what their
+          contributions are going towards.
+        </p>
         <iframe
           title="Donation Form"
           src="https://secure.lglforms.com/form_engine/s/BRtP7QUKyHOyEYsZROsRew"
           height="1500px"
-          width="110%"
+          width="100%"
           style={{
             border: '1px solid transparent',
             outline: '1px solid transparent',
