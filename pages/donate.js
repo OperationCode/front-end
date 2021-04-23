dcf2d6d0a8e3c745f482d7d88805c94045fb7dbd
alexzw7 <alexzw@umich.edu>
2021-04-23 02:03:17 -0400

    added more to why donate

--- pages/donate.js
+++ pages/donate.js
@@ -10,20 +10,27 @@ function DonatePage() {
       <Head title={pageTitle} />
       <HeroBanner title={pageTitle} />
       <Container theme="white">
-        <h1>Why Donate?</h1>
-        <p>
-          All donations directly help provide our nation&apos;s finest who&apos;ve worn the uniform
-          and their families the opportunity to learn software development, enter the tech industry,
-          and code the future.
-        </p>
-        <p>
-          Your donation helps keep our mentorship program up and running, allows us to provide
-          coding scholarships and licenses, and more.
-        </p>
-        <p>
-          We pride ourselves in our transparency, making sure that our community knows what their
-          contributions are going towards.
-        </p>
+        <div style={{ borderBottom: '4px solid #252e3e' }}>
+          <h1>Why Donate?</h1>
+          <p>
+            All donations go directly towards providing our nation&apos;s finest who&apos;ve worn
+            the uniform and their families the opportunity to learn software development, enter the
+            tech industry, and code the future.
+          </p>
+          <p>
+            As the largest community of military veterans, service members, and spouses, we have
+            ample experience and have helped thousands of members enter into the tech industry.
+          </p>
+          <p>
+            Your donation helps keep our mentorship program up and running, allows us to provide
+            coding scholarships and licenses, and more.
+          </p>
+          <p>
+            We pride ourselves in transparency, making sure that our community knows what their
+            contributions are going towards.
+          </p>
+          <br />
+        </div>
         <iframe
           title="Donation Form"
           src="https://secure.lglforms.com/form_engine/s/BRtP7QUKyHOyEYsZROsRew"
