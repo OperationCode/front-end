2632b478b91c3e7b92c69815c73b8003408e5277
Kyle Holmberg <kylemh.email@gmail.com>
2021-09-30 05:11:35 -0400

    resolve lint errors

--- components/Press/PressLinks/Articles.js
+++ components/Press/PressLinks/Articles.js
@@ -5,8 +5,7 @@ const Boston = [
   },
   {
     title: 'MassChallenge Finalists: Finnest, Operation Code, Sunrise Health',
-    url:
-      'https://www.necn.com/news/business/MassChallenge-Finnest_-Operation-Code_-Sunrise-Health_NECN-441063423.html',
+    url: 'https://www.necn.com/news/business/MassChallenge-Finnest_-Operation-Code_-Sunrise-Health_NECN-441063423.html',
   },
   {
     title: 'TechBreakfast at Microsoft NERD',
@@ -22,8 +21,7 @@ const Boston = [
   },
   {
     title: 'Operation Code – Creating a Coding Community with Veterans and Citizens',
-    url:
-      'https://venturefizz.com/stories/boston/operation-code-creating-coding-community-veterans-and-citizens',
+    url: 'https://venturefizz.com/stories/boston/operation-code-creating-coding-community-veterans-and-citizens',
   },
   {
     title: 'Innovation Spotlight: Operation Code',
@@ -38,8 +36,7 @@ const Boston = [
 const General = [
   {
     title: 'TechHire Educator Spotlight: Operation Code',
-    url:
-      'https://blog.opportunityatwork.org/techhire-educator-spotlight-operation-code-debd0a796f9d',
+    url: 'https://blog.opportunityatwork.org/techhire-educator-spotlight-operation-code-debd0a796f9d',
   },
   {
     title: 'Why Veterans Will Make Excellent Programmers',
@@ -51,8 +48,7 @@ const General = [
   },
   {
     title: 'Hacking Entrepreneurship — An Interview with David Molina of Operation Code',
-    url:
-      'https://medium.com/the-hum/hacking-entrepreneurship-an-interview-with-david-molina-of-operation-code-12a7e199e4e0',
+    url: 'https://medium.com/the-hum/hacking-entrepreneurship-an-interview-with-david-molina-of-operation-code-12a7e199e4e0',
   },
   {
     title: 'Operation Code: Connecting Veterans with Code Skills',
@@ -65,8 +61,7 @@ const General = [
   {
     title:
       'When the call of duty is technology, veterans rally to support each other through Operation Code', // eslint-disable-line max-len
-    url:
-      'https://www.geekwire.com/2016/call-duty-technology-veterans-rally-support-operation-code/',
+    url: 'https://www.geekwire.com/2016/call-duty-technology-veterans-rally-support-operation-code/',
   },
   {
     title: 'Get Coding Now with Operation Code Army Veteran and Founder David Molina',
@@ -86,8 +81,7 @@ const General = [
   },
   {
     title: 'Helping Veterans Learn to Code with David Molina',
-    url:
-      'https://softwareengineeringdaily.com/2016/03/20/helping-veterans-learn-code-david-molina/',
+    url: 'https://softwareengineeringdaily.com/2016/03/20/helping-veterans-learn-code-david-molina/',
   },
   {
     title: 'Podcast: a developing story as told by developers',
@@ -99,13 +93,11 @@ const General = [
   },
   {
     title: 'From Aviation Electrician to Back End Engineering: Bret Funk’s Operation Code story',
-    url:
-      'http://blog.teamtreehouse.com/from-aviation-electrician-to-back-end-engineering-bret-funk-operation-code-story',
+    url: 'http://blog.teamtreehouse.com/from-aviation-electrician-to-back-end-engineering-bret-funk-operation-code-story',
   },
   {
     title: 'Navy Veteran to Software Developer: Geno Guerrero’s Operation Code Story',
-    url:
-      'http://blog.teamtreehouse.com/navy-veteran-to-software-developer-geno-guerreros-operation-code-story',
+    url: 'http://blog.teamtreehouse.com/navy-veteran-to-software-developer-geno-guerreros-operation-code-story',
   },
   {
     title: 'From Marine Corps Veteran to Front End Developer: Billy Le’s Operation Code Story',
@@ -117,8 +109,7 @@ const General = [
   },
   {
     title: "Coding Boot Camps Go After Veterans To Take Silicon Valley's Vacant Tech Jobs",
-    url:
-      'https://www.ibtimes.com/coding-boot-camps-go-after-veterans-take-silicon-valleys-vacant-tech-jobs-2174421',
+    url: 'https://www.ibtimes.com/coding-boot-camps-go-after-veterans-take-silicon-valleys-vacant-tech-jobs-2174421',
   },
   {
     title: '20 Diversity and Inclusion Leaders to Follow in 2018',
@@ -126,8 +117,7 @@ const General = [
   },
   {
     title: 'Tech innovation meets military service: GeekWire’s Memorial Day remembrance and update',
-    url:
-      'http://bitshare.cm/news/tech-innovation-meets-military-service-geekwires-memorial-day-remembrance-and-update/',
+    url: 'http://bitshare.cm/news/tech-innovation-meets-military-service-geekwires-memorial-day-remembrance-and-update/',
   },
   {
     title: "MAP HOSTS CONGRESSIONAL BRIEFING ON VETERANS' READINESS IN TECH CAREERS",
@@ -135,13 +125,11 @@ const General = [
   },
   {
     title: "Analytics Pros, Inc. Hosts Training for Veterans and Veterans' Spouses",
-    url:
-      'https://www.advfn.com/news_Analytics-Pros-Inc-Hosts-Training-for-Veterans-a_72657464.html',
+    url: 'https://www.advfn.com/news_Analytics-Pros-Inc-Hosts-Training-for-Veterans-a_72657464.html',
   },
   {
     title: 'Episode 36 – How to pick a programming language to learn for new developers – Part 2',
-    url:
-      'http://mentoringdevelopers.com/episode-36-how-to-pick-a-programming-language-to-learn-for-new-developers-part-2/',
+    url: 'http://mentoringdevelopers.com/episode-36-how-to-pick-a-programming-language-to-learn-for-new-developers-part-2/',
   },
   {
     title: 'How to “upskill” Veterans’ training for technology jobs',
@@ -157,13 +145,11 @@ const General = [
   },
   {
     title: 'Seattle coding-school tuition to be covered by GI Bill',
-    url:
-      'https://www.seattletimes.com/business/technology/seattles-code-fellows-wins-approval-under-gi-bill/',
+    url: 'https://www.seattletimes.com/business/technology/seattles-code-fellows-wins-approval-under-gi-bill/',
   },
   {
     title: 'Veteran: GI Bill should cover code school',
-    url:
-      'https://www.koin.com/news/veteran-gi-bill-should-cover-code-school_20180208085618706/960235415',
+    url: 'https://www.koin.com/news/veteran-gi-bill-should-cover-code-school_20180208085618706/960235415',
   },
   {
     title:
@@ -180,26 +166,22 @@ const General = [
   },
   {
     title: 'Operation Code Looks to Help Veterans Land IT Careers',
-    url:
-      'https://news.clearancejobs.com/2017/07/17/operation-code-looks-help-veterans-land-careers/',
+    url: 'https://news.clearancejobs.com/2017/07/17/operation-code-looks-help-veterans-land-careers/',
   },
 ];
 
 const NYC = [
   {
     title: 'New program aims to help veterans land jobs in tech industry',
-    url:
-      'https://wtkr.com/2018/05/16/new-program-aims-to-help-veterans-land-jobs-in-tech-industry/',
+    url: 'https://wtkr.com/2018/05/16/new-program-aims-to-help-veterans-land-jobs-in-tech-industry/',
   },
   {
     title: 'Veterans in Residence',
-    url:
-      'http://www.wdtv.com/content/news/Veterans-in-Residence-program-helping-veterans-grow-businesses-482707221.html',
+    url: 'http://www.wdtv.com/content/news/Veterans-in-Residence-program-helping-veterans-grow-businesses-482707221.html',
   },
   {
     title: 'After Flying Solo, Veterans Find Others Who Have Their Backs',
-    url:
-      'https://www.wework.com/blog/posts/after-flying-solo-veterans-find-others-who-have-their-back',
+    url: 'https://www.wework.com/blog/posts/after-flying-solo-veterans-find-others-who-have-their-back',
   },
 ];
 
