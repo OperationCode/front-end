69bd41b1ab51fe925f620b774f28b17b0c8c7789
Kyle Holmberg <kylemh.email@gmail.com>
2021-10-02 19:29:25 -0400

    run linter

--- common/constants/urls.js
+++ common/constants/urls.js
@@ -1,7 +1,6 @@
 const s3hostName = 'operation-code-assets.s3.us-east-2.amazonaws.com';
 const s3 = `https://${s3hostName}/`;
-const leadershipCircleLink =
-  'https://secure.lglforms.com/form_engine/s/L428AQ2rrsFJQyy5Fbglvg';
+const leadershipCircleLink = 'https://secure.lglforms.com/form_engine/s/L428AQ2rrsFJQyy5Fbglvg';
 const codeOfConduct = `https://github.com/OperationCode/operationcode_docs/blob/master/community/code_of_conduct.md`;
 
 module.exports = { s3hostName, s3, leadershipCircleLink, codeOfConduct };
