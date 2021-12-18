d5445c18cf02da6651c7180af25f2b7e75ad6255
Kyle Holmberg <inbox@kylemh.com>
2021-12-18 10:51:28 +0700

    Revert "Updating Accordion component to Radix Accordion" (#1555)

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -1,9 +1,14 @@
-import { arrayOf, node, number, shape, string, oneOfType } from 'prop-types';
+import { useState } from 'react';
+import { arrayOf, bool, node, number, shape, string, oneOfType } from 'prop-types';
 import classNames from 'classnames';
-import { ACCORDION_CONTENT, ACCORDION_TOGGLE_BUTTON } from 'common/constants/testIDs';
 import Chevron from 'public/static/images/icons/FontAwesome/angle-right-solid.svg';
-import * as RadixAccordion from '@radix-ui/react-accordion';
+import { ACCORDION_CONTENT, ACCORDION_TOGGLE_BUTTON } from 'common/constants/testIDs';
+import ScreenReaderOnly, { toggleMessages } from '../ScreenReaderOnly/ScreenReaderOnly';
+import Card from '../Cards/Card/Card';
 import styles from './Accordion.module.css';
+
+const ChevronRight = () => <Chevron className={styles.icon} />;
+const ChevronDown = () => <Chevron className={classNames(styles.icon, styles.rotate90)} />;
 
 Accordion.propTypes = {
   /** Accessibility ID to use for joining elements together
@@ -18,48 +23,69 @@ Accordion.propTypes = {
     /** Section of content associated with header */
     bodyChildren: oneOfType([node, arrayOf(node)]).isRequired,
   }).isRequired,
+  /** Should Accordion have animation on hover */
+  hasAnimationOnHover: bool,
 };
 
 Accordion.defaultProps = {
   className: undefined,
+  hasAnimationOnHover: false,
 };
 
 /**
  * @description A component whose main content is invisible until revealed by the user
  * @see http://web-accessibility.carnegiemuseums.org/code/accordions/
  */
-function Accordion({ accessibilityId, className, content }) {
+function Accordion({ accessibilityId, className, content, hasAnimationOnHover }) {
+  const [isContentVisible, setContentVisibility] = useState(false);
+
+  const toggleAccordionContent = () => setContentVisibility(previousState => !previousState);
+
   const contentId = `content-${accessibilityId}`;
   const accordionId = `accordion-control-${accessibilityId}`;
 
   return (
-    <RadixAccordion.Root
-      type="single"
+    <Card
       className={classNames(styles.Accordion, className)}
-      collapsible
+      hasAnimationOnHover={hasAnimationOnHover}
     >
-      <RadixAccordion.Item value={accordionId}>
-        <RadixAccordion.Header>
-          <RadixAccordion.Trigger
-            className={styles.headingContainer}
-            data-testid={ACCORDION_TOGGLE_BUTTON}
-            id={accordionId}
-          >
-            <div className={styles.headingContainer}>{content.headingChildren}</div>
-            <Chevron className={styles.icon} />
-          </RadixAccordion.Trigger>
-        </RadixAccordion.Header>
-        <RadixAccordion.Content>
-          <section
-            className={styles.accordionContent}
-            data-testid={ACCORDION_CONTENT}
-            id={contentId}
-          >
-            {content.bodyChildren}
-          </section>
-        </RadixAccordion.Content>
-      </RadixAccordion.Item>
-    </RadixAccordion.Root>
+      <div className={styles.headingContainer}>{content.headingChildren}</div>
+
+      <button
+        aria-controls={contentId}
+        aria-expanded={isContentVisible}
+        className={styles.accordionToggleButton}
+        data-testid={ACCORDION_TOGGLE_BUTTON}
+        id={accordionId}
+        onClick={toggleAccordionContent}
+        type="button"
+      >
+        {isContentVisible ? (
+          <>
+            <ScreenReaderOnly>{toggleMessages.close}</ScreenReaderOnly>
+            <ChevronDown />
+          </>
+        ) : (
+          <>
+            <ScreenReaderOnly>{toggleMessages.open}</ScreenReaderOnly>
+            <ChevronRight />
+          </>
+        )}
+      </button>
+
+      <section
+        aria-hidden={!isContentVisible}
+        className={classNames(styles.accordionContent, {
+          [styles.visible]: isContentVisible,
+          [styles.invisible]: !isContentVisible,
+        })}
+        data-testid={ACCORDION_CONTENT}
+        id={contentId}
+        style={{ display: isContentVisible ? 'block' : 'none' }}
+      >
+        {content.bodyChildren}
+      </section>
+    </Card>
   );
 }
 
