86709789a4f54248859328f8e4e8dd003ddaf070
Kyle Holmberg <kylemh.email@gmail.com>
2022-12-23 23:21:36 -0500

    adjust join form styles

--- pages/join.js
+++ pages/join.js
@@ -1,6 +1,7 @@
 import Head from 'components/head';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
 import Content from 'components/Content/Content';
+import styles from 'styles/join.module.css';
 
 const pageTitle = 'Join';
 
@@ -13,13 +14,17 @@ function Join() {
       <Content
         theme="gray"
         columns={[
-          <script
-            src="https://cdn.virtuoussoftware.com/virtuous.embed.min.js"
-            data-vform="925226EB-B502-4DAF-A38F-FAFBB8C98146"
-            data-orgId="3423"
-            data-isGiving="false"
-            data-dependencies="[]"
-          />,
+          <div className={styles.joinForm}>
+            <script
+              src="https://cdn.virtuoussoftware.com/virtuous.embed.min.js"
+              crossOrigin=""
+              fetchpriority="high"
+              data-vform="925226EB-B502-4DAF-A38F-FAFBB8C98146"
+              data-orgId="3423"
+              data-isGiving="false"
+              data-dependencies="[]"
+            />
+          </div>,
         ]}
       />
     </>
