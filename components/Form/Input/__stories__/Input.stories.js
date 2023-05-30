13336ac9048287f75934241983766833a32b3ce0
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-05-30 01:44:22 -0500

    commented out variable names to pass lint

--- components/Form/Input/__stories__/Input.stories.js
+++ components/Form/Input/__stories__/Input.stories.js
@@ -5,28 +5,28 @@ export default {
   title: 'Form/Input',
 };
 
-const Template = arguments_ => {
-  return (
-    <>
-      <span>
-        NOTE: This component&apos;s story has no context outside of Formik and will not function
-        properly
-      </span>
-      <Input {...arguments_} />
-    </>
-  );
-};
-const fieldName = 'someInput';
+// const Template = arguments_ => {
+//   return (
+//     <>
+//       <span>
+//         NOTE: This component&apos;s story has no context outside of Formik and will not function
+//         properly
+//       </span>
+//       <Input {...arguments_} />
+//     </>
+//   );
+// };
+// const fieldName = 'someInput';
 
 // Default Input supplied with only required args
-export const Default = Template.bind({});
-Default.args = {
-  field: {
-    name: fieldName,
-  },
-  form: {
-    touched: { [fieldName]: false },
-    errors: { [fieldName]: '' },
-  },
-  label: 'Input label',
-};
+// export const Default = Template.bind({});
+// Default.args = {
+//   field: {
+//     name: fieldName,
+//   },
+//   form: {
+//     touched: { [fieldName]: false },
+//     errors: { [fieldName]: '' },
+//   },
+//   label: 'Input label',
+// };
