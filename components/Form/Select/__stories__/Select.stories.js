87b65e21bcf5f1373841c7fc8e46b10ff0c53d5b
The Stedman <46408716+recondesigns@users.noreply.github.com>
2023-06-06 07:38:52 -0500

    Fix #1521: chromatic is not working (#1705)
    Co-authored-by: Kyle Holmberg <kylemh.email@gmail.com>

--- components/Form/Select/__stories__/Select.stories.js
+++ components/Form/Select/__stories__/Select.stories.js
@@ -1,47 +1,77 @@
+import { Formik, Field } from 'formik';
+import Form from '../../Form';
 import Select from '../Select';
+
+const selectOptions = [
+  { label: 'Air Force', value: 'air force' },
+  { label: 'Army', value: 'army' },
+  { label: 'Coast Guard', value: 'coast guard' },
+  { label: 'Marines', value: 'marines' },
+  { label: 'Navy', value: 'navy' },
+  { label: 'Space Force', value: 'space force' },
+];
+
+/** Template to be shared by stories. */
+const SelectTemplate = args => {
+  const {
+    field: { name },
+    label,
+    options,
+  } = args;
+
+  return (
+    <Formik initialValues={{ [`${name}`]: '' }} onSubmit={values => console.log(values)}>
+      <Form>
+        <Field name={name}>
+          {({ field, form }) => (
+            <div>
+              <Select field={field} form={form} label={label} options={options} />
+            </div>
+          )}
+        </Field>
+      </Form>
+    </Formik>
+  );
+};
+
+/** Default Select supplied with only required args */
+export const Default = SelectTemplate.bind({});
+
+Default.args = {
+  field: { name: 'branchSelect' },
+  label: 'Please select a  branch:',
+  options: selectOptions.slice(0, 1),
+};
+
+/** Select component with multiple options supplied */
+export const WithMultipleOptions = SelectTemplate.bind({});
+
+WithMultipleOptions.args = {
+  field: { name: 'branchSelect' },
+  label: 'Please select a  branch:',
+  options: selectOptions,
+};
 
 export default {
   component: Select,
   title: 'Form/Select',
-};
-
-const Template = arguments_ => {
-  return (
-    <>
-      <span>
-        NOTE: This component&apos;s story has no context outside of Formik and will not function
-        properly
-      </span>
-      <Select {...arguments_} />
-    </>
-  );
-};
-
-const fieldName = 'someInput';
-const options = [
-  { label: 'Option1 Label', value: 'Option1 Value' },
-  { label: 'Option2 Label', value: 'Option2 Value' },
-  { label: 'Option3 Label', value: 'Option3 Value' },
-];
-
-/** Default Select supplied with only required args */
-export const Default = Template.bind({});
-Default.args = {
-  field: {
-    name: fieldName,
-    value: '',
+  argTypes: {
+    form: {
+      table: { disable: true },
+    },
   },
-  form: {
-    touched: { [fieldName]: false },
-    errors: { [fieldName]: '' },
+  parameters: {
+    controls: { sort: 'requiredFirst' },
   },
-  label: 'Select Label',
-  options: options.slice(0, 1),
-};
-
-/** Select component with multiple options supplied */
-export const WithMultipleOptions = Template.bind({});
-WithMultipleOptions.args = {
-  ...Default.args,
-  options,
+  decorators: [
+    SelectStory => (
+      <>
+        <span>
+          NOTE: This component&apos;s story has no context outside of Formik and will not function
+          properly
+        </span>
+        <SelectStory />
+      </>
+    ),
+  ],
 };
