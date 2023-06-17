e05c9068e05ff4273fb443fe2d12c3fea2cbcaf3
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-06-17 00:54:55 -0500

    added documentation to ProgressIndicator stories

--- components/ProgressIndicator/__stories__/ProgressIndicator.stories.js
+++ components/ProgressIndicator/__stories__/ProgressIndicator.stories.js
@@ -1,3 +1,12 @@
+import {
+  Title,
+  ArgTypes,
+  Canvas,
+  Heading,
+  Subheading,
+  Description,
+  Source,
+} from '@storybook/blocks';
 import ProgressIndicator from '../ProgressIndicator';
 
 export const Default = {
@@ -29,6 +38,53 @@ const meta = {
     },
     totalSteps: {
       control: { type: 'number' },
+    },
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title>Progress Indicator</Title>
+          <Canvas of={Default} />
+          <ArgTypes />
+          <Heading>Best practices</Heading>
+          <br />
+          <Subheading>Basic usage</Subheading>
+          <Description>Import the `ProgressIndicator` component and render.</Description>
+          <Source
+            format
+            code={`
+              import ProgressIndicator from 'components/ProgressIndicator/ProgressIndicator';
+            `}
+          />
+          <Description>Use in the UI where needed.</Description>
+          <Source
+            format
+            code={`
+              function Example() {
+                return (
+                  <div>
+                    <ProgressIndicator />
+                  </div>
+                );
+              }
+            `}
+          />
+          <Subheading>totalSteps</Subheading>
+          <Description>
+            Sets the number of &quot;steps&quot; are needed to fill the indicator bar. *The
+            `totalSteps` value needs to be more than, or equal to the `stepNumber` value*.
+          </Description>
+          <Canvas of={TotalSteps} sourceState="shown" />
+          <Subheading>stepNumber</Subheading>
+          <Description>
+            Sets the number of &quot;completed&quot; steps and fills the indicator bar relative to
+            the `totalSteps` value. *The `stepNumber` value needs to be less than, or equal to the
+            `totalSteps` value*.
+          </Description>
+          <Canvas of={StepNumber} sourceState="shown" />
+        </>
+      ),
     },
   },
 };
