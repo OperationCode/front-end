78cfb375c6c2e80ebf237840c2ecdc257950c730
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-06-17 01:48:41 -0500

    updated Modal stories to new format

--- components/Modal/__stories__/Modal.stories.js
+++ components/Modal/__stories__/Modal.stories.js
@@ -1,16 +1,49 @@
+import { useState } from 'react';
 import { descriptions } from 'common/constants/descriptions';
 import Modal from '../Modal';
 
-export default {
-  component: Modal,
+export const Default = {
+  render: args => {
+    const [isDemoModalOpen, setIsDemoModalOpen] = useState(args.isOpen);
+
+    return (
+      <Modal
+        {...args}
+        isOpen={isDemoModalOpen}
+        onRequestClose={prevValue => setIsDemoModalOpen(!prevValue)}
+      />
+    );
+  },
+  args: {
+    isOpen: true,
+    children: descriptions.medium,
+    screenReaderLabel: 'You have completed the form.',
+  },
+};
+
+export const NoCloseOnOverlayClick = {
+  render: args => {
+    const [isDemoModalOpen, setIsDemoModalOpen] = useState(args.isOpen);
+
+    return (
+      <Modal
+        {...args}
+        isOpen={isDemoModalOpen}
+        onRequestClose={prevValue => setIsDemoModalOpen(!prevValue)}
+      />
+    );
+  },
+  args: {
+    shouldCloseOnOverlayClick: false,
+    isOpen: true,
+    children: descriptions.medium,
+    screenReaderLabel: 'You have completed the form.',
+  },
+};
+
+const meta = {
   title: 'Modal',
+  component: Modal,
 };
 
-const Template = arguments_ => <Modal {...arguments_} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  children: descriptions.medium,
-  isOpen: true,
-  screenReaderLabel: 'Screen reader declaration',
-};
+export default meta;
