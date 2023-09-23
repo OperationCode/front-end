b59594ea86f6f32604d16286cfafa7b2ccc05706
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-09-23 04:35:44 +0000

    Converted all the css into tailwind css of Modal component

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -2,10 +2,8 @@ import { node, string, bool, func } from 'prop-types';
 import classNames from 'classnames';
 import * as Dialog from '@radix-ui/react-dialog';
 import { gtag } from 'common/utils/thirdParty/gtag';
-import CardStyles from 'components/Cards/Card/Card.module.css';
 import CloseButton from 'components/Buttons/CloseButton/CloseButton';
 import { MODAL_CONTENT, MODAL_OVERLAY } from 'common/constants/testIDs';
-import ModalStyles from './Modal.module.css';
 
 Modal.propTypes = {
   children: node.isRequired,
@@ -41,7 +39,7 @@ function Modal({
     <Dialog.Root defaultOpen={false} open={isOpen}>
       <Dialog.Portal>
         <Dialog.Overlay
-          className={ModalStyles.overlay}
+          className="inset-0 fixed bg-gray"
           onClick={shouldCloseOnOverlayClick ? onRequestClose : undefined}
           data-testid={MODAL_OVERLAY}
         >
@@ -50,7 +48,11 @@ function Modal({
           </Dialog.Close>
         </Dialog.Overlay>
         <Dialog.Content
-          className={classNames(CardStyles.Card, className, ModalStyles.modalContent)}
+          className={classNames(
+            'items-center bg-white text-secondary flex flex-col flex-nowrap justify-around m-4 min-h-[100px] min-w-[100px] p-6',
+            className,
+            'fixed items-center justify-center overflow-hidden w-10/12 top-3/4 left-3/4 -translate-x-2/4 -translate-y-2/4 z-[2]',
+          )}
         >
           <div className={childrenClassName} data-testid={MODAL_CONTENT}>
             {children}
