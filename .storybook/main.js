310f33d26c6923685c02848799670f6f8ff83f2f
Kyle Holmberg <kylemh.email@gmail.com>
2023-09-23 23:35:24 +0300

    integrate tailwind into Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,14 @@ const svgoConfig = require('../common/config/svgo');
 const config = {
   stories: ['../components/**/__stories__/*.stories.js'],
   staticDirs: ['../public'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      /** @see https://storybook.js.org/recipes/tailwindcss#:~:text=then%20leave%20the%20options%20object%20empty. */
+      options: {},
+    },
+  ],
   webpackFinal: async config => {
     // Find the Storybook Webpack rule relevant to SVG files.
     const imageRule = config.module.rules.find(rule => {
