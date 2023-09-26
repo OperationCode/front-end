c7f00f0e68ad1f37b4db6fe871fd3950c204d64e
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-09-26 16:54:38 +0000

    Converted all the css into Tailwind css of Drawer component

--- components/Drawer/Drawer.js
+++ components/Drawer/Drawer.js
@@ -15,12 +15,14 @@ Drawer.defaultProps = {
 function Drawer({ children, className, isVisible }) {
   return (
     <div
-      className={classNames(className, {
-        '-left-0 hidden lg:block transition-all ease-in-out duration-1000 fixed top- bottom-0  overflow-hidden width-full z-[2]':
-          isVisible,
-        '-left-[100%] hidden lg:block transition-all ease-in-out duration-1000 fixed top- bottom-0  overflow-hidden width-full z-[2]':
-          !isVisible,
-      })}
+      className={classNames(
+        className,
+        'hidden lg:block transition-all ease-in-out duration-1000 fixed top- bottom-0  overflow-hidden width-full z-[2]',
+        {
+          '-left-0': isVisible,
+          '-left-[100%]': !isVisible,
+        },
+      )}
     >
       <div className="h-full w-full">{children}</div>
     </div>
