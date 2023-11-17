c6398444d4ee9b046f9959693f4ba8cc91c06298
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-11-17 13:44:51 +0000

    Replaced all the css into tailwind css on OutboundLink component

--- components/OutboundLink/OutboundLink.js
+++ components/OutboundLink/OutboundLink.js
@@ -3,7 +3,6 @@ import { gtag } from 'common/utils/thirdParty/gtag';
 import ExternalLinkIcon from 'static/images/icons/FontAwesome/external-link-square-alt-solid.svg';
 import ScreenReaderOnly from 'components/ScreenReaderOnly/ScreenReaderOnly';
 import classNames from 'node_modules/classnames/index';
-import styles from './OutboundLink.module.css';
 
 OutboundLink.propTypes = {
   // will report this label plus the URL from where it was clicked
@@ -37,7 +36,7 @@ function OutboundLink({
 
   return (
     <a
-      className={classNames(styles.OutboundLink, className)}
+      className={classNames('inline-flex items-start', className)}
       data-testid={testID}
       href={href}
       onClick={trackOutboundLinkClick}
@@ -48,7 +47,7 @@ function OutboundLink({
 
       <ScreenReaderOnly>Opens in new window</ScreenReaderOnly>
 
-      {hasIcon && <ExternalLinkIcon className={styles.externalLinkIcon} />}
+      {hasIcon && <ExternalLinkIcon className="fill-current relative my-0 mx-1 bottom-0 w-3" />}
     </a>
   );
 }
