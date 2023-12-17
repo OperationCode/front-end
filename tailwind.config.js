6c4d915fbc3c0abe4a794f230820c2835432bbcb
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-12-17 11:47:10 +0530

    Replaced all the css into tailwind css on Label component (#1785)

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
       colors: {
         themeGray: '#121212',
         primary: '#3ed6f0',
-        secondary: '#252e3e',
+        secondary: 'rbg(37, 46, 62, 0.75)',
       },
     },
     maxWidth: {
