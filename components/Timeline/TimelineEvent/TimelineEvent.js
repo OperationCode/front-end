4ff4e36f7067b0f95eb665a8e957b431d548b643
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-12-20 20:05:36 +0530

    Replaced all the css into tailwind css on TimelineEvent component (#1786)
    Co-authored-by: Kyle Holmberg <kylemh.email@gmail.com>

--- components/Timeline/TimelineEvent/TimelineEvent.js
+++ components/Timeline/TimelineEvent/TimelineEvent.js
@@ -8,8 +8,8 @@ TimelineEvent.propTypes = {
 
 function TimelineEvent({ content, title }) {
   return (
-    <div className={styles.eventContainer}>
-      <h4 className={styles.eventTitle}>{title}</h4>
+    <div className="mb-8 border-1 border-solid border-[#c5c5c5] p-5 rounded-md">
+      <h4 className="capitalize md:text-2xl">{title}</h4>
 
       <div className={styles.eventContent}>{content}</div>
     </div>
