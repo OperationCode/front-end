9bd2ecd13c5ab83765704ac9e6ee020504b241cf
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-12-21 10:18:35 +0000

    Replaced all the css into tailwind css on TimelineNav component

--- components/Timeline/TimelineNav/TimelineNav.js
+++ components/Timeline/TimelineNav/TimelineNav.js
@@ -3,20 +3,20 @@ import historyData from '../historyData';
 
 const TimelineNav = () => {
   return (
-    <section className="py-0 px-16 sm:p-0">
+    <section className="px-16 sm:p-0">
       <nav className="flex justify-around">
         {Object.keys(historyData).map(year => (
           <Link
             key={year}
             to={`event-${year}`}
             smooth
-            className="font-bold text-xl cursor-pointer transition-all duration-100 ease-in-out hover:scale-[1.1]"
+            className="font-bold text-xl cursor-pointer opacity-60 hover:opacity-60 transition-all duration-100 ease-in-out hover:scale-[1.1]"
           >
             {year}
           </Link>
         ))}
       </nav>
-      <div className="mt-4 bg-[#3d7d7d7] h-1" />
+      <div className="mt-4 bg-[#d7d7d7] h-1" />
     </section>
   );
 };
