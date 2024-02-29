ce2899834c8dd1606ea0eff420a4ffca6ae19973
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-02-29 14:37:16 -0600

    converted Accordion.test.js to .jsx and updated tests

--- components/Accordion/__tests__/Accordion.test.tsx
+++ components/Accordion/__tests__/Accordion.test.tsx
@@ -1,20 +1,29 @@
 import { fireEvent, render } from '@testing-library/react';
-
 import {
   ACCORDION_CONTENT,
   ACCORDION_TOGGLE_BUTTON,
   SCREEN_READER_ONLY,
 } from 'common/constants/testIDs';
-import { Default } from '../__stories__/Accordion.stories';
 import { toggleMessages } from '../../ScreenReaderOnly/ScreenReaderOnly';
+import Accordion from '../Accordion';
 
 describe('Accordion', () => {
   it('should render invisible text that turns visible on toggle click', async () => {
-    const component = render(<Default {...Default.args} />);
+    const component = render(
+      <Accordion
+        className=""
+        content={{
+          headingChildren: <h6>{'faq.title'}</h6>,
+          bodyChildren: <p>{'faq.content'}</p>,
+        }}
+        accessibilityId="1"
+      />,
+    );
     const Content = component.queryByTestId(ACCORDION_CONTENT);
 
     expect(Content).not.toBeVisible();
 
+    // @ts-expect-error
     fireEvent.click(component.queryByTestId(ACCORDION_TOGGLE_BUTTON));
 
     expect(Content).toBeVisible();
@@ -23,13 +32,24 @@ describe('Accordion', () => {
 
 describe('Accordion Accessibility', () => {
   it('should display the correct screenReader text for toggle button', async () => {
-    const component = render(<Default {...Default.args} />);
+    const component = render(
+      <Accordion
+        className=""
+        content={{
+          headingChildren: <h6>{'faq.title'}</h6>,
+          bodyChildren: <p>{'faq.content'}</p>,
+        }}
+        accessibilityId="1"
+      />,
+    );
+
     const Button = component.queryByTestId(SCREEN_READER_ONLY);
 
+    //@ts-expect-error
     expect(Button.textContent).toBe(toggleMessages.open);
-
+    //@ts-expect-error
     fireEvent.click(Button);
-
+    //@ts-expect-error
     expect(Button.textContent).toBe(toggleMessages.close);
   });
 });
