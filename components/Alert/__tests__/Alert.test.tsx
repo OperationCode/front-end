92957fc5c746edfd527f434585f6232e5646a71c
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-02-29 15:04:34 -0600

    converted Alert test files to .tsx and updated snapshot

--- components/Alert/__tests__/Alert.test.tsx
+++ components/Alert/__tests__/Alert.test.tsx
@@ -1,28 +1,32 @@
 import { fireEvent, render } from '@testing-library/react';
 import createSnapshotTest from 'test-utils/createSnapshotTest';
 import { ALERT_CLOSE_BUTTON } from 'common/constants/testIDs';
-
-import { ErrorAlert, SuccessAlert, WarningAlert } from '../__stories__/Alert.stories';
+import Alert from '../Alert';
 
 describe('Alert', () => {
   it('should render error alert with required props', () => {
-    createSnapshotTest(<ErrorAlert {...ErrorAlert.args} />);
+    createSnapshotTest(<Alert type="error">Error Alert JSX or Text</Alert>);
   });
 
   it('should call close handler when close alert button clicked', () => {
     const onCloseMock = vi.fn();
 
-    const { queryByTestId } = render(<SuccessAlert {...SuccessAlert.args} onClose={onCloseMock} />);
+    const { queryByTestId } = render(
+      <Alert type="success" onClose={onCloseMock}>
+        Success Alert JSX or Text
+      </Alert>,
+    );
 
     expect(onCloseMock).toHaveBeenCalledTimes(0);
 
+    // @ts-expect-error
     fireEvent.click(queryByTestId(ALERT_CLOSE_BUTTON));
 
     expect(onCloseMock).toHaveBeenCalledTimes(1);
   });
 
   it('should NOT render button if close handler not provided', () => {
-    const { queryByTestId } = render(<WarningAlert {...WarningAlert.args} />);
+    const { queryByTestId } = render(<Alert type="warning">Warning Alert JSX or Text</Alert>);
 
     expect(queryByTestId(ALERT_CLOSE_BUTTON)).toBeNull();
   });
