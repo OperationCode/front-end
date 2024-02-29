04338784c6922b361d19cfc782e48b9b781d9058
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-02-29 13:34:38 -0600

    converted CloseButton.js to .tsx

--- components/Buttons/CloseButton/CloseButton.tsx
+++ components/Buttons/CloseButton/CloseButton.tsx
@@ -1,22 +1,26 @@
-import { bool, func, oneOf } from 'prop-types';
 import classNames from 'classnames';
 import { CLOSE_BUTTON } from 'common/constants/testIDs';
 import ScreenReaderOnly from 'components/ScreenReaderOnly/ScreenReaderOnly';
 import PlusIcon from 'static/images/icons/plus.svg';
 import styles from './CloseButton.module.css';
 
-CloseButton.propTypes = {
-  disabled: bool,
-  onClick: func.isRequired,
-  theme: oneOf(['primary', 'secondary', 'white']),
-};
+export type CloseButtonProps = {
+  /**
+   * Sets the button color theme.
+   */
+  theme?: 'primary' | 'secondary' | 'white';
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 CloseButton.defaultProps = {
   disabled: false,
   theme: 'primary',
 };
 
-export default function CloseButton({ disabled, onClick, theme }) {
+export default function CloseButton({
+  disabled = false,
+  onClick,
+  theme = 'primary',
+}: CloseButtonProps) {
   return (
     <button
       className={styles.CloseButton}
