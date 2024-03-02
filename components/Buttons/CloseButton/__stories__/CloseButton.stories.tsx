c0aaa0f8fa3579752c9acc0b93e9cac0fbe4df1b
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-03-02 14:54:01 -0600

    moved CloseButton stories below default export

--- components/Buttons/CloseButton/__stories__/CloseButton.stories.tsx
+++ components/Buttons/CloseButton/__stories__/CloseButton.stories.tsx
@@ -3,16 +3,16 @@ import CloseButton from '../CloseButton';
 
 type CloseButtonStoryType = StoryObj<typeof CloseButton>;
 
-export const Default: CloseButtonStoryType = {
-  render: args => <CloseButton {...args} />,
-  args: {
-    theme: 'primary',
-  },
-};
-
 const meta: Meta<typeof CloseButton> = {
   title: 'Buttons/Closebutton',
   component: CloseButton,
 };
 
 export default meta;
+
+export const Default: CloseButtonStoryType = {
+  render: args => <CloseButton {...args} />,
+  args: {
+    theme: 'primary',
+  },
+};
