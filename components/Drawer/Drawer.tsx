5ea7f3e07cab12d4107d18904cbf71826e8cbcfa
The Stedman <46408716+recondesigns@users.noreply.github.com>
2024-03-05 00:06:01 -0600

    Migrate Drawer, ErrorDisplay, and FeaturedJobItem components to typescript (#1804)

---
+++ components/Drawer/Drawer.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+
+export type DrawerPropsType = {
+  /**
+   * Content to be rendered in the Drawer.
+   */
+  children: React.ReactNode;
+  /**
+   * Applies class names to the container element.
+   */
+  className?: string;
+  /**
+   * Sets if the content is visible.
+   * @default false
+   */
+  isVisible?: boolean;
+};
+
+function Drawer({ children, className, isVisible = false }: DrawerPropsType) {
+  return (
+    <div
+      className={classNames(
+        className,
+        'hidden lg:block transition-all ease-in-out duration-1000 fixed top-0 bottom-0  overflow-hidden width-full z-[2]',
+        {
+          '-left-0': isVisible,
+          '-left-full': !isVisible,
+        },
+      )}
+    >
+      <div className="w-full h-full">{children}</div>
+    </div>
+  );
+}
+
+export default Drawer;
---
+++ components/Drawer/__stories__/Drawer.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Drawer from '../Drawer';
+
+type DrawerStorytype = StoryObj<typeof Drawer>;
+
+const meta: Meta<typeof Drawer> = {
+  title: 'Drawer',
+  component: Drawer,
+  parameters: {
+    viewport: {
+      defaultViewport: 'tablet',
+    },
+  },
+};
+
+export default meta;
+
+/**
+ * Default Drawer supplied with only required args.
+ */
+export const Default: DrawerStorytype = {
+  render: args => <Drawer {...args} />,
+  args: {
+    children: 'Drawer content will only display on display size of Tablet or smaller',
+    isVisible: true,
+  },
+};
