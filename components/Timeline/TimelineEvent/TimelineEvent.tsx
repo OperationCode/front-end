525257647f50a6450e243bc8a988bacf7616bd0b
Kyle Holmberg <kylemh.email@gmail.com>
2024-03-13 16:03:03 +0900

    i guess i was wrong 😂

--- components/Timeline/TimelineEvent/TimelineEvent.tsx
+++ components/Timeline/TimelineEvent/TimelineEvent.tsx
@@ -1,7 +1,7 @@
 import styles from './TimelineEvent.module.css';
 
 export type TimelineEventPropsType = {
-  content: React.ReactNode[];
+  content: React.ReactNode | React.ReactNode[];
   title: string;
 };
 
