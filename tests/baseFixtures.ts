642875ae8c69c754938e075d2489be638bbf1a90
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2024-06-06 15:24:04 -0700

    added base fixtures and updated package.json

---
+++ tests/baseFixtures.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as crypto from 'crypto';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { test as baseTest } from '@playwright/test';
+
+const istanbulCLIOutput = path.join(process.cwd(), '.nyc_output');
+
+export function generateUUID(): string {
+  return crypto.randomBytes(16).toString('hex');
+}
+
+export const test = baseTest.extend({
+  context: async ({ context }, use) => {
+    await context.addInitScript(() =>
+      window.addEventListener('beforeunload', () =>
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-underscore-dangle
+        (window as any).collectIstanbulCoverage(JSON.stringify((window as any).__coverage__)),
+      ),
+    );
+    await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
+    await context.exposeFunction('collectIstanbulCoverage', (coverageJSON: string) => {
+      if (coverageJSON)
+        fs.writeFileSync(
+          path.join(istanbulCLIOutput, `playwright_coverage_${generateUUID()}.json`),
+          coverageJSON,
+        );
+    });
+    await use(context);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const page of context.pages()) {
+      // eslint-disable-next-line no-await-in-loop
+      await page.evaluate(() =>
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-underscore-dangle
+        (window as any).collectIstanbulCoverage(JSON.stringify((window as any).__coverage__)),
+      );
+    }
+  },
+});
+
+export const { expect } = test;
