9e9594142d5115d1af7e6a1c95907e687640a8a3
Michael Dragomir <michaeldragomir@gmail.com>
2024-06-08 14:58:40 -0700

    initial PR for corporate training

---
+++ pages/corporate_training.tsx
@@ -0,0 +1,13 @@
+import Head from 'components/head';
+
+const pageTitle = 'Corporate Training: Breaking Biases';
+
+const CorporateTraining = () => {
+  return (
+    <div>
+      <Head title={pageTitle} />
+    </div>
+  );
+};
+
+export default CorporateTraining;
