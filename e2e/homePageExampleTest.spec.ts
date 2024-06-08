f241da47a1817beecd56b878c91f3b6478adb6c0
Michael Dragomir <michaeldragomir@gmail.com>
2024-06-08 07:13:29 -0700

    text update

--- e2e/homePageExampleTest.spec.ts
+++ e2e/homePageExampleTest.spec.ts
@@ -6,12 +6,8 @@ test('HomePage Example Test', async ({ page }) => {
   await expect(page).toHaveTitle('Operation Code | Home');
 });
 
-test('get started link', async ({ page }) => {
+test('learn more link', async ({ page }) => {
   await page.goto('https://operationcode.org/');
-
-  // Click the get started link.
   await page.getByRole('link', { name: 'learn more' }).click();
-
-  // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'about us' })).toBeVisible();
 });
