2c22e32ecb6a0a25116e76f6e43112bfd662a691
Michael Dragomir <michaeldragomir@gmail.com>
2024-06-08 07:08:21 -0700

    testing out playwright

--- e2e/homePageExampleTest.spec.ts
+++ e2e/homePageExampleTest.spec.ts
@@ -5,3 +5,13 @@ test('HomePage Example Test', async ({ page }) => {
   await page.goto('https://operationcode.org/');
   await expect(page).toHaveTitle('Operation Code | Home');
 });
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://operationcode.org/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'learn more' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'about us' })).toBeVisible();
+});
