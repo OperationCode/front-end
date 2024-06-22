04d1b3b00bcadec9686cbd1d8087e771c83d21db
Kyle Holmberg <kylemh.email@gmail.com>
2024-06-22 13:05:36 +0700

    delete unused 'UpgradeBrowserOverlay'

--- components/UpgradeBrowserOverlay/__tests__/UpgradeBrowserOverlay.test.tsx
+++
@@ -1,8 +0,0 @@
-import { UpgradeBrowserOverlay } from '../UpgradeBrowserOverlay';
-import createShallowSnapshotTest from '@/test-utils/createShallowSnapshotTest';
-
-describe('UpgradeBrowserOverlay', () => {
-  it('should render with no props passed', () => {
-    createShallowSnapshotTest(<UpgradeBrowserOverlay />);
-  });
-});
