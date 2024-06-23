432ff29a559bcbb32c396408d70ef7ef4447daba
Kyle Holmberg <kylemh.email@gmail.com>
2024-06-23 18:23:08 +0700

    for good measure

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -13,7 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-import './commands';
+import './commands.ts';
 import '@cypress/code-coverage/support';
 
 // beforeEach(() => {
