26240007df0b6377ae39ddc9dea7c88b74396f4d
Franciscus Agnew <franciscusagnew@users.noreply.github.com>
2025-01-31 11:50:53 -0500

    Cleanup for cross-branch conflicts

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Router from 'next/router';
 import classNames from 'classnames';
 import Logo from 'public/static/images/logo.svg';
+
 import { desktopNavItems, mobileNavItems } from 'common/constants/navigation';
 import NavMobile from 'components/Nav/NavMobile/NavMobile';
 import dynamic from 'next/dynamic';
@@ -12,76 +13,76 @@ import styles from './Nav.module.css';
 const NavListItem = dynamic(() => import('components/Nav/NavListItem/NavListItem'), { ssr: false });
 
 export const Nav = () => {
-  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+    const [isMobileNavOpen, setMobileNavOpen] = useState(false);
 
-  const openMobileMenu = () => {
-    setMobileNavOpen(true);
-    document.body.style.overflow = 'hidden';
-  };
-
-  const closeMobileMenu = () => {
-    setMobileNavOpen(false);
-    document.body.style.overflow = 'auto';
-  };
-
-  useEffect(() => {
-    Router.events.on('routeChangeComplete', closeMobileMenu);
-
-    return () => {
-      Router.events.off('routeChangeComplete', closeMobileMenu);
+    const openMobileMenu = () => {
+        setMobileNavOpen(true);
+        document.body.style.overflow = 'hidden';
     };
-  }, []);
 
-  return (
-    <>
-      {/* Always rendered, but conditionally displayed via media query */}
-      <NavMobile
-        isOpen={isMobileNavOpen}
-        closeMenu={closeMobileMenu}
-        openMenu={openMobileMenu}
-        navItems={mobileNavItems}
-      />
+    const closeMobileMenu = () => {
+        setMobileNavOpen(false);
+        document.body.style.overflow = 'auto';
+    };
 
-      <header className={styles.NavDesktop}>
-        <div className={styles.desktopNavContainer} data-testid="Desktop Nav Container">
-          <nav data-testid="Desktop Nav">
-            <Link href="/" key="Home">
-              <a
-                className={classNames(styles.logoLink, styles.link)}
-                onContextMenu={event => {
-                  event.preventDefault();
-                  Router.push('/branding');
-                }}
-              >
-                <Logo className={styles.logo} style={{ width: 224, height: 42 }} fill="#f7f7f7" />
-              </a>
-            </Link>
+    useEffect(() => {
+        Router.events.on('routeChangeComplete', closeMobileMenu);
 
-            <ul className={styles.link}>
-              {desktopNavItems.map(navItem => (
-                <NavListItem
-                  key={navItem.name}
-                  {...navItem}
-                  icon={
-                    'icon' in navItem && navItem.icon === 'UserLogo' ? (
-                      <UserLogo className={styles.navIcon} />
-                    ) : null
-                  }
-                />
-              ))}
+        return () => {
+            Router.events.off('routeChangeComplete', closeMobileMenu);
+        };
+    }, []);
 
-              {/* stylistic one-off */}
-              <li key="Donate">
-                <Link href="/donate">
-                  <a className={classNames(styles.link, styles.donateLink)}>Donate</a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </>
-  );
+    return (
+        <>
+            {/* Always rendered, but conditionally displayed via media query */}
+            <NavMobile
+                isOpen={isMobileNavOpen}
+                closeMenu={closeMobileMenu}
+                openMenu={openMobileMenu}
+                navItems={mobileNavItems}
+            />
+
+            <header className={styles.NavDesktop}>
+                <div className={styles.desktopNavContainer} data-testid="Desktop Nav Container">
+                    <nav data-testid="Desktop Nav">
+                        <Link href="/" key="Home">
+                            <a
+                                className={classNames(styles.logoLink, styles.link)}
+                                onContextMenu={event => {
+                                    event.preventDefault();
+                                    Router.push('/branding');
+                                }}
+                            >
+                                <Logo className={styles.logo} style={{ width: 224, height: 42 }} fill="#f7f7f7" />
+                            </a>
+                        </Link>
+
+                        <ul className={styles.link}>
+                            {desktopNavItems.map(navItem => (
+                                <NavListItem
+                                    key={navItem.name}
+                                    {...navItem}
+                                    icon={
+                                        'icon' in navItem && navItem.icon === 'UserLogo' ? (
+                                            <UserLogo className={styles.navIcon} />
+                                        ) : null
+                                    }
+                                />
+                            ))}
+
+                            {/* stylistic one-off */}
+                            <li key="Donate">
+                                <Link href="/donate">
+                                    <a className={classNames(styles.link, styles.donateLink)}>Donate</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Nav;
--- components/Nav/NavListItem/NavListItem.tsx
+++ components/Nav/NavListItem/NavListItem.tsx
@@ -4,137 +4,133 @@ import Link from 'next/link';
 import PlusIcon from 'static/images/icons/plus.svg';
 import MinusIcon from 'static/images/icons/minus.svg';
 import OutboundLink from 'components/OutboundLink/OutboundLink';
-import { twMerge } from 'tailwind-merge';
 import styles from './NavListItem.module.css';
 
 interface SublinkType {
-  name: string;
-  href: string;
-  isExternal?: boolean;
-}
+    name: string;
+    href: string;
+    isExternal?: boolean;
+};
 
 export interface NavListItemPropsType {
-  /**
-   * Text used for the label.
-   */
-  name: string;
-  /**
-   * Url to be passed to the base anchor element.
-   */
-  href: string;
-  /**
-   * List of child links containing the `name` and `href`
-   */
-  sublinks?: SublinkType[];
-  /**
-   * Includes an optional icon.
-   */
-  icon?: React.ReactElement | null;
+    /**
+     * Text used for the label.
+     */
+    name: string;
+    /**
+     * Url to be passed to the base anchor element.
+     */
+    href: string;
+    /**
+     * List of child links containing the `name` and `href`
+     */
+    sublinks?: SublinkType[];
+    /**
+     * Includes an optional icon.
+     */
+    icon?: React.ReactElement | null;
 }
 
-function NavListItem({ sublinks, href, name, icon = null }: NavListItemPropsType) {
-  const [areSublinksVisible, setSublinksVisible] = useState(false);
+function NavListItem({ sublinks, href, name, icon = null}: NavListItemPropsType) {
+    const [areSublinksVisible, setSublinksVisible] = useState(false);
 
-  const handleKeyDown = (event: React.KeyboardEvent, indexKeyedOn: number) => {
-    const lastSublinkIndex = sublinks && sublinks.length - 1;
-    const isLastSublink = indexKeyedOn === lastSublinkIndex;
-    const isFirstSublink = indexKeyedOn === 0;
+    const handleKeyDown = (event: React.KeyboardEvent, indexKeyedOn: number) => {
+        const lastSublinkIndex = sublinks && sublinks.length - 1;
+        const isLastSublink = indexKeyedOn === lastSublinkIndex;
+        const isFirstSublink = indexKeyedOn === 0;
 
-    const didHitTab = event.key === 'Tab';
-    const didTabForward = didHitTab && !event.shiftKey;
-    const didTabBackward = didHitTab && event.shiftKey;
+        const didHitTab = event.key === 'Tab';
+        const didTabForward = didHitTab && !event.shiftKey;
+        const didTabBackward = didHitTab && event.shiftKey;
 
-    const shouldHideSublinks =
-      (isLastSublink && didTabForward) || (isFirstSublink && didTabBackward);
+        const shouldHideSublinks =
+            (isLastSublink && didTabForward) || (isFirstSublink && didTabBackward);
 
-    if (shouldHideSublinks) {
-      setSublinksVisible(false);
-    }
-  };
+        if (shouldHideSublinks) {
+            setSublinksVisible(false);
+        }
+    };
 
-  const hasSublinks = sublinks && sublinks.length > 0;
-  const exposeSublinks = () => setSublinksVisible(true);
-  const hideSublinks = () => setSublinksVisible(false);
-  const invertSublinkVisibility = () => setSublinksVisible(previousState => !previousState);
+    const hasSublinks = sublinks && sublinks.length > 0;
+    const exposeSublinks = () => setSublinksVisible(true);
+    const hideSublinks = () => setSublinksVisible(false);
+    const invertSublinkVisibility = () => setSublinksVisible(previousState => !previousState);
 
-  return (
-    <li className={styles.NavListItem}>
-      <Link href={href}>
-        <a
-          className={classNames(
-            twMerge(styles.link, '[&>svg]:-bottom-2 [&>svg]:right-3'),
-            styles.navItemLink,
-          )}
-          onMouseEnter={exposeSublinks}
-          onMouseLeave={hideSublinks}
-          role="link"
-          tabIndex={0}
-          data-testid={`Nav Item ${name}`}
-        >
-          <span className={styles.linkContent}>{name}</span>
-          {icon && icon}
-        </a>
-      </Link>
+    return (
+        <li className={styles.NavListItem}>
+            <Link href={href}>
+                <a
+                    className={classNames(styles.link, styles.navItemLink)}
+                    onMouseEnter={exposeSublinks}
+                    onMouseLeave={hideSublinks}
+                    role="link"
+                    tabIndex={0}
+                    data-testid={`Nav Item ${name}`}
+                >
+                    <span className={styles.linkContent}>{name}</span>
+                    {icon && icon}
+                </a>
+            </Link>
 
-      {hasSublinks && (
-        <>
-          <button
-            aria-expanded={areSublinksVisible}
-            aria-haspopup={hasSublinks}
-            aria-label="submenu"
-            className={styles.sublinkToggleButton}
-            onClick={invertSublinkVisibility}
-            onMouseEnter={exposeSublinks}
-            onMouseLeave={hideSublinks}
-            type="button"
-          >
-            {areSublinksVisible ? (
-              <MinusIcon className={styles.icon} data-testid="minus-icon" />
-            ) : (
-              <PlusIcon className={styles.icon} data-testid="plus-icon" />
-            )}
-          </button>
-
-          <ul
-            className={classNames(styles.sublinksList, {
-              [styles.invisible]: !areSublinksVisible,
-            })}
-            onMouseEnter={exposeSublinks}
-            onMouseLeave={hideSublinks}
-          >
-            {sublinks.map((sublink, index) => (
-              <li className={styles.sublinkListItem} key={sublink.name}>
-                {/* 😞 next/link fought being mocked, so `prefetch` has test-specific code */}
-                {!sublink.isExternal ? (
-                  <Link href={sublink.href} prefetch={process.env.NODE_ENV === 'production'}>
-                    <a
-                      className={twMerge(styles.link, '[&>svg]:-bottom-2 [&>svg]:right-3')}
-                      role="link"
-                      tabIndex={0}
-                      data-testid={`Nav Item ${sublink.name}`}
-                      onKeyDown={event => handleKeyDown(event, index)}
+            {hasSublinks && (
+                <>
+                    <button
+                        aria-expanded={areSublinksVisible}
+                        aria-haspopup={hasSublinks}
+                        aria-label="submenu"
+                        className={styles.sublinkToggleButton}
+                        onClick={invertSublinkVisibility}
+                        onMouseEnter={exposeSublinks}
+                        onMouseLeave={hideSublinks}
+                        type="button"
                     >
-                      <span className={styles.linkContent}>{sublink.name}</span>
-                    </a>
-                  </Link>
-                ) : (
-                  <OutboundLink
-                    analyticsEventLabel={`Clicked on ${sublink.name} -> ${sublink.href}`}
-                    className={twMerge(styles.link, '[&>svg]:-bottom-2 [&>svg]:right-3')}
-                    data-testid={`Nav Item ${sublink.name}`}
-                    href={sublink.href}
-                    hasIcon
-                  >
-                    <span className={twMerge(styles.link, '[&>svg]:-bottom-2 [&>svg]:right-3')}>{sublink.name}</span>
-                  </OutboundLink>
-                )}
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-    </li>
-  );
+                        {areSublinksVisible ? (
+                            <MinusIcon className={styles.icon} data-testid="minus-icon" />
+                        ) : (
+                            <PlusIcon className={styles.icon} data-testid="plus-icon" />
+                        )}
+                    </button>
+
+                    <ul
+                        className={classNames(styles.sublinksList, {
+                            [styles.invisible]: !areSublinksVisible,
+                        })}
+                        onMouseEnter={exposeSublinks}
+                        onMouseLeave={hideSublinks}
+                    >
+                        {sublinks.map((sublink, index) => (
+                            <li className={styles.sublinkListItem} key={sublink.name}>
+                                {/* 😞 next/link fought being mocked, so `prefetch` has test-specific code */}
+                                {!sublink.isExternal ? (
+                                    <Link href={sublink.href} prefetch={process.env.NODE_ENV === 'production'}>
+                                        <a
+                                            className={styles.link}
+                                            role="link"
+                                            tabIndex={0}
+                                            data-testid={`Nav Item ${sublink.name}`}
+                                            onKeyDown={event => handleKeyDown(event, index)}
+                                        >
+                                            <span className={styles.linkContent}>{sublink.name}</span>
+                                        </a>
+                                    </Link>
+                                ) : (
+                                    <OutboundLink
+                                        analyticsEventLabel={`Clicked on ${sublink.name} -> ${sublink.href}`}
+                                        className={styles.link}
+                                        data-testid={`Nav Item ${sublink.name}`}
+                                        href={sublink.href}
+                                        hasIcon
+                                    >
+                                        <span className={styles.link}>{sublink.name}</span>
+                                    </OutboundLink>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </li>
+    );
 }
 
 export default NavListItem;
