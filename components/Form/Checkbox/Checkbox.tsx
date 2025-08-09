9b393aee1920cd6b3b6d4607c2e15845738608ba
Billy Le <hi@billyle.dev>
2025-08-09 12:10:00 -0500

    fix custom tw colors opacity. replace Input css module with tw (#1906)
    * fix custom tw colors opacity. replace Input css module with tw
    
    * type cast hslWithOpacity values to string
    
    * remove arbitrary values

--- components/Form/Checkbox/Checkbox.tsx
+++ components/Form/Checkbox/Checkbox.tsx
@@ -16,13 +16,13 @@ function Checkbox({
   id,
   label,
 }: CheckboxProps) {
-  const hasErrors = errors[name];
+  const hasErrors = Boolean(errors[name]);
   return (
     <div className="relative m-4" data-testid={CHECKBOX}>
       <Label for={name} isHidden={false}>
         <input
           {...field}
-          className="border border-secondary/5 rounded-[3px] text-lg p-2 scale-150 mr-3 disabled:opacity-60 hover:disabled:cursor-not-allowed"
+          className="border border-secondary/5 rounded-sm text-lg p-2 scale-150 mr-3 disabled:opacity-60 hover:disabled:cursor-not-allowed"
           id={id || name}
           name={name}
           type="checkbox"
