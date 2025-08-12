4d6cfb7888d697bd3769505dc60cc9b2ebab2084
Billy Le <hi@billyle.dev>
2025-08-12 14:05:28 -0500

    add NavLink type

--- common/constants/navigation.ts
+++ common/constants/navigation.ts
@@ -1,128 +1,128 @@
-import flattenDepth from 'lodash/flattenDepth';
+interface NavLink {
+  name: string;
+  href: string;
+  sublinks?: { name: string; href: string }[];
+  isExternal?: boolean;
+}
 
-const donate = {
+type MobileNavLink = Pick<NavLink, 'name' | 'href'>;
+
+const donate: NavLink = {
   href: '/donate',
   name: 'Donate',
 };
 
-const services = {
+const services: NavLink = {
   name: 'Services',
   href: '/services',
 };
 
-const about = {
+const about: NavLink = {
   name: 'About Us',
   href: '/about',
 };
 
-const history = {
+const history: NavLink = {
   name: 'History',
   href: '/history',
 };
 
-const team = {
+const team: NavLink = {
   name: 'Our Team',
   href: '/team',
 };
 
-const contact = {
+const contact: NavLink = {
   name: 'Contact Us',
   href: '/contact',
 };
 
-const faq = {
+const faq: NavLink = {
   name: 'FAQ',
   href: '/faq',
 };
 
-const podcast = {
+const podcast: NavLink = {
   name: 'Podcast',
   href: '/podcast',
 };
 
-const branding = {
+const branding: NavLink = {
   name: 'Branding',
   href: '/branding',
 };
 
-const getInvolved = {
+const getInvolved: NavLink = {
   name: 'Get Involved',
   href: '/get_involved',
 };
 
-const merchStore = {
+const merchStore: NavLink = {
   name: 'Merch Store',
   href: '/swag',
   isExternal: true,
 };
 
-const jobs = {
+const jobs: NavLink = {
   href: '/jobs',
   name: 'Job Board',
 };
 
-const chapters = {
+const chapters: NavLink = {
   name: 'Chapters',
   href: '/chapters',
 };
 
-const sponsorship = {
+const sponsorship: NavLink = {
   name: 'Sponsorship',
   href: '/sponsorship',
 };
 
-const scholarship = {
+const scholarship: NavLink = {
   name: 'Scholarship',
   href: '/scholarship',
 };
 
-const projectRebuild = {
+const projectRebuild: NavLink = {
   name: 'Project Rebuild',
   href: '/project_rebuild',
 };
 
-const press = {
+const press: NavLink = {
   name: 'Press',
   href: '/press',
 };
 
-const corporateTraining = {
+const corporateTraining: NavLink = {
   name: 'Corporate Training',
   href: '/corporate-training',
 };
 
-const servicesGroup = {
+const servicesGroup: NavLink = {
   ...services,
   sublinks: [podcast, scholarship, projectRebuild, corporateTraining],
 };
 
-const aboutUsGroup = {
+const aboutUsGroup: NavLink = {
   ...about,
   sublinks: [team, history, faq, branding],
 };
 
-const getInvolvedGroup = {
+const getInvolvedGroup: NavLink = {
   ...getInvolved,
   sublinks: [chapters, sponsorship, merchStore, contact, donate],
 };
 
 // MARK: Nav items
-export const desktopNavItems = [aboutUsGroup, servicesGroup, getInvolvedGroup];
+export const desktopNavItems: NavLink[] = [aboutUsGroup, servicesGroup, getInvolvedGroup];
 
 // Extracts sublinks to list everything as a single, top-level list
-export const mobileNavItems = flattenDepth(
-  [about, getInvolved, ...servicesGroup.sublinks, ...getInvolvedGroup.sublinks].map(
-    ({
-      sublinks = [],
-      ...item
-    }: {
-      name: string;
-      href: string;
-      sublinks?: { name: string; href: string }[];
-    }) => [item, sublinks],
-  ),
-  2,
-);
+export const mobileNavItems: MobileNavLink[] = [
+  about,
+  getInvolved,
+  servicesGroup.sublinks as MobileNavLink[],
+  getInvolvedGroup.sublinks as MobileNavLink[],
+].flat();
 
 export const footerItems = {
   items: [
