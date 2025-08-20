e6d36d0a7884bca369f73b4c11af1c4a5313e498
Billy Le <hi@billyle.dev>
2025-08-20 13:08:58 -0500

    replaced Branding page styles with tw

--- components/Branding/Swatch/Swatch.tsx
+++ components/Branding/Swatch/Swatch.tsx
@@ -13,10 +13,13 @@ export interface Swatch {
 
 function Swatch({ colorName, hexCode }: Swatch) {
   return (
-    <figure className="border-1 border-solid border-[#333333] p-4 m-6 w-56">
+    <figure className="border border-solid border-theme-gray-800 p-4 m-6 w-56">
       <ScreenReaderOnly>{`A block of the color ${colorName}`}</ScreenReaderOnly>
 
-      <div className="h-56 mb-4" style={{ backgroundColor: hexCode }} />
+      <div
+        className="h-56 mb-4 border border-solid border-theme-gray-800"
+        style={{ backgroundColor: hexCode }}
+      />
 
       <figcaption>
         <h6>{colorName.toUpperCase()}</h6>
